a0e0adfe771d973bf66b97257263390e681d1874
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2021-11-16 14:34:45 +0100

    Add a dependency to monorepo

--- packages/studio-ui/next.config.mjs
+++ packages/studio-ui/next.config.mjs
@@ -1,12 +1,14 @@
+/** @type {import("@mui/studio-core").StudioConfiguration} */
 const studioConfig = process.env.STUDIO_UI_CONFIG ? JSON.parse(process.env.STUDIO_UI_CONFIG) : {};
 
 // We can pass this to serverRuntimeConfig and publicRuntimeConfig as desired
 // QUESTION: Do we really want to depend on runtime config? What are the alternatives?
-// Running a backend server and fetching configuration ourselves on app startup?
-// Do we want to keep the app statically servable?
+// Avoid serverRuntimeConfig and just read the env variable in API routes?
 console.log(`Config: ${JSON.stringify(studioConfig, null, 2)}`);
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  reactStrictMode: true,
+};
 
 export default config;

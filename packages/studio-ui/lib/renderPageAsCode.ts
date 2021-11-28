ae385fc0703872fbb0cda793384610f8d8d03d01
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2021-11-28 10:07:54 +0100

    fix warning pretteir

--- packages/studio-ui/lib/renderPageAsCode.ts
+++ packages/studio-ui/lib/renderPageAsCode.ts
@@ -1,5 +1,5 @@
 import * as prettier from 'prettier';
-import parserTypescript from 'prettier/parser-typescript';
+import parserBabel from 'prettier/parser-babel';
 import { transform, Transform } from 'sucrase';
 import { StudioPage, NodeId, StudioNode, CodeGenContext } from './types';
 import { getNode } from './studioPage';
@@ -286,8 +286,8 @@ export default function renderPageAsCode(
 
   if (config.pretty) {
     code = prettier.format(code, {
-      parser: 'typescript',
-      plugins: [parserTypescript],
+      parser: 'babel-ts',
+      plugins: [parserBabel],
     });
   }
 

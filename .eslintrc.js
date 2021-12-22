5f89dd56fd87852d5076fc49cc18bbae8efd8416
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2021-12-22 12:04:45 +0100

    fix some linting issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,11 +9,11 @@ module.exports = {
    */
   rules: {
     ...baseline.rules,
-    'import/prefer-default-export': 'off',
+    'import/prefer-default-export': ['off'],
     // TODO move rule into the main repo once it has upgraded
-    '@typescript-eslint/return-await': 'off',
+    '@typescript-eslint/return-await': ['off'],
     // TODO move rule into main repo to allow deep @material-ui/monorepo imports
-    'no-restricted-imports': 'off',
+    'no-restricted-imports': ['off'],
     'jsdoc/require-param': ['error', { contexts: ['TSFunctionType'] }],
     'jsdoc/require-param-type': ['error', { contexts: ['TSFunctionType'] }],
     'jsdoc/require-param-name': ['error', { contexts: ['TSFunctionType'] }],
@@ -25,4 +25,13 @@ module.exports = {
     'no-alert': ['off'],
     'no-console': ['off'],
   },
+  overrides: baseline.overrides.map((override) => {
+    return {
+      ...override,
+      rules: {
+        ...override.rules,
+        'no-restricted-imports': ['off'],
+      },
+    };
+  }),
 };

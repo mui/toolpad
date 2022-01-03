fe3b37a2b07f25bc510ac813ff62b1100e651430
Olivier Tassinari <olivier.tassinari@gmail.com>
2022-01-03 12:45:58 +0100

    [core] Prefer MUI over Material-UI (#21)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-const baseline = require('@material-ui/monorepo/.eslintrc');
+const baseline = require('@mui/monorepo/.eslintrc');
 
 module.exports = {
   ...baseline,
@@ -12,7 +12,7 @@ module.exports = {
     'import/prefer-default-export': ['off'],
     // TODO move rule into the main repo once it has upgraded
     '@typescript-eslint/return-await': ['off'],
-    // TODO move rule into main repo to allow deep @material-ui/monorepo imports
+    // TODO move rule into main repo to allow deep @mui/monorepo imports
     'no-restricted-imports': ['off'],
     'jsdoc/require-param': ['error', { contexts: ['TSFunctionType'] }],
     'jsdoc/require-param-type': ['error', { contexts: ['TSFunctionType'] }],

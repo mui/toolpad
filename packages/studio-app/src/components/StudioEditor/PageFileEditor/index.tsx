3419b9d471303476316df02293d10466e18a3646
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-01-13 15:25:42 +0100

    open component tab on sleection

--- packages/studio-app/src/components/StudioEditor/PageFileEditor/index.tsx
+++ packages/studio-app/src/components/StudioEditor/PageFileEditor/index.tsx
@@ -3,6 +3,7 @@ import { styled } from '@mui/system';
 import RenderPanel from './RenderPanel';
 import ComponentPanel from './ComponentPanel';
 import BindingEditor from './BindingEditor';
+import { useEditorApi, useEditorState } from '../EditorProvider';
 
 const classes = {
   content: 'StudioContent',
@@ -29,6 +30,16 @@ interface PageFileEditorProps {
 }
 
 export default function PageFileEditor({ className }: PageFileEditorProps) {
+  const state = useEditorState();
+  const api = useEditorApi();
+
+  React.useEffect(() => {
+    // TODO: better way for this?
+    if (state.selection) {
+      api.pageEditor.setComponentPanelTab('component');
+    }
+  }, [api, state.selection]);
+
   return (
     <PageFileEditorRoot className={className}>
       <RenderPanel className={classes.renderPanel} />

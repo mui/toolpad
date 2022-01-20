2a4b358e6ace3b0173ac2f341889a37efcf60e89
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-01-20 16:07:46 +0100

    We don't use this anymore

--- packages/studio-core/src/constants.ts
+++ packages/studio-core/src/constants.ts
@@ -1,4 +1,3 @@
-export const DEFINITION_KEY = Symbol.for('studio.componentDefinition');
 export const RUNTIME_PROP_NODE_ID = '__studioNodeId';
 export const RUNTIME_PROP_STUDIO_SLOTS = '__studioSlots';
 export const RUNTIME_PROP_STUDIO_SLOTS_TYPE = '__studioSlotsType';

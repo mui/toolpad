03a6439a7c0581e40ff0fbc89cdebba142646765
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-02-04 16:49:53 +0100

    runtimeconfig

--- packages/studio-app/src/config.ts
+++ packages/studio-app/src/config.ts
@@ -1,9 +1,9 @@
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig } = getConfig();
+
 export interface SharedConfig {
   demoMode: boolean;
 }
 
-const clientConfig: SharedConfig = {
-  demoMode: process.env.DEMO_MODE === 'true',
-};
-
-export default clientConfig;
+export default publicRuntimeConfig as SharedConfig;

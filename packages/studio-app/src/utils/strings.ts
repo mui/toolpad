87d48d018fee7fd393f5e8882d2ec2222da1e33d
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-02-28 11:37:39 +0100

    Inject a datagrid license

--- packages/studio-app/src/utils/strings.ts
+++ packages/studio-app/src/utils/strings.ts
@@ -42,3 +42,12 @@ export function generateUniqueString(
   }
   return suggestion;
 }
+
+export function escapeHtml(unsafe: string): string {
+  return unsafe
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
+}

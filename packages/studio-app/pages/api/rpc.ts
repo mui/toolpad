ea2ad76e40ada6c79d48977649735bd749902db9
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-03-09 11:18:09 +0100

    Introduce rpc context (#179)
    * rpc
    
    * bring in context
    
    * remove asyncocalstorage

--- packages/studio-app/pages/api/rpc.ts
+++ packages/studio-app/pages/api/rpc.ts
@@ -1,6 +1,5 @@
 import { NextApiHandler } from 'next';
-import { AsyncLocalStorage } from 'async_hooks';
-import type { IncomingMessage, ServerResponse } from 'http';
+import type { IncomingMessage } from 'http';
 import {
   testConnection,
   execApi,
@@ -15,32 +14,33 @@ import {
 } from '../../src/server/data';
 import { hasOwnProperty } from '../../src/utils/collections';
 
-const asyncLocalStorage = new AsyncLocalStorage<RpcContext>();
-
-export function getContext(): RpcContext {
-  const ctx = asyncLocalStorage.getStore();
-  if (!ctx) {
-    throw new Error('Not in a request context');
-  }
-  return ctx;
-}
-
 interface RpcContext {
   req: IncomingMessage;
-  res: ServerResponse;
 }
 
 export interface Method<P extends any[] = any[], R = any> {
   (...params: P): Promise<R>;
 }
-
-export interface Methods {
+export interface MethodsGroup {
   readonly [key: string]: Method;
 }
 
+export interface MethodResolvers {
+  readonly [key: string]: MethodResolver<any>;
+}
+
 export interface Definition {
-  readonly query: Methods;
-  readonly mutation: Methods;
+  readonly query: MethodResolvers;
+  readonly mutation: MethodResolvers;
+}
+
+export type MethodsOfGroup<R extends MethodResolvers> = {
+  [K in keyof R]: (...params: Parameters<R[K]>[0]) => ReturnType<R[K]>;
+};
+
+export interface MethodsOf<D extends Definition> {
+  readonly query: MethodsOfGroup<D['query']>;
+  readonly mutation: MethodsOfGroup<D['mutation']>;
 }
 
 export interface RpcRequest {
@@ -65,37 +65,59 @@ function createRpcHandler(definition: Definition): NextApiHandler<RpcResponse> {
       res.status(404).end();
       return;
     }
-    const method = definition[type][name];
+    const method: MethodResolver<any> = definition[type][name];
     const context = { req, res };
-    const result = await asyncLocalStorage.run(context, () => method(...params));
+    const result = await method(params, context);
     const responseData: RpcResponse = { result };
     res.json(responseData);
   };
 }
 
+interface MethodResolver<F extends Method> {
+  (params: Parameters<F>, ctx: RpcContext): ReturnType<F>;
+}
+
+function createMethod<F extends Method>(handler: MethodResolver<F>): MethodResolver<F> {
+  return handler;
+}
+
 const rpcServer = {
   query: {
-    execApi: (...args: Parameters<typeof execApi>) => {
-      // DEMO: how we can add authentication in the mix:
-      //   const ctx = getContext();
-      //   console.log(ctx.req.headers);
+    execApi: createMethod<typeof execApi>((args) => {
       return execApi(...args);
-    },
-
-    getReleases,
-    findActiveDeployment,
-    loadReleaseDom,
-    loadDom,
+    }),
+    getReleases: createMethod<typeof getReleases>((params) => {
+      return getReleases(...params);
+    }),
+    findActiveDeployment: createMethod<typeof findActiveDeployment>((params) => {
+      return findActiveDeployment(...params);
+    }),
+    loadReleaseDom: createMethod<typeof loadReleaseDom>((params) => {
+      return loadReleaseDom(...params);
+    }),
+    loadDom: createMethod<typeof loadDom>((params) => {
+      return loadDom(...params);
+    }),
   },
   mutation: {
-    createRelease,
-    deleteRelease,
-    createDeployment,
-    testConnection,
-    saveDom,
+    createRelease: createMethod<typeof createRelease>((params) => {
+      return createRelease(...params);
+    }),
+    deleteRelease: createMethod<typeof deleteRelease>((params) => {
+      return deleteRelease(...params);
+    }),
+    createDeployment: createMethod<typeof createDeployment>((params) => {
+      return createDeployment(...params);
+    }),
+    testConnection: createMethod<typeof testConnection>((params) => {
+      return testConnection(...params);
+    }),
+    saveDom: createMethod<typeof saveDom>((params) => {
+      return saveDom(...params);
+    }),
   },
 } as const;
 
-export type ServerDefinition = typeof rpcServer;
+export type ServerDefinition = MethodsOf<typeof rpcServer>;
 
 export default createRpcHandler(rpcServer);

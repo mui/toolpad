5c89a1ccd394dcec5a7df12f86a70701a6428448
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-04-05 12:04:03 +0200

    Start looking into some testing infra (#59)
    * Prepare coderenderer for elaborate databinding
    
    * remove stringliteral for now
    
    * towards render functions
    
    * remove renderNodeInternal
    
    * hoist Page Component
    
    * Add most basic test
    
    * misisng includes
    
    This reverts commit c79fde41c90f1dedb9d68d69e7435f67c5a77cec.
    
    * tsx
    
    * Add test to ci
    
    * unit
    
    * like zis?
    
    * transform
    
    * experimentje
    
    * this wasn't necessary
    
    * update test
    
    * Update yarn.lock
    
    * comment
    
    * rename
    
    * update testing library
    
    * enable test

---
+++ packages/toolpad-app/src/renderPageCode.test.tsx
@@ -0,0 +1,85 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import * as React from 'react';
+import * as esbuild from 'esbuild';
+import { render } from '@testing-library/react';
+import renderPageCode from './renderPageCode';
+import * as appDom from './appDom';
+
+const TOOLPAD_NS = 'toolpad';
+
+// Bundles a Toolpad page into a single JS file
+async function bundle(files: Record<string, string>, entry: string): Promise<string> {
+  const result = await esbuild.build({
+    entryPoints: [entry],
+    jsx: 'transform',
+    write: false,
+    format: 'iife',
+    bundle: true,
+    globalName: 'exports',
+    external: ['react', 'react-dom'],
+    plugins: [
+      {
+        name: 'resolver',
+        setup(build) {
+          build.onResolve({ filter: /.*/ }, async (args) => {
+            if (args.kind === 'entry-point') {
+              return { namespace: TOOLPAD_NS, path: args.path };
+            }
+            if (args.namespace === TOOLPAD_NS) {
+              if (args.path.startsWith('.') || args.path.startsWith('/')) {
+                return { namespace: TOOLPAD_NS, path: args.path };
+              }
+            }
+            return {};
+          });
+
+          build.onLoad({ filter: /.*/, namespace: TOOLPAD_NS }, (args) => {
+            return { contents: files[args.path], loader: 'tsx', resolveDir: process.cwd() };
+          });
+        },
+      },
+    ],
+  });
+  return result.outputFiles[0].text;
+}
+
+describe('renderPageCode', () => {
+  test('Basic Toolpad dom rendering Typography component', async () => {
+    let dom = appDom.createDom();
+    const root = appDom.getNode(dom, dom.root, 'app');
+    const page = appDom.createNode(dom, 'page', {
+      name: 'Page',
+      attributes: {
+        title: appDom.createConst(''),
+        urlQuery: appDom.createConst({}),
+      },
+    });
+    dom = appDom.addNode(dom, page, root, 'pages');
+    const text = appDom.createNode(dom, 'element', {
+      attributes: { component: appDom.createConst('Typography') },
+      props: { children: { type: 'const', value: 'Hello World' } },
+    });
+    dom = appDom.addNode(dom, text, page, 'children');
+
+    const { code } = renderPageCode('app-123', dom, page.id);
+
+    const result = await bundle(
+      {
+        '/my-page.tsx': code,
+      },
+      '/my-page.tsx',
+    );
+    // eslint-disable-next-line no-eval
+    const { default: Page } = eval(`() => {
+      ${result};
+      return exports;
+    }`)();
+
+    const { getByText } = render(<Page />);
+    const typography = getByText('Hello World');
+    expect(typography).toHaveClass('MuiTypography-root');
+  });
+});

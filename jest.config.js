5816e49f0ebce8b7595dd32ca5e7ab7579000bd2
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-04-08 14:28:50 +0200

    or this

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,9 @@ module.exports = {
   transform: {
     '^.+\\.(t|j)sx?$': '@swc/jest',
   },
+  transformIgnorePatterns: ['node_modules/(?!(@mui)/).*'],
   setupFilesAfterEnv: ['<rootDir>/test/utils/jest-setup.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 };

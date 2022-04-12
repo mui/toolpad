e5b2c03254fa40d40f0a084295809fbdd3858d21
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-04-12 13:08:11 +0200

    can be false

--- packages/toolpad-app/editorRuntime/canvas.tsx
+++ packages/toolpad-app/editorRuntime/canvas.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
-import { match as pathToRegexpMatchh } from 'path-to-regexp';
+import { match as pathToRegexpMatch } from 'path-to-regexp';
 import EditorCanvas from '../src/components/EditorCanvas';
 import { APP_ROOT_ID } from '../src/constants';
 
 const EDITOR_BASENAME = '/editor-canvas';
 const { pathname } = new URL(window.location.href);
-const match = pathToRegexpMatchh('/editor-canvas/:appId/:path*', { decode: decodeURIComponent });
-const { params } = match(pathname);
+const match = pathToRegexpMatch<{ appId: string | undefined }>('/editor-canvas/:appId/:path*', {
+  decode: decodeURIComponent,
+});
+const result = match(pathname);
 
-if (!params.appId) {
+if (!result || !result.params.appId) {
   throw new Error(`Can't find appId in url`);
 }
 const editorPathname = pathname.slice(EDITOR_BASENAME.length + 1);

1a1bc434dee8adedbd61d8aa6c563915d1d46934
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-04-13 10:15:04 +0200

    Add explainer

--- packages/toolpad-app/runtime/core/index.tsx
+++ packages/toolpad-app/runtime/core/index.tsx
@@ -1,1 +1,6 @@
+/*
+This module re-exports everything from the @mui/toolpad-core. We will use it in the importmap.
+This way it reloads changes without requiring us to rerun ./scripts/esInstall.ts
+*/
 export * from '@mui/toolpad-core';
+export { default } from '@mui/toolpad-core';

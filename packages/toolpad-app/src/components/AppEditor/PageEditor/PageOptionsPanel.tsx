80ea1108b7f8d516dd3c129505b7c3594a66793d
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-04-19 11:40:06 +0200

    Remove old code rendering code (#287)
    * WIP
    
    * use same defaults in classic
    
    * theme support
    
    * support query state
    
    * appId
    
    * skip test
    
    * resolve params
    
    * update pagestate
    
    * upgrade node
    
    * try this
    
    * or this
    
    * try with latest jest
    
    * so done with this
    
    * remove config circuit breaker
    
    * revert
    
    * Add router
    
    * not needed
    
    * tweak
    
    * refactor a bit
    
    * start editor experience
    
    * don't need those anymore
    
    * support url searchparams
    
    * fix page state
    
    * hookable componnets
    
    * invariant test
    
    * pull components into context
    
    * ComponentRenderer
    
    * rendertoolpadcomponents
    
    * merge
    
    * start comm channel
    
    * add types
    
    * khfsd
    
    * fix type
    
    * Slots implementation
    
    * Replace PageView
    
    * Image
    
    * next step
    
    * can be false
    
    * aftermerge
    
    * next step
    
    * subroute pages
    
    * fix route
    
    * prod urls
    
    * component importer
    
    * ts ignore
    
    * clean dom before inlining
    
    * safer implementation
    
    * bring appId in
    
    * update components code
    
    * refactor toolpadComponents
    
    * code componnets, almost
    
    * versioning
    
    * fix type
    
    * unnecessary comments
    
    * fix that
    
    * add a comment
    
    * extract theme stuff
    
    * remove some logs
    
    * refactor theme
    
    * rename
    
    * Update name
    
    * code components runtime
    
    * rename
    
    * Remove all unused code rendering code
    
    * remove tis too

--- packages/toolpad-app/src/components/AppEditor/PageEditor/PageOptionsPanel.tsx
+++ packages/toolpad-app/src/components/AppEditor/PageEditor/PageOptionsPanel.tsx
@@ -1,47 +1,16 @@
-import {
-  Stack,
-  Button,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  Switch,
-  FormControlLabel,
-  Typography,
-  Divider,
-  DialogActions,
-} from '@mui/material';
+import { Stack, Button, Typography, Divider } from '@mui/material';
 import * as React from 'react';
 import PageIcon from '@mui/icons-material/Web';
-import SourceIcon from '@mui/icons-material/Source';
-import renderPageCode from '../../../renderPageCode';
 import { useDom } from '../../DomLoader';
 import { usePageEditorState } from './PageEditorProvider';
 import DerivedStateEditor from './DerivedStateEditor';
 import QueryStateEditor from './QueryStateEditor';
 import UrlQueryEditor from './UrlQueryEditor';
-import { NodeId } from '../../../types';
 import NodeNameEditor from '../NodeNameEditor';
 import * as appDom from '../../../appDom';
 
 // TODO: remove deprecated state
 const DEPRECATED = false;
-
-interface PageSourceProps {
-  appId: string;
-  pageNodeId: NodeId;
-  editor?: boolean;
-}
-
-function PageSource({ appId, pageNodeId, editor }: PageSourceProps) {
-  const dom = useDom();
-
-  const source = React.useMemo(() => {
-    const { code } = renderPageCode(appId, dom, pageNodeId, { pretty: true, editor });
-    return code;
-  }, [appId, dom, editor, pageNodeId]);
-
-  return <pre>{source}</pre>;
-}
 
 export default function PageOptionsPanel() {
   const state = usePageEditorState();
@@ -49,9 +18,6 @@ export default function PageOptionsPanel() {
   const dom = useDom();
 
   const page = appDom.getNode(dom, pageNodeId, 'page');
-
-  const [dialogOpen, setDialogOpen] = React.useState(false);
-  const [debugEditor, setDebugEditor] = React.useState(false);
 
   return (
     <div>
@@ -66,35 +32,12 @@ export default function PageOptionsPanel() {
         >
           Preview
         </Button>
-        <Button startIcon={<SourceIcon />} color="inherit" onClick={() => setDialogOpen(true)}>
-          View Source
-        </Button>
         <Divider variant="middle" sx={{ alignSelf: 'stretch' }} />
         <Typography variant="subtitle1">Page State:</Typography>
         <UrlQueryEditor pageNodeId={pageNodeId} />
         {DEPRECATED && <DerivedStateEditor />}
         <QueryStateEditor />
       </Stack>
-      <Dialog fullWidth maxWidth="lg" open={dialogOpen} onClose={() => setDialogOpen(false)}>
-        <DialogTitle>Page component</DialogTitle>
-        <DialogContent>
-          <FormControlLabel
-            control={
-              <Switch
-                checked={debugEditor}
-                onChange={(event) => setDebugEditor(event.target.checked)}
-              />
-            }
-            label="editor"
-          />
-          <PageSource pageNodeId={pageNodeId} editor={debugEditor} appId={state.appId} />
-        </DialogContent>
-        <DialogActions>
-          <Button color="inherit" variant="text" onClick={() => setDialogOpen(false)}>
-            Close
-          </Button>
-        </DialogActions>
-      </Dialog>
     </div>
   );
 }

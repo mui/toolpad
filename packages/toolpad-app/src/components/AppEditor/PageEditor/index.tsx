6c29a7be74f569d9cda7947d9f36d7741931e79d
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-04-19 12:32:35 +0200

    wip

--- packages/toolpad-app/src/components/AppEditor/PageEditor/index.tsx
+++ packages/toolpad-app/src/components/AppEditor/PageEditor/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { styled } from '@mui/material';
+import { styled, Box } from '@mui/material';
 import { useParams } from 'react-router-dom';
+import SplitPane from 'react-split-pane';
 import RenderPanel from './RenderPanel';
 import ComponentPanel from './ComponentPanel';
 import { PageEditorProvider } from './PageEditorProvider';
@@ -12,13 +13,18 @@ import NotFoundEditor from '../NotFoundEditor';
 
 const classes = {
   componentPanel: 'Toolpad_ComponentPanel',
+  pageContentPanel: 'Toolpad_PageContentPanel',
   renderPanel: 'Toolpad_RenderPanel',
 };
 
 const PageEditorRoot = styled('div')(({ theme }) => ({
+  position: 'relative',
   display: 'flex',
   flexDirection: 'row',
   overflow: 'hidden',
+  [`& .${classes.pageContentPanel}`]: {
+    flex: 1,
+  },
   [`& .${classes.renderPanel}`]: {
     flex: 1,
   },
@@ -40,8 +46,22 @@ export default function PageEditor({ appId, className }: PageEditorProps) {
   return pageNode ? (
     <PageEditorProvider key={nodeId} appId={appId} nodeId={nodeId as NodeId}>
       <PageEditorRoot className={className}>
-        <ComponentCatalog />
-        <RenderPanel className={classes.renderPanel} />
+        <Box sx={{ flex: 1, position: 'relative' }}>
+          <SplitPane
+            split="horizontal"
+            allowResize
+            defaultSize="80%"
+            paneStyle={{
+              width: '100%',
+            }}
+          >
+            <Box sx={{ display: 'flex', flexDirection: 'row', width: '100%', flex: 1 }}>
+              <ComponentCatalog />
+              <RenderPanel className={classes.renderPanel} />
+            </Box>
+            <Box />
+          </SplitPane>
+        </Box>
         <ComponentPanel className={classes.componentPanel} />
       </PageEditorRoot>
     </PageEditorProvider>

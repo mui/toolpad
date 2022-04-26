0081f14f1a254530b5b8b9170fcea1e015d79ec6
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-04-26 11:25:59 +0200

    Remove jsdocs rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ const baseline = require('@mui/monorepo/.eslintrc');
 
 module.exports = {
   ...baseline,
-  plugins: [...baseline.plugins, 'jsdoc'],
   /**
    * Sorted alphanumerically within each group. built-in and each plugin form
    * their own groups.
@@ -12,15 +11,6 @@ module.exports = {
     'import/prefer-default-export': ['off'],
     // TODO move rule into the main repo once it has upgraded
     '@typescript-eslint/return-await': ['off'],
-    // TODO move rule into main repo to allow deep @mui/monorepo imports
-    'no-restricted-imports': ['off'],
-    'jsdoc/require-param': ['error', { contexts: ['TSFunctionType'] }],
-    'jsdoc/require-param-type': ['error', { contexts: ['TSFunctionType'] }],
-    'jsdoc/require-param-name': ['error', { contexts: ['TSFunctionType'] }],
-    'jsdoc/require-param-description': ['error', { contexts: ['TSFunctionType'] }],
-    'jsdoc/require-returns': ['error', { contexts: ['TSFunctionType'] }],
-    'jsdoc/require-returns-type': ['error', { contexts: ['TSFunctionType'] }],
-    'jsdoc/require-returns-description': ['error', { contexts: ['TSFunctionType'] }],
     // TODO: resolve these, non critical for pathfinder prototype
     'no-alert': ['off'],
     'no-console': ['off'],

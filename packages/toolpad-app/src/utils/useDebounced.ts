16853740f36236c5af43c4b1d2f19c1201e3dc03
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-04-26 15:13:11 +0200

    The effect on unmount is unnecesary (#317)
    * The effect on unmount is unnecesary as clearTimeout already runs on the other effect's cleanup
    
    * immediately update when allowed
    
    * set update time always

--- packages/toolpad-app/src/utils/useDebounced.ts
+++ packages/toolpad-app/src/utils/useDebounced.ts
@@ -3,15 +3,10 @@ import * as React from 'react';
 export default function useDebounced<T>(value: T, delay: number): T {
   const [debouncedValue, setDebouncedValue] = React.useState(value);
 
-  const timeoutRef = React.useRef<NodeJS.Timeout>();
-
   React.useEffect(() => {
     const timeoutId = setTimeout(() => setDebouncedValue(value), delay);
-    timeoutRef.current = timeoutId;
     return () => clearTimeout(timeoutId);
   }, [value, delay]);
-
-  React.useEffect(() => () => timeoutRef.current && clearTimeout(timeoutRef.current), []);
 
   return debouncedValue;
 }

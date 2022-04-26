3126bec09a443fc9b0a7866925a7db9f5553a2c7
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-04-26 11:03:59 +0200

    set update time always

--- packages/toolpad-app/src/utils/useThottled.ts
+++ packages/toolpad-app/src/utils/useThottled.ts
@@ -4,20 +4,24 @@ export default function useThrottled<T>(value: T, throttle: number): T {
   const [throttledValue, setThrottledValue] = React.useState(value);
   const lastUpdate = React.useRef(-Infinity);
 
+  const updateThrottledValue = React.useCallback((newValue: T) => {
+    lastUpdate.current = Date.now();
+    setThrottledValue(newValue);
+  }, []);
+
   React.useEffect(() => {
     const now = Date.now();
     const elapsed = now - lastUpdate.current;
 
     if (elapsed > throttle) {
-      lastUpdate.current = now;
-      setThrottledValue(value);
+      updateThrottledValue(value);
       return () => {};
     }
 
     const delay = throttle - elapsed;
-    const timeoutId = setTimeout(() => setThrottledValue(value), delay);
+    const timeoutId = setTimeout(() => updateThrottledValue(value), delay);
     return () => clearTimeout(timeoutId);
-  }, [value, throttle]);
+  }, [value, throttle, updateThrottledValue]);
 
   const now = Date.now();
   const elapsed = now - lastUpdate.current;

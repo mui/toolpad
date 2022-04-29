09d6e4a3fa499dba238cb70f08a60a41cb5800b7
Bharat Kashyap <bharatkashyap@outlook.com>
2022-04-29 21:32:13 +0530

    add `rowIdField` prop

---
+++ packages/toolpad-components/src/utils/useDebounced.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export default function useDebounced<T>(value: T, delay: number): T {
+  const [debouncedValue, setDebouncedValue] = React.useState(() => value);
+
+  React.useEffect(() => {
+    const timeoutId = setTimeout(() => setDebouncedValue(() => value), delay);
+    return () => clearTimeout(timeoutId);
+  }, [value, delay]);
+
+  return debouncedValue;
+}

62c66c1561cff7a311449430a9f9a1607ac2223b
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-05-03 18:14:18 +0200

    error testing

--- packages/toolpad-app/runtime/pageEditor/ToolpadApp.spec.tsx
+++ packages/toolpad-app/runtime/pageEditor/ToolpadApp.spec.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { render, waitFor, screen, fireEvent, act } from '@testing-library/react';
+import { render, waitFor as waitForOrig, screen, fireEvent, act } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { LiveBindings } from '@mui/toolpad-core';
 import ToolpadApp from './ToolpadApp';
 import * as appDom from '../../src/appDom';
 import { getToolpadComponents } from '../../src/toolpadComponents';
+import { setEventHandler } from '../coreRuntime';
+
+// More sensible default for these tests
+const waitFor: typeof waitForOrig = (waiter, options) =>
+  waitForOrig(waiter, { timeout: 5000, ...options });
 
 function renderPage(initPage: (dom: appDom.AppDom, page: appDom.PageNode) => appDom.AppDom) {
   const appId = '12345';
@@ -48,7 +54,7 @@ test(`Static Text`, async () => {
     return dom;
   });
 
-  const text = await waitFor(() => screen.getByText('Hello World'), { timeout: 10000 });
+  const text = await waitFor(() => screen.getByText('Hello World'));
   expect(text).toHaveClass('MuiTypography-root');
 });
 
@@ -63,7 +69,7 @@ test(`Default Text`, async () => {
     return dom;
   });
 
-  const text = await waitFor(() => screen.getByText('Text'), { timeout: 10000 });
+  const text = await waitFor(() => screen.getByText('Text'));
   expect(text).toHaveClass('MuiTypography-root');
 });
 
@@ -88,7 +94,7 @@ test(`simple databinding`, async () => {
     return dom;
   });
 
-  const text = await waitFor(() => screen.getByText('Default Text'), { timeout: 10000 });
+  const text = await waitFor(() => screen.getByText('Default Text'));
   const textField = screen.getByLabelText('The Input');
 
   act(() => {
@@ -99,34 +105,81 @@ test(`simple databinding`, async () => {
   expect(text).toHaveTextContent('Hello Everybody');
 });
 
-test(`simple databinding`, async () => {
-  renderPage((dom, page) => {
-    const textField = appDom.createNode(dom, 'element', {
-      name: 'theTextInput',
-      attributes: { component: appDom.createConst('TextField') },
-      props: {
-        label: appDom.createConst('The Input'),
-        value: appDom.createConst('Default Text'),
-      },
-    });
-    dom = appDom.addNode(dom, textField, page, 'children');
+test(`Databinding errors`, async () => {
+  let bindings: LiveBindings | undefined;
 
-    const text = appDom.createNode(dom, 'element', {
-      attributes: { component: appDom.createConst('Typography') },
-      props: { value: { type: 'jsExpression', value: 'theTextInput.value' } },
-    });
-    dom = appDom.addNode(dom, text, page, 'children');
-
-    return dom;
+  const cleanup = setEventHandler(window, (event) => {
+    if (event.type === 'pageBindingsUpdated') {
+      bindings = event.bindings;
+    }
   });
 
-  const text = await waitFor(() => screen.getByText('Default Text'), { timeout: 10000 });
-  const textField = screen.getByLabelText('The Input');
+  try {
+    let nonExisting: appDom.ElementNode;
+    let selfReferencing: appDom.ElementNode;
+    let cyclic1: appDom.ElementNode;
+    let cyclic2: appDom.ElementNode;
+    renderPage((dom, page) => {
+      dom = appDom.addNode(
+        dom,
+        appDom.createNode(dom, 'element', {
+          attributes: { component: appDom.createConst('Typography') },
+          props: { value: appDom.createConst('Marker') },
+        }),
+        page,
+        'children',
+      );
 
-  act(() => {
-    textField.focus();
-    fireEvent.change(textField, { target: { value: 'Hello Everybody' } });
-  });
+      nonExisting = appDom.createNode(dom, 'element', {
+        attributes: { component: appDom.createConst('Typography') },
+        props: { value: { type: 'jsExpression', value: 'nonExisting.foo' } },
+      });
+      dom = appDom.addNode(dom, nonExisting, page, 'children');
 
-  expect(text).toHaveTextContent('Hello Everybody');
+      selfReferencing = appDom.createNode(dom, 'element', {
+        name: 'selfReferencing',
+        attributes: { component: appDom.createConst('Typography') },
+        props: { value: { type: 'jsExpression', value: 'selfReferencing.value' } },
+      });
+      dom = appDom.addNode(dom, selfReferencing, page, 'children');
+
+      cyclic1 = appDom.createNode(dom, 'element', {
+        name: 'cyclic1',
+        attributes: { component: appDom.createConst('Typography') },
+        props: { value: { type: 'jsExpression', value: 'cyclic2.value' } },
+      });
+      dom = appDom.addNode(dom, cyclic1, page, 'children');
+
+      cyclic2 = appDom.createNode(dom, 'element', {
+        name: 'cyclic2',
+        attributes: { component: appDom.createConst('Typography') },
+        props: { value: { type: 'jsExpression', value: 'cyclic1.value' } },
+      });
+      dom = appDom.addNode(dom, cyclic2, page, 'children');
+
+      return dom;
+    });
+
+    await waitFor(() => screen.getByText('Marker'));
+    expect(bindings).toBeDefined();
+    expect(bindings![`${nonExisting!.id}.props.value`]?.error).toHaveProperty(
+      'message',
+      'nonExisting is not defined',
+    );
+    expect(bindings![`${selfReferencing!.id}.props.value`]?.error).toHaveProperty(
+      'message',
+      'Cycle detected "selfReferencing.value"',
+    );
+    expect(bindings![`${cyclic1!.id}.props.value`]?.error).toHaveProperty(
+      'message',
+      'Cycle detected "cyclic1.value"',
+    );
+    expect(bindings![`${cyclic2!.id}.props.value`]?.error).toHaveProperty(
+      'message',
+      'Cycle detected "cyclic1.value"',
+    );
+    // console.log(bindings);
+  } finally {
+    cleanup();
+  }
 });

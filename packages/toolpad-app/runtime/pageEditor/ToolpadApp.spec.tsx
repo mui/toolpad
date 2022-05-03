ca95b2560662deba1e0dd181d44260967458ac14
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-05-03 17:03:38 +0200

    add input test

--- packages/toolpad-app/runtime/pageEditor/ToolpadApp.spec.tsx
+++ packages/toolpad-app/runtime/pageEditor/ToolpadApp.spec.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { render, waitFor, screen } from '@testing-library/react';
+import { render, waitFor, screen, fireEvent, act } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import ToolpadApp from './ToolpadApp';
 import * as appDom from '../../src/appDom';
 import { getToolpadComponents } from '../../src/toolpadComponents';
 
-test(`simple databinding`, async () => {
+test(`Static Text`, async () => {
   const appId = '12345';
   const version = 'preview';
 
@@ -18,12 +18,15 @@ test(`simple databinding`, async () => {
       urlQuery: appDom.createConst({}),
     },
   });
-  dom = appDom.addNode(dom, page, root, 'pages');
-  const text = appDom.createNode(dom, 'element', {
-    attributes: { component: appDom.createConst('Typography') },
-    props: { value: { type: 'const', value: 'Hello World' } },
-  });
-  dom = appDom.addNode(dom, text, page, 'children');
+
+  {
+    dom = appDom.addNode(dom, page, root, 'pages');
+    const text = appDom.createNode(dom, 'element', {
+      attributes: { component: appDom.createConst('Typography') },
+      props: { value: appDom.createConst('Hello World') },
+    });
+    dom = appDom.addNode(dom, text, page, 'children');
+  }
 
   const components = getToolpadComponents(appId, version, dom);
 
@@ -39,10 +42,62 @@ test(`simple databinding`, async () => {
     />,
   );
 
-  const typography = await waitFor(() => screen.getByText('Hello World'), {
-    interval: 1000,
-    timeout: 60000,
+  const text = await waitFor(() => screen.getByText('Hello World'));
+
+  expect(text).toHaveClass('MuiTypography-root');
+});
+
+test(`simple databinding`, async () => {
+  const appId = '12345';
+  const version = 'preview';
+
+  let dom = appDom.createDom();
+  const root = appDom.getNode(dom, dom.root, 'app');
+  const page = appDom.createNode(dom, 'page', {
+    name: 'Page',
+    attributes: {
+      title: appDom.createConst(''),
+      urlQuery: appDom.createConst({}),
+    },
   });
 
-  expect(typography).toHaveClass('MuiTypography-root');
+  {
+    dom = appDom.addNode(dom, page, root, 'pages');
+    const textField = appDom.createNode(dom, 'element', {
+      name: 'theTextInput',
+      attributes: { component: appDom.createConst('TextField') },
+      props: { label: appDom.createConst('The Input') },
+    });
+    dom = appDom.addNode(dom, textField, page, 'children');
+    const text = appDom.createNode(dom, 'element', {
+      attributes: { component: appDom.createConst('Typography') },
+      props: { value: { type: 'jsExpression', value: 'theTextInput.value' } },
+    });
+    dom = appDom.addNode(dom, text, page, 'children');
+  }
+
+  const components = getToolpadComponents(appId, version, dom);
+
+  window.history.replaceState({}, 'Test page', `/toolpad/pages/${page.id}`);
+
+  render(
+    <ToolpadApp
+      appId={appId}
+      version={version}
+      basename="toolpad"
+      dom={dom}
+      components={components}
+    />,
+  );
+
+  const textField = await waitFor(() => screen.getByLabelText('The Input'));
+
+  act(() => {
+    textField.focus();
+    fireEvent.change(textField, { target: { value: 'Hello Everybody' } });
+  });
+
+  const text = await waitFor(() => screen.getByText('Hello Everybody'));
+
+  expect(text).toHaveClass('MuiTypography-root');
 });

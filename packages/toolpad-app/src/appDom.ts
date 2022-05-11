ee5bce4b1ee4c8770198d1540672483ccc333c1d
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-05-11 18:46:58 +0200

    only save certain props

--- packages/toolpad-app/src/appDom.ts
+++ packages/toolpad-app/src/appDom.ts
@@ -702,7 +702,7 @@ export function moveNode(
 export function saveNode(dom: AppDom, node: AppDomNode) {
   return update(dom, {
     nodes: update(dom.nodes, {
-      [node.id]: node,
+      [node.id]: update(dom.nodes[node.id], omit(node, ...RESERVED_NODE_PROPERTIES)),
     }),
   });
 }

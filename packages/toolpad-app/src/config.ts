e69658fbdd101522564e161be94a4ecbfcd69527
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-05-19 13:53:10 +0200

    Add some documentation

--- packages/toolpad-app/src/config.ts
+++ packages/toolpad-app/src/config.ts
@@ -1,5 +1,26 @@
 import { RUNTIME_CONFIG_WINDOW_PROPERTY } from './constants';
 
+/**
+ * There are three ways of configuring toolpad:
+ *
+ * 1. Build time:
+ *    These can be used both serverside and browserside.
+ *    They are inlined at build time. Use when you know the values at build time.
+ *    They can be used to tree-shake code at build time
+ *      - Configure in next.config.js `env` option
+ *      - Use as process.env.MY_VARIABLE
+ *
+ * 2. Runtime, public:
+ *    These can be used both on the server and in the browser
+ *      - Configure in this file in the default export
+ *      - Use by importing this module (`./src/config`)
+ *
+ * 3. Runtime, private:
+ *    These can be used both on the server only, they are suitable for secrets
+ *      - Configure in the file `./src/server.config`
+ *      - Use by importing the module `./src/server.config`
+ */
+
 type ToolpadTargetType = 'CLOUD' | 'CE' | 'PRO';
 
 // These are inlined at build time
@@ -38,8 +59,9 @@ function getBrowsersideRuntimeConfig(): RuntimeConfig {
 export const runtimeConfig: RuntimeConfig =
   typeof window === 'undefined'
     ? {
+        // Define runtime config here
         foo: process.env.TOOLPAD_FOO,
       }
     : getBrowsersideRuntimeConfig();
 
-export default runtimeConfig as RuntimeConfig;
+export default runtimeConfig;

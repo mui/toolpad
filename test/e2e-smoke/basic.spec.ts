37cc7ad7e643a3e0fd383edc954ee4aa2d217446
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-05-20 16:47:08 +0200

    like dis

--- test/e2e-smoke/basic.spec.ts
+++ test/e2e-smoke/basic.spec.ts
@@ -2,7 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
   await page.goto('/');
-  console.log(await page.content());
   const brand = page.locator('data-test-id=brand');
   await expect(brand).toHaveText('MUI Toolpad');
 });

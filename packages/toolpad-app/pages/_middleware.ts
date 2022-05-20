08e4931d2e31886d82055a0cd2b63dcbed142233
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-05-20 17:40:26 +0200

    Get rid of next.js custom server

---
+++ packages/toolpad-app/pages/_middleware.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from 'next/server';
+import config from '../src/server/config';
+
+export function middleware(req: NextRequest) {
+  if (!config.basicAuthUser) {
+    return NextResponse.next();
+  }
+
+  const basicAuth = req.headers.get('authorization');
+
+  if (basicAuth) {
+    const auth = basicAuth.split(' ')[1];
+    const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':');
+
+    if (user === config.basicAuthUser && pwd === config.basicAuthPassword) {
+      return NextResponse.next();
+    }
+  }
+
+  return new Response('Auth required', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  });
+}

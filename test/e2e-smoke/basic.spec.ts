9925d64467556b33ba48b358b355e2171071e903
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-05-22 13:41:39 +0200

    update test

--- test/e2e-smoke/basic.spec.ts
+++ test/e2e-smoke/basic.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('basic test', async ({ page }) => {
   await page.goto('/');
   const brand = page.locator('data-test-id=brand');
-  await expect(brand).toHaveText('MUI Toolpad');
+  await expect(brand).toHaveText('MUI Toolpad CE');
 });

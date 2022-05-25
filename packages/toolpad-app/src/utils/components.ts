792a78338abe25ca4435d1755b7bca284bf8d529
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2022-05-25 14:22:25 +0100

    Run prettier

--- packages/toolpad-app/src/utils/components.ts
+++ packages/toolpad-app/src/utils/components.ts
@@ -1,9 +1,8 @@
-import type * as appDom from "../appDom";
-
+import type * as appDom from '../appDom';
 
 export const PAGE_ROW_COMPONENT_ID = 'PageRow';
 export const PAGE_COLUMN_COMPONENT_ID = 'PageColumn';
 
 export function getElementNodeComponentId(elementNode: appDom.ElementNode): string {
-    return elementNode.attributes.component.value
+  return elementNode.attributes.component.value;
 }

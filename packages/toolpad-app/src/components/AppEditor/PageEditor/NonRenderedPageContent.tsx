f5e8fd2e4aaecd9a0eb299aa11f804b6ce2e3396
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-06-09 12:47:05 +0200

    proper merge

--- packages/toolpad-app/src/components/AppEditor/PageEditor/NonRenderedPageContent.tsx
+++ packages/toolpad-app/src/components/AppEditor/PageEditor/NonRenderedPageContent.tsx
@@ -1,19 +1,14 @@
 import { Box, Typography } from '@mui/material';
 import * as React from 'react';
-import DerivedStateEditor from './DerivedStateEditor';
-import QueryStateEditor from './QueryStateEditor';
+import QueryEditor from './QueryEditor';
 import UrlQueryEditor from './UrlQueryEditor';
-
-// TODO: remove deprecated state
-const DEPRECATED = false;
 
 export default function NonRenderedPageContent() {
   return (
     <Box>
       <Typography variant="subtitle1">Page State:</Typography>
       <UrlQueryEditor />
-      {DEPRECATED && <DerivedStateEditor />}
-      <QueryStateEditor />
+      <QueryEditor />
     </Box>
   );
 }

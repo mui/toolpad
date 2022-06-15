9cc9eb839ccd79b1c2c64dd06fecd27554346d7a
Bharat Kashyap <bharatkashyap@outlook.com>
2022-06-15 18:42:19 +0200

    Rename to allow re-use

--- packages/toolpad-app/src/toolpadDataSources/context.tsx
+++ packages/toolpad-app/src/toolpadDataSources/context.tsx
@@ -3,18 +3,18 @@ import { NodeId } from '@mui/toolpad-core';
 import { createProvidedContext } from '../utils/react';
 import client from '../api';
 
-export interface QueryEditorContext {
+export interface ConnectionContext {
   appId: string;
   connectionId: NodeId;
 }
 
-const [useQueryEditorContext, QueryEditorContextProvider] =
-  createProvidedContext<QueryEditorContext>('QueryEditor');
+const [useConnectionContext, ConnectionContextProvider] =
+  createProvidedContext<ConnectionContext>('QueryEditor');
 
-export { useQueryEditorContext, QueryEditorContextProvider };
+export { useConnectionContext, ConnectionContextProvider };
 
 export function usePrivateQuery<Q = unknown, R = unknown>(query: Q | null): UseQueryResult<R> {
-  const { appId, connectionId } = useQueryEditorContext();
+  const { appId, connectionId } = useConnectionContext();
   return client.useQuery(
     'dataSourceFetchPrivate',
     query == null ? null : [appId, connectionId, query],

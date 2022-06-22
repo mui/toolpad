4c16fda2c3cb3423f3acdd845dd6be48539f705b
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-06-22 12:55:47 +0200

    default tab size

--- packages/toolpad-app/src/components/TsModuleEditor.tsx
+++ packages/toolpad-app/src/components/TsModuleEditor.tsx
@@ -24,6 +24,7 @@ export default function TsModuleEditor({ path, extraLibs, value, onChange }: TsM
       editor.updateOptions({
         minimap: { enabled: false },
         accessibilitySupport: 'off',
+        tabSize: 2,
       });
 
       monaco.languages.typescript.typescriptDefaults.setCompilerOptions({

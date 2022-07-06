506d8047abeb664854f9d17b6fc3addf4187066c
Bharat Kashyap <bharatkashyap@outlook.com>
2022-07-06 17:38:35 +0530

    add toolpad specific docs routes

--- docs/src/components/landing/ToolpadHero.js
+++ docs/src/components/landing/ToolpadHero.js
@@ -5,6 +5,7 @@ import GradientText from 'docs/src/components/typography/GradientText';
 import GetStartedButtons from 'docs/src/components/home/GetStartedButtons';
 import HeroContainer from 'docs/src/layouts/HeroContainer';
 import IconImage from 'docs/src/components/icon/IconImage';
+import ROUTES from '../../route';
 
 export default function ToolpadHero() {
   return (
@@ -35,7 +36,7 @@ export default function ToolpadHero() {
           </Typography>
           <GetStartedButtons
             installation="install intructions here"
-            to={'/toolpad/getting-started'}
+            to={ROUTES.toolpadDocs}
             sx={{ justifyContent: { xs: 'center', md: 'flex-start' } }}
           />
         </Box>
---
+++ docs/src/route.ts
@@ -0,0 +1,5 @@
+const ROUTES = {
+  toolpadDocs: '/toolpad/getting-started',
+};
+
+export default ROUTES;

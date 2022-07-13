27d10e4658792090aa341101cf24b34d6ff50106
Bharat Kashyap <bharatkashyap@outlook.com>
2022-07-13 18:52:05 +0530

    Landing page update

--- docs/src/components/landing/ToolpadHero.js
+++ docs/src/components/landing/ToolpadHero.js
@@ -2,50 +2,36 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import GradientText from 'docs/src/components/typography/GradientText';
-import GetStartedButtons from 'docs/src/components/home/GetStartedButtons';
-import HeroContainer from 'docs/src/layouts/HeroContainer';
 import IconImage from 'docs/src/components/icon/IconImage';
-import ROUTES from '../../route';
+import ToolpadHeroContainer from '../../layouts/ToolpadHeroContainer.tsx';
 
 export default function ToolpadHero() {
   return (
-    <HeroContainer
-      left={
-        <Box sx={{ textAlign: { xs: 'center', md: 'left' } }}>
-          <Typography
-            fontWeight="bold"
-            variant="body2"
-            color={(theme) => (theme.palette.mode === 'dark' ? 'primary.400' : 'primary.600')}
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: { xs: 'center', md: 'flex-start' },
-              '& > *': { mr: 1, width: 28, height: 28 },
-            }}
-          >
-            <IconImage name="product-toolpad" /> MUI Toolpad
-          </Typography>
-          <Typography variant="h1" sx={{ my: 2, maxWidth: 500 }}>
-            Toolpad
-            <br />
-            <GradientText>title</GradientText>
-            <br /> here
-          </Typography>
-          <Typography color="text.secondary" sx={{ mb: 3, maxWidth: 500 }}>
-            toolpad intro text
-          </Typography>
-          <GetStartedButtons
-            installation="install intructions here"
-            to={ROUTES.toolpadDocs}
-            sx={{ justifyContent: { xs: 'center', md: 'flex-start' } }}
-          />
-        </Box>
-      }
-      rightSx={{
-        p: { md: 2, lg: 3, xl: 4 },
-        overflow: 'hidden', // the components on the Hero section are mostly illustrative, even though they're interactive. That's why scrolling is disabled.
-      }}
-      right={<div>content here</div>}
-    />
+    <ToolpadHeroContainer>
+      <Box sx={{ textAlign: { xs: 'center', md: 'center' } }}>
+        <Typography
+          fontWeight="bold"
+          variant="body2"
+          color={(theme) => (theme.palette.mode === 'dark' ? 'primary.400' : 'primary.600')}
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: { xs: 'center', md: 'center' },
+            '& > *': { mr: 1, width: 28, height: 28 },
+          }}
+        >
+          <IconImage name="product-toolpad" /> Toolpad
+        </Typography>
+        <Typography variant="h1" sx={{ my: 2 }}>
+          Low-code
+          <br />
+          <GradientText>admin builder</GradientText>
+        </Typography>
+        <Typography color="text.secondary" sx={{ mb: 3, maxWidth: 500, mx: 'auto' }}>
+          Drag and drop pre-built components, connect to data sources, APIs and build your internal
+          tools 10x faster. Open-source and powered by MUI.
+        </Typography>
+      </Box>
+    </ToolpadHeroContainer>
   );
 }
---
+++ docs/src/layouts/ToolpadHeroContainer.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+
+export default function HeroContainer({ children }: { children: React.ReactNode }) {
+  return (
+    <Box sx={{ overflow: 'hidden' }}>
+      <Container
+        sx={{
+          minHeight: 500,
+          height: 'calc(100vh - 120px)',
+          maxHeight: { xs: 500, sm: 700, xl: 1000 },
+          transition: '0.3s',
+        }}
+      >
+        <Grid container alignItems="center" wrap="nowrap" sx={{ height: '100%', mx: 'auto' }}>
+          <Grid item md={12} lg={12} sx={{ m: 'auto' }}>
+            {children}
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+}

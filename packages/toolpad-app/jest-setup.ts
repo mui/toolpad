87037685f3e52a8c099d5bb1374b77144df85e3d
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-07-20 12:26:48 +0200

    Add fetch polyfills to jest

---
+++ packages/toolpad-app/jest-setup.ts
@@ -0,0 +1,30 @@
+import { TextDecoder, TextEncoder } from 'util';
+import fetch, { Headers, Request, Response } from 'node-fetch';
+
+if (!global.TextDecoder) {
+  // @ts-expect-error
+  global.TextDecoder = TextDecoder;
+} else {
+  // eslint-disable-next-line no-console
+  console.info('TextDecoder not polyfilled: trunning on a platform that supports TextDecoder');
+}
+
+if (!global.TextEncoder) {
+  global.TextEncoder = TextEncoder;
+} else {
+  // eslint-disable-next-line no-console
+  console.info('TextEncoder not polyfilled: trunning on a platform that supports TextEncoder');
+}
+
+if (!global.fetch) {
+  // @ts-expect-error
+  global.fetch = fetch;
+  global.Headers = Headers;
+  // @ts-expect-error
+  global.Request = Request;
+  // @ts-expect-error
+  global.Response = Response;
+} else {
+  // eslint-disable-next-line no-console
+  console.info('Fetch API not polyfilled: trunning on a platform that supports fetch');
+}

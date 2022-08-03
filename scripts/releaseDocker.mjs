59f2115617b7c90ed9620c12fdd8f7a9463f59ed
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-08-03 18:23:55 +0200

    pollzz

--- scripts/releaseDocker.mjs
+++ scripts/releaseDocker.mjs
@@ -8,7 +8,6 @@ import { Octokit } from '@octokit/rest';
 
 const RELEASE_DOCKER_WORKFLOW_ID = 31597666;
 const IMAGE_NAME = 'muicom/toolpad';
-const LATEST_TAG = 'latest';
 
 async function checkTagExists(image, tag) {
   try {
@@ -23,7 +22,13 @@ async function checkTagExists(image, tag) {
   }
 }
 
-async function createDockerTag(octokit, commit, dockerTag) {
+/**
+ * @param {import('@octokit/rest').Octokit} octokit
+ * @param {string} commit
+ * @param {string} version
+ * @param {boolean} latest
+ */
+async function createDockerTag(octokit, commit, version, latest) {
   await octokit.actions.createWorkflowDispatch({
     owner: 'mui',
     repo: 'mui-toolpad',
@@ -31,13 +36,10 @@ async function createDockerTag(octokit, commit, dockerTag) {
     workflow_id: RELEASE_DOCKER_WORKFLOW_ID,
     inputs: {
       commit,
-      docker_tag: dockerTag,
+      version,
+      latest: String(latest),
     },
   });
-}
-
-async function dockerCreateTags(octokit, commit, dockerTags) {
-  await Promise.all(dockerTags.map((dockerTag) => createDockerTag(octokit, commit, dockerTag)));
 }
 
 async function commitLog(count = 100) {
@@ -66,6 +68,22 @@ async function currentBranch() {
   const { stdout } = await execa('git', ['rev-parse', '--abbrev-ref', 'HEAD']);
   return stdout;
 }
+/**
+ * @param {import('@octokit/rest').Octokit} octokit
+ * @param {Date} after
+ */
+async function getWorkflowRuns(octokit, { status, after } = {}) {
+  const res = await octokit.actions.listWorkflowRuns({
+    owner: 'mui',
+    repo: 'mui-toolpad',
+    workflow_id: RELEASE_DOCKER_WORKFLOW_ID,
+    created: after ? `>${after.toISOString()}` : undefined,
+    event: 'workflow_dispatch',
+    status: status ? 'completed' : undefined,
+  });
+
+  return res.data.workflow_runs;
+}
 
 async function main({ yes, force, commit, releaseTag, noLatest, githubToken }) {
   if (!githubToken) {
@@ -78,7 +96,7 @@ async function main({ yes, force, commit, releaseTag, noLatest, githubToken }) {
     auth: githubToken,
   });
 
-  await dockerCreateTags(octokit, commit, ['test', 'test2']);
+  const [lastRunBefore] = await getWorkflowRuns(octokit);
 
   if (!commit) {
     const branch = await currentBranch();
@@ -135,10 +153,7 @@ async function main({ yes, force, commit, releaseTag, noLatest, githubToken }) {
   const isPrerelease = parsedVersion ? parsedVersion.prerelease.length > 0 : true;
 
   const tags = [releaseTag];
-
-  if (!noLatest && !isPrerelease) {
-    tags.push(LATEST_TAG);
-  }
+  const latest = !noLatest && !isPrerelease;
 
   if (!yes) {
     const baseImage = `${IMAGE_NAME}:${commit}`;
@@ -170,7 +185,31 @@ async function main({ yes, force, commit, releaseTag, noLatest, githubToken }) {
     yargs.exit(1, `Tag "${IMAGE_NAME}:${releaseTag}" already exists`);
   }
 
-  // await dockerCreateTags(IMAGE_NAME, commit, tags);
+  await createDockerTag(octokit, commit, releaseTag, latest);
+
+  // Best effort polling for completion
+  // See: https://stackoverflow.com/a/69500478
+  let completed = false;
+  let polls = 0;
+  while (!completed) {
+    polls += 1;
+    if (polls > 60) {
+      throw new Error(
+        `Timeout polling for workflow status, please check manually: https://github.com/mui/mui-toolpad/actions`,
+      );
+    }
+    // eslint-disable-next-line no-await-in-loop
+    await new Promise((r) => setTimeout(r, 2000));
+    const lastCreationDate = new Date(lastRunBefore?.created_at ?? 0);
+    // eslint-disable-next-line no-console
+    console.log('Checking for completion, please wait...');
+    // eslint-disable-next-line no-await-in-loop
+    const runs = await getWorkflowRuns(octokit, {
+      status: 'completed',
+      after: lastCreationDate,
+    });
+    completed = runs.length > 0;
+  }
 }
 
 yargs(hideBin(process.argv))

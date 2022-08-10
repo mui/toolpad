8a06572d774295bec43d614b110a7d8efe74e52a
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-08-10 15:01:17 +0200

    Make sure queries are default enabled

--- packages/toolpad-app/src/toolpad/AppEditor/PageEditor/QueryEditor.tsx
+++ packages/toolpad-app/src/toolpad/AppEditor/PageEditor/QueryEditor.tsx
@@ -561,7 +561,7 @@ function QueryNodeEditorDialog<Q, P>({
               server
               label="Enabled"
               propType={{ type: 'boolean' }}
-              value={input.attributes.enabled ?? null}
+              value={input.attributes.enabled ?? appDom.createConst(true)}
               onChange={handleEnabledChange}
             />
             <FormControlLabel

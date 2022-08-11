59b2283f55e48af55790e7297c539810ede08fbf
prakhargupta1 <prakhar@mui.com>
2022-08-11 17:41:32 +0530

    Minor text fix.

--- docs/src/content/upvote.js
+++ docs/src/content/upvote.js
@@ -1,7 +1,7 @@
 const upvote = {
   title: '👍 Upvote the features you want to get prioritized',
   description:
-    'Want to request a feature ? Head over to our repo and if not already listed, feel free to open a issue explaining the use case',
+    'Want to request a feature? Head over to our repo and if not already listed, feel free to open an issue explaining the use case',
   action: {
     href: '/',
     label: 'Upvote',

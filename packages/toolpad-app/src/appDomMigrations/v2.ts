52592abf9ff805588f7b842b458134ef9ea816c7
Bharat Kashyap <bharatkashyap@outlook.com>
2022-08-12 16:38:53 +0800

    new structure for migration

---
+++ packages/toolpad-app/src/appDomMigrations/v2.ts
@@ -0,0 +1,13 @@
+import invariant from 'invariant';
+import { AppDom } from '../appDom';
+
+export default {
+  up(dom: AppDom) {
+    invariant(dom.version === '1', 'Can only migrate dom of version 1');
+    return dom;
+  },
+  down(dom: AppDom) {
+    invariant(dom.version === '2', 'Can only migrate dom of version 2');
+    return dom;
+  },
+};

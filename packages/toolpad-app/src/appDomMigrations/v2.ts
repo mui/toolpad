19a9c44db02a935d7f79bf908eb784d84860ff43
Bharat Kashyap <bharatkashyap@outlook.com>
2022-08-16 17:40:30 +0800

    [appDom] use `number` for versions

--- packages/toolpad-app/src/appDomMigrations/v2.ts
+++ packages/toolpad-app/src/appDomMigrations/v2.ts
@@ -3,11 +3,11 @@ import { AppDom } from '../appDom';
 
 export default {
   up(dom: AppDom) {
-    invariant(dom.version === '1', 'Can only migrate dom of version 1');
+    invariant(dom.version === 1, 'Can only migrate dom of version 1');
     return dom;
   },
   down(dom: AppDom) {
-    invariant(dom.version === '2', 'Can only migrate dom of version 2');
+    invariant(dom.version === 2, 'Can only migrate dom of version 2');
     return dom;
   },
 };

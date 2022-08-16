58b155f88259dcd87c52beeab0b47b71e6c37bdd
Bharat Kashyap <bharatkashyap@outlook.com>
2022-08-16 16:16:03 +0800

    [docs] Add configured "overline"

--- docs/src/content/useCases.js
+++ docs/src/content/useCases.js
@@ -3,6 +3,7 @@ import Typography from '@mui/material/Typography';
 import GradientText from '../components/landing/GradientText';
 
 const useCases = {
+  overline: 'Use Cases',
   Headline: (
     <Typography variant="h2" maxWidth={300} marginBottom={3}>
       Toolpad is ideal for <GradientText>building</GradientText>
@@ -12,21 +13,21 @@ const useCases = {
     {
       title: 'Monitoring dashboard',
       wip: false,
-      imageUrl: '/static/toolpad/marketing/toolpad-monitoring-dashboard.png',
+      imageUrl: '/static/toolpad/marketing/toolpad-monitoring-dashboard.svg',
       description:
         'Quickly provide the latest numbers and status reports that fuel data-driven decisions. Build and share your dashboard by combining data from multiple sources with dependable version control to rollback changes when needed.',
     },
     {
       title: 'Admin panel',
       wip: false,
-      imageUrl: '/static/toolpad/marketing/toolpad-admin-panel.png',
+      imageUrl: '/static/toolpad/marketing/toolpad-admin-panel.svg',
       description:
         'Enable your teams to quickly view and manage customer orders, queries, and refunds by creating apps powered by native Toolpad integrations and third party APIs from providers like Stripe, Twilio, etc. Toolpad allows end users to create, read, update or delete records.',
     },
     {
       title: 'Custom CMS',
       wip: false,
-      imageUrl: '/static/toolpad/marketing/toolpad-custom-cms.png',
+      imageUrl: '/static/toolpad/marketing/toolpad-custom-cms.svg',
       description:
         'Enable your operations team to efficiently manage day to day chores using operation specific content management apps. Built with MUI components, apps are fast, manageable and scalable. Toolpad allows you to bind data the pro-code way by writing JavaScript anywhere. ',
     },

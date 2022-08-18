ca0e7262396919cd1029006081e0c8f0ef6f6ab6
Bharat Kashyap <bharatkashyap@outlook.com>
2022-08-18 18:58:47 +0800

    Add production form link

--- docs/src/route.ts
+++ docs/src/route.ts
@@ -4,7 +4,7 @@ const ROUTES = {
   TOOLPAD_SIGN_UP_URL:
     process.env.CONTEXT !== 'production' || process.env.NODE_ENV !== 'production'
       ? 'https://f0433e60.sibforms.com/serve/MUIEAC_Mm3SjecDuu_45huhRqqUS166FHh15psgNSmBZkXFSxEqRrUd5r__6BC0XkjnxnI9aE5VtmO5o3BODzXdwD04vC0uXK3MmcJ1eUAEPv5EdOtsHAXshvzE_F-fHN3DVT86dpiJI7ZjI28zOqrVBppsk2b0aGhP05bXTS8b0BxdQdJJHOdWSD7dB_Wx92aKmDzG8aadeU7oy'
-      : '/toolpad/sign-up',
+      : 'https://f0433e60.sibforms.com/serve/MUIEAGFYDweUFQkPjnLIMSkBeIONRgiHjnai-Osxix4qSMaIbjoS_t3QNieqXAXxMn8daD3eHz-CrHTaZtPBOyIqUnczRpgsOc9Q34MSh5JGJUgwUhxFcsGoR_uxE_92m_YuFLsIxKktMt39HrbmJa0rDbFAP_E5aaJS9xR3p5_fj8BjdIPye4yfNnRY7TM_4SIv3j3LrQUJ_P52',
 };
 
 export default ROUTES;

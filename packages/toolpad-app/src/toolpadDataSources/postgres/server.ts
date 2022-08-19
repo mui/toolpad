95d54df277c801bc2f88e46de78b25b3ba63293b
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-08-19 12:21:55 +0200

    parameter substiturton

--- packages/toolpad-app/src/toolpadDataSources/postgres/server.ts
+++ packages/toolpad-app/src/toolpadDataSources/postgres/server.ts
@@ -1,4 +1,4 @@
-import { Client } from 'pg';
+import { Client, QueryConfig } from 'pg';
 import { ServerDataSource } from '../../types';
 import { Maybe } from '../../utils/types';
 import {
@@ -8,6 +8,42 @@ import {
   PostgresResult,
 } from './types';
 
+/**
+ * Substitute named parameters ($varName) in a postgres query with their positional parameter ($1)
+ */
+function parseQuery(sql: string, params: [string, any][]): QueryConfig {
+  const substitutions = new Map(params.map(([name], i) => [name.toLowerCase(), i + 1]));
+  const sqlWithNamedVars = sql.replaceAll(/\$([a-zA-Z][a-zA-Z0-9]*)/g, (match, varName) => {
+    const index = substitutions.get(varName.toLowerCase());
+    if (typeof index === 'number') {
+      return `$${index}`;
+    }
+    return match;
+  });
+
+  return {
+    text: sqlWithNamedVars,
+    values: params.map(([, value]) => value),
+  };
+}
+
+/**
+ * Augment variables ($1, $2,...) in an error message with their name "$varName($1)"
+ */
+function parseErrorMessage(msg: string, params: [string, any][]): string {
+  const substitutions = new Map(params.map(([name], i) => [i + 1, name]));
+  console.log(substitutions);
+  const msgWithNamedVars = msg.replaceAll(/\$(\d+)/g, (match, index) => {
+    const varName = substitutions.get(Number(index));
+    if (typeof varName === 'string') {
+      return `$${varName}(${match})`;
+    }
+    return match;
+  });
+
+  return msgWithNamedVars;
+}
+
 async function execBase(
   connection: Maybe<PostgresConnectionParams>,
   postgresQuery: PostgresQuery,
@@ -16,14 +52,17 @@ async function execBase(
   const client = new Client({
     ...connection,
   });
+  const paramEntries = Object.entries(params);
   try {
     await client.connect();
-    const res = await client.query(postgresQuery.sql, Object.values(params));
+
+    const res = await client.query(parseQuery(postgresQuery.sql, paramEntries));
 
     return {
       data: res.rows,
     };
   } catch (error: any) {
+    error.message = parseErrorMessage(error.message, paramEntries);
     return {
       data: [],
       error,

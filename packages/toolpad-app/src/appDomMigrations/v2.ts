c18b526f5592a57973c9b3dc757d6c01edc08353
Bharat Kashyap <bharatkashyap@outlook.com>
2022-08-20 18:24:01 +0800

    remove merge conflict markers that crep in somehow

--- packages/toolpad-app/src/appDomMigrations/v2.ts
+++ packages/toolpad-app/src/appDomMigrations/v2.ts
@@ -1,5 +1,4 @@
 import invariant from 'invariant';
-<<<<<<< HEAD
 import { AppDom, QueryNode, createConst, isQuery, ElementNode, isElement, ref } from '../appDom';
 import { VersionOne, VersionTwo } from '../utils/types';
 import { update } from '../utils/immutability';
@@ -89,17 +88,5 @@ export default {
   down(dom: AppDom): Omit<AppDom, 'version'> & { version: VersionOne } {
     invariant(dom.version === 2, 'Can only migrate dom of version 2');
     return { ...dom, version: 1 };
-=======
-import { AppDom } from '../appDom';
-
-export default {
-  up(dom: AppDom) {
-    invariant(dom.version === 1, 'Can only migrate dom of version 1');
-    return dom;
-  },
-  down(dom: AppDom) {
-    invariant(dom.version === 2, 'Can only migrate dom of version 2');
-    return dom;
->>>>>>> 9e7ca81dffa0d3e6909aed6fc562c04b09717477
   },
 };

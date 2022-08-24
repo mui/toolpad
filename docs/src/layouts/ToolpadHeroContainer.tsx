390ac8ea31ea06509afda99f56926d077c3c1f22
Bharat Kashyap <bharatkashyap@outlook.com>
2022-08-24 15:38:05 +0800

    Fix hero layout issues without extra DOM node

--- docs/src/layouts/ToolpadHeroContainer.tsx
+++ docs/src/layouts/ToolpadHeroContainer.tsx
@@ -13,7 +13,6 @@ export default function HeroContainer({
     <Container
       sx={{
         minHeight: 500,
-        height: { xs: '100vh', sm: '140vh' },
         maxHeight: { xs: 800, sm: 1000, md: 1200, lg: 1400 },
         transition: '0.3s',
         display: 'flex',

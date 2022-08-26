72b87c7fd85a0c10eda871e9d1dfd76975de4f28
Bharat Kashyap <bharatkashyap@outlook.com>
2022-08-26 15:47:41 +0800

    Outline on Safari, no truncate video on all sizes

--- docs/src/components/landing/DemoVideo.js
+++ docs/src/components/landing/DemoVideo.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
 
 const Video = styled('video')(({ theme }) => ({
@@ -8,30 +9,43 @@ const Video = styled('video')(({ theme }) => ({
   borderRadius: '10px 10px 0 0 ',
   overflow: 'hidden',
   objectFit: 'cover',
-  objectPosition: 'top',
-  outline: `1px solid ${
-    theme.palette.mode === 'dark' ? theme.palette.primaryDark[400] : theme.palette.grey[300]
-  }`,
+  width: '100%',
+  height: '100%',
 }));
 
 export default function DemoVideo() {
   return (
-    <React.Fragment>
+    <Box
+      sx={(theme) => ({
+        borderStyle: 'solid',
+        maxWidth: 1100,
+        height: { xs: 200, sm: 390, md: 445, lg: 615 },
+        width: {
+          xs: 360,
+          sm: theme.breakpoints.values.sm + 100,
+          md: theme.breakpoints.values.md - 100,
+          lg: theme.breakpoints.values.lg - 100,
+        },
+        borderColor: `${
+          theme.palette.mode === 'dark' ? theme.palette.primaryDark[400] : theme.palette.grey[300]
+        }`,
+        borderRadius: '10px 10px 0 0',
+        borderWidth: '1px 1px 0 1px',
+      })}
+    >
       <Video
         sx={{
-          height: { lg: 655 },
           borderWidth: { xs: '10px 10px 0 10px', sm: '20px 20px 0 20px' },
         }}
-        width={1400}
-        playsInline
-        autoPlay
         muted
+        autoPlay
+        playsInline
         loop
-        poster={'/static/toolpad/marketing/hero-screenshot.svg'}
+        poster={'/static/toolpad/marketing/hero-screenshot.png'}
       >
         <source src="/static/toolpad/marketing/hero-video.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </Video>
-    </React.Fragment>
+    </Box>
   );
 }

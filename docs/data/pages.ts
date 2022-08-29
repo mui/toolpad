fa17eacfe84977f67b143328daa6c4701646a71f
bytasv <vytautas.butkus@gmail.com>
2022-08-29 11:48:10 +0300

    Fix docs paths

--- docs/data/pages.ts
+++ docs/data/pages.ts
@@ -7,22 +7,22 @@ const pages: MuiPage[] = [
     icon: 'DescriptionIcon',
     children: [
       { pathname: '/toolpad/getting-started/introduction', title: 'Introduction' },
-      { pathname: '/toolpad/getting-started/quickstart/', title: 'Quickstart' },
+      { pathname: '/toolpad/getting-started/quickstart', title: 'Quickstart' },
     ],
   },
   {
-    pathname: '/toolpad/core-concepts/',
+    pathname: '/toolpad/core-concepts',
     title: 'Core concepts',
     icon: 'CodeIcon',
     children: [
       {
-        pathname: '/toolpad/core-concepts/connecting-to-data-sources/',
+        pathname: '/toolpad/core-concepts/connecting-to-data-sources',
         title: 'Connecting to data sources',
       },
-      { pathname: '/toolpad/core-concepts/building-ui/', title: 'Building UI' },
-      { pathname: '/toolpad/core-concepts/data-binding/', title: 'Data binding' },
+      { pathname: '/toolpad/core-concepts/building-ui', title: 'Building UI' },
+      { pathname: '/toolpad/core-concepts/data-binding', title: 'Data binding' },
       {
-        pathname: '/toolpad/core-concepts/versioning-and-deploying/',
+        pathname: '/toolpad/core-concepts/versioning-and-deploying',
         title: 'Versioning & deploying',
       },
     ],

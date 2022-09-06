80fd971900c9510434b4bc47382762a472e6c8e0
Bharat Kashyap <bharatkashyap@outlook.com>
2022-09-06 14:34:38 +0530

    Remove debugging code

--- docs/pages/_document.js
+++ docs/pages/_document.js
@@ -1,10 +1,3 @@
 import MyDocument from '@mui/monorepo/docs/pages/_document';
 
-// eslint-disable-next-line no-console
-console.log(
-  process.env.PULL_REQUEST,
-  typeof process.env.PULL_REQUEST,
-  JSON.stringify(process.env.PULL_REQUEST),
-);
-
 export default MyDocument;

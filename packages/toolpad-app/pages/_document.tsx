048a8b240e7b685396ee79609b12653f34c6d820
Bharat Kashyap <bharatkashyap@outlook.com>
2022-09-07 19:28:14 +0530

    Move favicon to the root of the public folder

--- packages/toolpad-app/pages/_document.tsx
+++ packages/toolpad-app/pages/_document.tsx
@@ -68,7 +68,7 @@ export default class MyDocument extends Document<ToolpadDocumentProps> {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
-          <link rel="shortcut icon" href="/static/favicon.ico" />
+          <link rel="shortcut icon" href="/favicon.ico" />
           <script
             // eslint-disable-next-line react/no-danger
             dangerouslySetInnerHTML={{

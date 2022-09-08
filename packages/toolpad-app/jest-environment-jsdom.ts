8ac0c0c910d2995cfec22d74f12e4fe92bc097f7
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-09-08 14:21:41 +0200

    Fix node and jsdom tests interferring with each other

---
+++ packages/toolpad-app/jest-environment-jsdom.ts
@@ -0,0 +1,35 @@
+import { TextDecoder, TextEncoder } from 'util';
+import fetch, { Headers, Request, Response } from 'node-fetch';
+
+// my-custom-environment
+const JsdomEnvironment = require('jest-environment-jsdom').default;
+
+export default class CustomJsdomEnvironment extends JsdomEnvironment {
+  async setup() {
+    await super.setup();
+
+    if (!this.global.TextDecoder) {
+      this.global.TextDecoder = TextDecoder;
+    } else {
+      // eslint-disable-next-line no-console
+      console.info('TextDecoder not polyfilled: running on a platform that supports TextDecoder');
+    }
+
+    if (!this.global.TextEncoder) {
+      this.global.TextEncoder = TextEncoder;
+    } else {
+      // eslint-disable-next-line no-console
+      console.info('TextEncoder not polyfilled: running on a platform that supports TextEncoder');
+    }
+
+    if (!this.global.fetch) {
+      this.global.fetch = fetch;
+      this.global.Headers = Headers;
+      this.global.Request = Request;
+      this.global.Response = Response;
+    } else {
+      // eslint-disable-next-line no-console
+      console.info('Fetch API not polyfilled: running on a platform that supports fetch');
+    }
+  }
+}

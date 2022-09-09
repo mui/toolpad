b85873c0a37095ee16aa5c2f9428cbf6aa5dc6fa
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-09-09 10:47:34 +0200

    remove unnecessary comments

--- packages/toolpad-app/src/utils/errors.ts
+++ packages/toolpad-app/src/utils/errors.ts
@@ -5,26 +5,20 @@ export function errorFrom(maybeError: unknown): Error {
   if (maybeError instanceof Error) {
     return maybeError;
   }
+
   if (
     typeof maybeError === 'object' &&
     maybeError &&
     hasOwnProperty(maybeError, 'message') &&
     typeof maybeError.message! === 'string'
   ) {
-    return new Error(maybeError.message, {
-      // See https://github.com/microsoft/TypeScript/issues/48098
-      cause: maybeError,
-    });
+    return new Error(maybeError.message, { cause: maybeError });
   }
+
   if (typeof maybeError === 'string') {
-    return new Error(maybeError, {
-      // See https://github.com/microsoft/TypeScript/issues/48098
-      cause: maybeError,
-    });
+    return new Error(maybeError, { cause: maybeError });
   }
+
   const message = truncate(JSON.stringify(maybeError), 500);
-  return new Error(message, {
-    // See https://github.com/microsoft/TypeScript/issues/48098
-    cause: maybeError,
-  });
+  return new Error(message, { cause: maybeError });
 }

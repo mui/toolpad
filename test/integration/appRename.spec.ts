039c40b4049ff2a60b09e7a831602c8d3da8b0f1
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-09-14 19:35:44 +0200

    Self-document integration test mode (#995)
    * Rename var
    
    * this
    
    * set in PR previews
    
    * update creatApp

--- test/integration/appRename.spec.ts
+++ test/integration/appRename.spec.ts
@@ -1,29 +1,19 @@
-import { test, expect, Page } from '../playwright/test';
+import { ToolpadHome } from '../models/ToolpadHome';
+import { test, expect } from '../playwright/test';
 import generateId from '../utils/generateId';
 import * as locators from '../utils/locators';
-
-async function createApp(page: Page, name: string) {
-  await page.locator('button:has-text("create new")').click();
-
-  await page.fill('[role="dialog"] label:has-text("name")', name);
-
-  await page.click('[role="dialog"] button:has-text("create")');
-
-  await page.waitForNavigation({ url: /\/_toolpad\/app\/[^/]+\/pages\/[^/]+/ });
-}
 
 test('app create/rename flow', async ({ page }) => {
   const appName1 = `App ${generateId()}`;
   const appName2 = `App ${generateId()}`;
   const appName3 = `App ${generateId()}`;
 
-  await page.goto('/');
-  await createApp(page, appName1);
-
-  await page.goto('/');
-  await createApp(page, appName2);
-
-  await page.goto('/');
+  const homeModel = new ToolpadHome(page);
+  await homeModel.goto();
+  await homeModel.createApplication({ name: appName1 });
+  await homeModel.goto();
+  await homeModel.createApplication({ name: appName2 });
+  await homeModel.goto();
 
   await page.click(`${locators.toolpadHomeAppRow(appName1)} >> [aria-label="Application menu"]`);
 

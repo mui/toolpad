825b80d7ee34ade521cae846f404420555feb999
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2022-09-15 15:25:54 +0100

    use projectRoot

--- packages/toolpad-app/src/server/appTemplateDoms/doms.ts
+++ packages/toolpad-app/src/server/appTemplateDoms/doms.ts
@@ -3,14 +3,14 @@ import * as appDom from '../../appDom';
 
 import { AppTemplateId } from '../../types';
 import { readJsonFile } from '../../utils/fs';
+import projectRoot from '../projectRoot';
 
-// __dirname does not work, see: https://github.com/vercel/next.js/discussions/14341
 const DOMS_DIR_PATH = './src/server/appTemplateDoms';
 
 const APP_TEMPLATE_DOM_PATHS: Record<AppTemplateId, string | null> = {
   blank: null,
-  stats: path.resolve(DOMS_DIR_PATH, './statistics.json'),
-  images: path.resolve(DOMS_DIR_PATH, './images.json'),
+  stats: path.resolve(projectRoot, DOMS_DIR_PATH, './statistics.json'),
+  images: path.resolve(projectRoot, DOMS_DIR_PATH, './images.json'),
 };
 
 export function getAppTemplateDom(appTemplateId: AppTemplateId): Promise<appDom.AppDom> | null {

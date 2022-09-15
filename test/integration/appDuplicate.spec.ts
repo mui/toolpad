f1ce0e81b99d05e3bf9a220e027813abdeee2035
Bharat Kashyap <bharatkashyap@outlook.com>
2022-09-15 14:32:05 +0530

    Move more robust test to separate PR

--- test/integration/appDuplicate.spec.ts
+++ test/integration/appDuplicate.spec.ts
@@ -1,7 +1,6 @@
 import * as path from 'path';
 import { test, expect } from '../playwright/test';
 import { ToolpadHome } from '../models/ToolpadHome';
-import { ToolpadEditor } from '../models/ToolpadEditor';
 import { readJsonFile } from '../utils/fs';
 import * as locators from '../utils/locators';
 
@@ -11,20 +10,10 @@ test('duplicate app from home flow', async ({ page }) => {
   await homeModel.goto();
   const app = await homeModel.createApplication({ dom });
   await homeModel.goto();
-  const duplicateApp = await homeModel.duplicateApplication(app.name);
-
-  await expect(page.locator(locators.toolpadHomeAppRow(`${app.name} (copy)`))).toBeVisible();
-
   await homeModel.duplicateApplication(app.name);
+  await homeModel.goto();
+  await expect(page.locator(locators.toolpadHomeAppRow(`${app.name} (copy)`))).toBeVisible();
+  await homeModel.duplicateApplication(app.name);
+  await homeModel.goto();
   await expect(page.locator(locators.toolpadHomeAppRow(`${app.name} (copy 2)`))).toBeVisible();
-
-  const editorModel = new ToolpadEditor(page);
-  await editorModel.goto(duplicateApp.id);
-  await editorModel.goToPreview();
-
-  await page.locator('div', { hasText: 'Page' }).click();
-  await page.waitForNavigation();
-  await page.locator('button', { hasText: 'Go to Page 2' }).click();
-  await page.waitForNavigation();
-  await expect(page.locator('button', { hasText: 'Go to Page 1' })).toBeVisible();
 });

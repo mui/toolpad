4056f035a1d4e902a203bbfcc2ed567345e4f9c3
Bharat Kashyap <bharatkashyap@outlook.com>
2022-09-15 14:34:13 +0530

    The app doesn't navigate for some reason

--- test/integration/appDuplicate.spec.ts
+++ test/integration/appDuplicate.spec.ts
@@ -10,10 +10,18 @@ test('duplicate app from home flow', async ({ page }) => {
   await homeModel.goto();
   const app = await homeModel.createApplication({ dom });
   await homeModel.goto();
-  await homeModel.duplicateApplication(app.name);
+  const duplicateApp = await homeModel.duplicateApplication(app.name);
   await homeModel.goto();
   await expect(page.locator(locators.toolpadHomeAppRow(`${app.name} (copy)`))).toBeVisible();
   await homeModel.duplicateApplication(app.name);
   await homeModel.goto();
   await expect(page.locator(locators.toolpadHomeAppRow(`${app.name} (copy 2)`))).toBeVisible();
+
+  await page.goto(`/_toolpad/app/${duplicateApp.id}/preview/pages`);
+  await page.waitForNavigation({ url: /\/_toolpad\/app\/[^/]+\/preview\/pages/ });
+  await page.locator('button:has-text("Page")').click();
+  await page.waitForNavigation({ url: /\/_toolpad\/app\/[^/]+\/preview\/pages\/[^/]+/ });
+  await page.locator('button:has-text("Go to Page 2")').click();
+  await page.waitForNavigation({ url: /\/_toolpad\/app\/[^/]+\/preview\/pages\/[^/]+/ });
+  await expect(page.locator('button', { hasText: 'Go to Page 1' })).toBeVisible();
 });

f3d6b8ef20fc9d432d4d5b71f3ff47ea09814ca6
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-09-15 19:00:38 +0200

    only types

--- packages/toolpad-app/src/server/basicAuth.ts
+++ packages/toolpad-app/src/server/basicAuth.ts
@@ -1,4 +1,4 @@
-import { IncomingMessage, ServerResponse } from 'http';
+import type { IncomingMessage, ServerResponse } from 'http';
 import config from './config';
 
 export function checkBasicAuthHeader(headerContent: string | null): boolean {

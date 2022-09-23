4acdc1d38ae8493435b7ba238b70be98c6d9a154
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2022-09-23 20:12:13 +0100

    Revert change

--- packages/toolpad-app/sentry.server.config.js
+++ packages/toolpad-app/sentry.server.config.js
@@ -4,7 +4,7 @@
 
 import * as Sentry from '@sentry/nextjs';
 
-const SENTRY_DSN = process.env.SENTRY_DSN;
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
   enabled: process.env.NODE_ENV === 'production' && !!process.env.IS_SENTRY_ENABLED,

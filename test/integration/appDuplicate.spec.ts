524f8002a4504e5d49ab81cef7b57043952d9864
Bharat Kashyap <bharatkashyap@outlook.com>
2022-10-03 16:30:53 +0530

    Use ToolpadRuntime to compare Doms

--- test/integration/appDuplicate.spec.ts
+++ test/integration/appDuplicate.spec.ts
@@ -1,27 +1,34 @@
 import * as path from 'path';
 import { test, expect } from '../playwright/test';
 import { ToolpadHome } from '../models/ToolpadHome';
+import { ToolpadRuntime } from '../models/ToolpadRuntime';
 import { readJsonFile } from '../utils/fs';
-import * as locators from '../utils/locators';
 
 test('duplicate app from home flow', async ({ page }) => {
   const dom = await readJsonFile(path.resolve(__dirname, './duplicateNavigationDom.json'));
-  const homeModel = new ToolpadHome(page);
-  await homeModel.goto();
-  const app = await homeModel.createApplication({ dom });
-  await homeModel.goto();
-  const duplicateApp = await homeModel.duplicateApplication(app.name);
-  await homeModel.goto();
-  await expect(page.locator(locators.toolpadHomeAppRow(`${app.name} (copy)`))).toBeVisible();
-  await homeModel.duplicateApplication(app.name);
-  await homeModel.goto();
-  await expect(page.locator(locators.toolpadHomeAppRow(`${app.name} (copy 2)`))).toBeVisible();
 
-  await page.goto(`/_toolpad/app/${duplicateApp.id}/preview/pages`);
-  await page.waitForNavigation({ url: /\/_toolpad\/app\/[^/]+\/preview\/pages/ });
-  await page.locator('button:has-text("Page")').click();
-  await page.waitForNavigation({ url: /\/_toolpad\/app\/[^/]+\/preview\/pages\/[^/]+/ });
-  await page.locator('button:has-text("Go to Page 2")').click();
-  await page.waitForNavigation({ url: /\/_toolpad\/app\/[^/]+\/preview\/pages\/[^/]+/ });
-  await expect(page.locator('button', { hasText: 'Go to Page 1' })).toBeVisible();
+  const homeModel = new ToolpadHome(page);
+
+  await homeModel.goto();
+
+  const app = await homeModel.createApplication({ dom });
+
+  await homeModel.goto();
+
+  const duplicateApp = await homeModel.duplicateApplication(app.name);
+
+  await homeModel.goto();
+
+  await expect(homeModel.getAppRow(`${app.name} (copy)`)).toBeVisible();
+
+  await homeModel.duplicateApplication(app.name);
+
+  await homeModel.goto();
+
+  await expect(homeModel.getAppRow(`${app.name} (copy 2)`)).toBeVisible();
+
+  const runtimeModel = new ToolpadRuntime(page);
+  await runtimeModel.gotoPage(duplicateApp.id, 'Page 1');
+  await page.locator('button:has-text("Page 2")').click();
+  await expect(page.locator('button', { hasText: 'Page 1' })).toBeVisible();
 });

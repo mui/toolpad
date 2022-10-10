9d787b4c145684089830c9a9671b77cdae40ce96
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-10-10 10:40:19 +0200

    tweak cast

--- packages/toolpad-app/src/appDomMigrations/index.ts
+++ packages/toolpad-app/src/appDomMigrations/index.ts
@@ -4,8 +4,8 @@ import { CURRENT_APPDOM_VERSION } from '../appDom';
 
 const versions = new Map([[1, v1]]);
 
-export const latestVersion = Array.from(versions.keys()).pop();
+export const latestVersion = Array.from(versions.keys()).pop() as number;
 
 invariant(versions.size === CURRENT_APPDOM_VERSION, 'Unable to find the latest version');
 
-export const latestMigration = versions.get(latestVersion as number);
+export const latestMigration = versions.get(latestVersion);

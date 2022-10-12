81b504bf0d5cdea375848c448156f16507b2b064
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-10-12 11:26:52 +0200

    cleanup

--- packages/toolpad-app/src/appDom/migrations/v2.ts
+++ packages/toolpad-app/src/appDom/migrations/v2.ts
@@ -1,29 +1,30 @@
 import invariant from 'invariant';
 import * as appDom from '..';
+import { mapValues } from '../../utils/collections';
 
-function replaceMutation(node: appDom.MutationNode): appDom.QueryNode {
-  return {
-    ...node,
-    type: 'query',
-    parentProp: 'queries',
-    attributes: {
-      ...node.attributes,
-      mode: appDom.createConst('mutation'),
-    },
-  };
+function replaceMutation(node: any): appDom.AppDomNode {
+  if (node.type === 'mutation') {
+    return {
+      ...node,
+      type: 'query',
+      parentProp: 'queries',
+      attributes: {
+        ...node.attributes,
+        mode: appDom.createConst('mutation'),
+      },
+    };
+  }
+
+  return node;
 }
 
 export default {
   up(dom: appDom.AppDom): appDom.AppDom {
-    const { nodes, version = 0 } = dom;
-    invariant(version === 1, 'Can only migrate dom of version 1');
-
-    const migratedNodes = Object.fromEntries(
-      Object.entries(nodes).map(([id, node]) =>
-        appDom.isMutation(node) ? [id, replaceMutation(node)] : [id, node],
-      ),
-    );
-
-    return { ...dom, nodes: migratedNodes, version: 2 };
+    invariant(dom.version === 1, 'Can only migrate dom of version 1');
+    return {
+      ...dom,
+      nodes: mapValues(dom.nodes, (node) => replaceMutation(node)),
+      version: 2,
+    };
   },
 };

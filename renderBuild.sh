9ab3a309b08fa4959701b6d2180d61fbd73d00e1
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-10-17 10:45:46 +0200

    try cache

---
+++ renderBuild.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Ref: https://community.render.com/t/gatsby-build-caching-and-image-transformations/129/2
+
+restore_render_cache() {
+  local source_cache_dir="$1"
+  if [[ -d "$XDG_CACHE_HOME/$source_cache_dir" ]]; then
+    echo "CACHE HIT $source_cache_dir, rsyncing..."
+    rsync -a "$XDG_CACHE_HOME/$source_cache_dir/" $source_cache_dir
+  else
+    echo "CACHE MISS $source_cache_dir"
+  fi
+}
+
+save_render_cache() {
+  local source_cache_dir="$1"
+  echo "CACHE SAVE $source_cache_dir, rsyncing..."
+  mkdir -p "$XDG_CACHE_HOME/$source_cache_dir"
+  rsync -a $source_cache_dir/ "$XDG_CACHE_HOME/$source_cache_dir"
+}
+
+install_and_build_with_cache() {
+  local yarn_cache_dir = yarn cache dir
+
+  restore_render_cache "node_modules"
+  restore_render_cache "$yarn_cache_dir"
+  yarn --frozen-lockfile --prod=false
+  save_render_cache "node_modules"
+  save_render_cache "$yarn_cache_dir"
+
+  yarn release:build
+
+}
+
+install_and_build_with_cache
+

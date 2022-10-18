56a8188177b617458c50d3072f16be02cf499841
Bharat Kashyap <bharatkashyap@outlook.com>
2022-10-18 18:19:15 +0530

    [ui] Add branding theme to Toolpad app (#988)

--- packages/toolpad-app/src/components/HarViewer.tsx
+++ packages/toolpad-app/src/components/HarViewer.tsx
@@ -2,6 +2,7 @@ import { fromHar } from 'perf-cascade';
 import * as React from 'react';
 import { Har } from 'har-format';
 import { styled, SxProps } from '@mui/material';
+import { useTheme, Theme } from '@mui/material/styles';
 import 'perf-cascade/dist/perf-cascade.css';
 import { createHarLog } from '../utils/har';
 
@@ -11,6 +12,27 @@ function fixLinks(elm: Element) {
   elm.querySelectorAll('a').forEach((link) => link.setAttribute('target', '_blank'));
 }
 
+function forceDarkMode(elm: Element, theme: Theme) {
+  elm.querySelectorAll('svg').forEach((svg) => {
+    svg.setAttribute('fill', 'white');
+    if (svg.classList.contains('left-fixed-holder')) {
+      const fullLabel = svg.getElementsByClassName('label-full-bg');
+      if (fullLabel && fullLabel.length > 0) {
+        (fullLabel[0] as SVGElement).style.fill = theme.palette.primaryDark[500];
+      }
+    }
+
+    if (svg.classList.contains('tooltip-holder')) {
+      const tooltips = svg.getElementsByClassName('tooltip-payload');
+      if (tooltips && tooltips.length > 0) {
+        for (const tooltip of tooltips) {
+          (tooltip as HTMLDivElement).style.backgroundColor = theme.palette.primaryDark[500];
+        }
+      }
+    }
+  });
+}
+
 export interface HarViewerProps {
   value?: Har;
   sx?: SxProps;
@@ -18,18 +40,25 @@ export interface HarViewerProps {
 
 export default function HarViewer({ value = createHarLog(), sx }: HarViewerProps) {
   const rootRef = React.useRef<HTMLDivElement>(null);
+  const theme = useTheme();
 
   React.useEffect(() => {
     const root = rootRef.current;
     if (value && value.log.entries.length > 0 && root) {
       const svg = fromHar(value);
       fixLinks(svg);
+      if (theme.palette.mode === 'dark') {
+        forceDarkMode(svg, theme);
+      }
 
       const observer = new MutationObserver((entries) => {
         for (const entry of entries) {
           for (const node of entry.addedNodes) {
             if (node instanceof Element) {
               fixLinks(node);
+              if (theme.palette.mode === 'dark') {
+                forceDarkMode(svg, theme);
+              }
             }
           }
         }
@@ -47,7 +76,7 @@ export default function HarViewer({ value = createHarLog(), sx }: HarViewerProps
       };
     }
     return () => {};
-  }, [value]);
+  }, [value, theme]);
 
   return <HarViewerRoot ref={rootRef} sx={sx} />;
 }

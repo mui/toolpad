f99706d4afd77ce198e0dd2f6853f562cb0a18aa
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-10-19 14:28:31 +0200

    order

--- packages/toolpad-app/src/toolpad/AppEditor/ElementContext.tsx
+++ packages/toolpad-app/src/toolpad/AppEditor/ElementContext.tsx
@@ -1,4 +1,4 @@
-import * as appDom from '../../appDom';
 import * as React from 'react';
+import * as appDom from '../../appDom';
 
 export default React.createContext<appDom.ElementNode | undefined>(undefined);

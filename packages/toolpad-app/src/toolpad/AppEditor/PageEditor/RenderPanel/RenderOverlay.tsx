b899fb33f4ee9712940a0c2f161665d526f6727b
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2022-10-25 14:49:36 +0100

    Improve Paper component (#1031)

--- packages/toolpad-app/src/toolpad/AppEditor/PageEditor/RenderPanel/RenderOverlay.tsx
+++ packages/toolpad-app/src/toolpad/AppEditor/PageEditor/RenderPanel/RenderOverlay.tsx
@@ -239,7 +239,7 @@ export default function RenderOverlay({ canvasHostRef }: RenderOverlayProps) {
       const isSecondLastLayoutContainerChild =
         parent &&
         appDom.isElement(parent) &&
-        (isPageRow(parent) || isPageColumn(parent)) &&
+        isPageLayoutComponent(parent) &&
         parentChildren.length === 2;
 
       const hasNoLayoutContainerSiblings =
@@ -255,7 +255,11 @@ export default function RenderOverlay({ canvasHostRef }: RenderOverlayProps) {
           )[0];
 
           if (lastContainerChild.parentProp) {
-            if (moveTargetNodeId !== parent.id && moveTargetNodeId !== lastContainerChild.id) {
+            if (
+              moveTargetNodeId !== parent.id &&
+              moveTargetNodeId !== lastContainerChild.id &&
+              isPageLayoutComponent(parentParent)
+            ) {
               domApi.moveNode(
                 lastContainerChild,
                 parentParent,
@@ -781,8 +785,18 @@ export default function RenderOverlay({ canvasHostRef }: RenderOverlayProps) {
           appDom.isElement(activeDropNodeParent) &&
           isPageRow(activeDropNodeParent);
 
+        const activeDropNodeParentParent =
+          activeDropNodeParent && appDom.getParent(dom, activeDropNodeParent);
+        const activeDropNodeParentParentInfo =
+          activeDropNodeParentParent && nodesInfo[activeDropNodeParentParent.id];
+        const hasActiveDropNodeParentParentSlot = !!(
+          activeDropNodeParentParentInfo?.slots &&
+          activeDropNodeParentParentInfo?.slots[activeDropSlotParentProp || 'children']
+        );
+
         const hasDragOverParentRowOverride =
           isDragOverParentPageRow &&
+          hasActiveDropNodeParentParentSlot &&
           activeDropNodeSiblings.length === 0 &&
           (activeDropZone === DROP_ZONE_TOP || activeDropZone === DROP_ZONE_BOTTOM);
 
@@ -850,6 +864,7 @@ export default function RenderOverlay({ canvasHostRef }: RenderOverlayProps) {
         null;
 
       const isDraggingOverPage = dragOverNode ? appDom.isPage(dragOverNode) : false;
+      const isDraggingOverLayoutSlot = dragOverSlot?.type === 'layout';
       const isDraggingOverElement = appDom.isElement(dragOverNode);
 
       let parent = appDom.getParent(dom, dragOverNode);
@@ -868,8 +883,8 @@ export default function RenderOverlay({ canvasHostRef }: RenderOverlayProps) {
         addOrMoveNode = domApi.moveNode;
       }
 
-      // Drop on page
-      if (isDraggingOverPage) {
+      // Drop on page or layout slot
+      if (isDraggingOverPage || isDraggingOverLayoutSlot) {
         const newParentIndex =
           dragOverZone === DROP_ZONE_TOP
             ? appDom.getNewFirstParentIndexInNode(dom, dragOverNode, 'children')
@@ -886,7 +901,12 @@ export default function RenderOverlay({ canvasHostRef }: RenderOverlayProps) {
         }
       }
 
-      if (isDraggingOverElement && parent && (appDom.isPage(parent) || appDom.isElement(parent))) {
+      if (
+        isDraggingOverElement &&
+        !isDraggingOverLayoutSlot &&
+        parent &&
+        (appDom.isPage(parent) || appDom.isElement(parent))
+      ) {
         const isDraggingOverRow = isDraggingOverElement && isPageRow(dragOverNode);
 
         const isDraggingOverHorizontalContainer = dragOverSlot

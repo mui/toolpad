99fbe52579e727a7881342b3492049920d115842
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2022-10-26 17:38:09 +0100

    App templates tests

---
+++ test/integration/appTemplates.spec.ts
@@ -0,0 +1,51 @@
+import { ToolpadHome } from '../models/ToolpadHome';
+import { ToolpadRuntime } from '../models/ToolpadRuntime';
+import { test, expect } from '../playwright/test';
+
+test('can use statistics app template', async ({ page }) => {
+  const homeModel = new ToolpadHome(page);
+  await homeModel.goto();
+  const app = await homeModel.createApplication({ appTemplateId: 'stats' });
+
+  page.waitForNavigation();
+
+  const runtimeModel = new ToolpadRuntime(page);
+  await runtimeModel.gotoPage(app.id, 'stats');
+
+  await page.locator('h3:has-text("Statistics")').waitFor({ state: 'visible' });
+  await page.locator('text="Active Cases_text"').waitFor({ state: 'visible' });
+  await page.locator('text="USA"').waitFor({ state: 'visible' });
+});
+
+test.only('can use images app template', async ({ page }) => {
+  const homeModel = new ToolpadHome(page);
+  await homeModel.goto();
+  const app = await homeModel.createApplication({ appTemplateId: 'images' });
+
+  page.waitForNavigation();
+
+  const runtimeModel = new ToolpadRuntime(page);
+  await runtimeModel.gotoPage(app.id, 'dogBreedsPage');
+
+  await page.locator('h3:has-text("Dog Images")').waitFor({ state: 'visible' });
+
+  const imageLocator = page.locator('img');
+
+  expect(await imageLocator.getAttribute('src')).toBeNull();
+
+  const breedInputLocator = page.locator('[aria-haspopup="listbox"]').first();
+  await breedInputLocator.click();
+  await page.locator('li:has-text("australian")').click();
+
+  const subBreedInputLocator = page.locator('[aria-haspopup="listbox"]').nth(1);
+  await subBreedInputLocator.click();
+  await page.locator('li:has-text("shepherd")').click();
+
+  await page.locator('button:has-text("New Image")').click();
+
+  await page.waitForResponse('https://images.dog.ceo/**');
+
+  expect(await imageLocator.getAttribute('src')).toContain(
+    'https://images.dog.ceo/breeds/australian-shepherd',
+  );
+});

37b5621d55ced06c2469fb7a680dee8a092855d5
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2022-10-27 19:05:42 +0100

    Remove escape characters

--- packages/toolpad-components/src/DataGrid.tsx
+++ packages/toolpad-components/src/DataGrid.tsx
@@ -101,7 +101,7 @@ function SkeletonLoadingOverlay() {
 }
 
 const URL_REGEX =
-  /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
+  /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[\w]*))?)/;
 
 function inferColumnType(value: unknown): string {
   if (value instanceof Date) {

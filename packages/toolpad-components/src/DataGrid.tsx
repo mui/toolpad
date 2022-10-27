c19592904271593bdea7abd5adf5df6f3265a048
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2022-10-27 18:57:38 +0100

    Better URL regex

--- packages/toolpad-components/src/DataGrid.tsx
+++ packages/toolpad-components/src/DataGrid.tsx
@@ -100,8 +100,10 @@ function SkeletonLoadingOverlay() {
   );
 }
 
-const URL_REGEX =
-  /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)?/gi;
+const URL_REGEX = new RegExp(
+  '^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$',
+  'i',
+);
 
 function inferColumnType(value: unknown): string {
   if (value instanceof Date) {

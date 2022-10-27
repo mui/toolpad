78df32b9b7ad8b85ba5e43b55c8bb8ee07044655
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2022-10-27 19:06:55 +0100

    Add link to source

--- packages/toolpad-components/src/DataGrid.tsx
+++ packages/toolpad-components/src/DataGrid.tsx
@@ -100,6 +100,7 @@ function SkeletonLoadingOverlay() {
   );
 }
 
+// https://stackoverflow.com/questions/161738/what-is-the-best-regular-expression-to-check-if-a-string-is-a-valid-url
 const URL_REGEX =
   /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[\w]*))?)/;
 

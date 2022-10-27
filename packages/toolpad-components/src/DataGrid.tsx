fbb7369c3177e263819743a7a9045a458b0849d1
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2022-10-27 19:03:26 +0100

    Another url regex

--- packages/toolpad-components/src/DataGrid.tsx
+++ packages/toolpad-components/src/DataGrid.tsx
@@ -100,10 +100,8 @@ function SkeletonLoadingOverlay() {
   );
 }
 
-const URL_REGEX = new RegExp(
-  '^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$',
-  'i',
-);
+const URL_REGEX =
+  /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
 
 function inferColumnType(value: unknown): string {
   if (value instanceof Date) {

c1fc718dd25d81d8acaba2ae99ac726a7552512a
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-10-31 16:46:14 +0100

    infoz

--- test/integration/rest-basic/index.spec.ts
+++ test/integration/rest-basic/index.spec.ts
@@ -6,7 +6,14 @@ import { readJsonFile } from '../../utils/fs';
 
 // We can run our own httpbin instance if necessary:
 //    $ docker run -p 80:80 kennethreitz/httpbin
-const HTTPBIN_BASEURL = process.env.HTTPBIN_BASEURL || 'https://httpbin.org/';
+const customHttbinBaseUrl = process.env.HTTPBIN_BASEURL;
+
+if (customHttbinBaseUrl) {
+  // eslint-disable-next-line no-console
+  console.log(`Running tests with custom httpbin service: ${customHttbinBaseUrl}`);
+}
+
+const HTTPBIN_BASEURL = customHttbinBaseUrl || 'https://httpbin.org/';
 
 test('functions basics', async ({ page }) => {
   const dom = await readJsonFile(path.resolve(__dirname, './restDom.json'));

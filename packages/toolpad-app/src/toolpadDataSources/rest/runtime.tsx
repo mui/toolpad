5fe20569a7db4b33dd8928de32ffe2eeec2c6821
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-10-31 15:15:03 +0100

    WIP HAR

--- packages/toolpad-app/src/toolpadDataSources/rest/runtime.tsx
+++ packages/toolpad-app/src/toolpadDataSources/rest/runtime.tsx
@@ -1,3 +1,4 @@
+import type { Entry } from 'har-format';
 import { ExecFetchFn, RuntimeDataSource } from '../../types';
 import { FetchQuery, FetchResult } from './types';
 import { execfetch } from './shared';
@@ -9,9 +10,16 @@ export async function clientExec(
   serverFetch: ExecFetchFn<FetchQuery, FetchResult>,
 ): Promise<FetchResult> {
   if (fetchQuery.browser) {
+    const instrumentedFetch = async (...args: Parameters<typeof fetch>) => {
+      const req = new Request(...args);
+      const res = await window.fetch(req);
+      const entry: Entry = {};
+      return res;
+    };
+
     return execfetch(fetchQuery, params, {
       connection: null,
-      fetchImpl: window.fetch,
+      fetchImpl: instrumentedFetch,
       evalExpression,
     });
   }

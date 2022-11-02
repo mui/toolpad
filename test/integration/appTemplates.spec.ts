a339333fd8c1e14c04a4eb18d5db2df3ada7235a
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2022-11-02 13:12:27 +0000

    Use toHaveAttribute

--- test/integration/appTemplates.spec.ts
+++ test/integration/appTemplates.spec.ts
@@ -1,7 +1,6 @@
 import { ToolpadHome } from '../models/ToolpadHome';
 import { ToolpadRuntime } from '../models/ToolpadRuntime';
 import { test, expect } from '../playwright/test';
-import { waitForDataResponse } from '../utils/response';
 
 test('can use statistics app template', async ({ page }) => {
   const homeModel = new ToolpadHome(page);
@@ -19,7 +18,7 @@ test('can use statistics app template', async ({ page }) => {
   );
 });
 
-test('can use images app template', async ({ page, baseURL }) => {
+test('can use images app template', async ({ page }) => {
   const homeModel = new ToolpadHome(page);
   await homeModel.goto();
   const app = await homeModel.createApplication({ appTemplateId: 'images' });
@@ -34,15 +33,14 @@ test('can use images app template', async ({ page, baseURL }) => {
   const breedInputLocator = page.locator('[aria-haspopup="listbox"]').first();
   await breedInputLocator.click();
   await page.locator('li:has-text("australian")').click();
-  await waitForDataResponse(page, baseURL!);
 
   const subBreedInputLocator = page.locator('[aria-haspopup="listbox"]').nth(1);
   await subBreedInputLocator.click();
   await page.locator('li:has-text("shepherd")').click();
-  await waitForDataResponse(page, baseURL!);
 
   const imageLocator = page.locator('img');
-  expect(await imageLocator.getAttribute('src')).toContain(
-    'https://images.dog.ceo/breeds/australian-shepherd',
+  await expect(imageLocator).toHaveAttribute(
+    'src',
+    /^https:\/\/images.dog.ceo\/breeds\/australian-shepherd\/.*$/,
   );
 });
--- test/utils/response.ts
+++
@@ -1,6 +0,0 @@
-import { Page } from '@playwright/test';
-
-export function waitForDataResponse(page: Page, baseURL: string) {
-  const apiDataUrlRegex = new RegExp(`^${baseURL}api/data/.*$`);
-  return page.waitForResponse(apiDataUrlRegex);
-}

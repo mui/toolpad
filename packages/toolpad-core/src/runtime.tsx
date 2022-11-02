a11b8982bb005fbb3ea517420f7af97765dd2ad8
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-11-02 13:29:21 +0100

    lint

--- packages/toolpad-core/src/runtime.tsx
+++ packages/toolpad-core/src/runtime.tsx
@@ -99,7 +99,7 @@ export function NodeRuntimeWrapper({
         <NodeError error={error} />
       </NodeFiberHost>
     ),
-    [componentConfig, nodeId],
+    [NodeError, componentConfig, nodeId],
   );
 
   return (

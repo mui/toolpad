dba88a8799dbbe27d1ca20bece6832e1bc250fbc
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-11-02 11:39:17 +0100

    har

--- packages/toolpad-app/src/toolpadDataSources/rest/runtime.tsx
+++ packages/toolpad-app/src/toolpadDataSources/rest/runtime.tsx
@@ -3,6 +3,7 @@ import { ExecFetchFn, RuntimeDataSource } from '../../types';
 import { FetchQuery, FetchResult } from './types';
 import { execfetch } from './shared';
 import evalExpression from '../../utils/evalExpression';
+import { createHarLog } from '../../utils/har';
 
 export async function clientExec(
   fetchQuery: FetchQuery,
@@ -10,18 +11,57 @@ export async function clientExec(
   serverFetch: ExecFetchFn<FetchQuery, FetchResult>,
 ): Promise<FetchResult> {
   if (fetchQuery.browser) {
+    const har = createHarLog();
+
     const instrumentedFetch = async (...args: Parameters<typeof fetch>) => {
+      const startedDateTime = new Date().toISOString();
       const req = new Request(...args);
+      const url = new URL(req.url);
       const res = await window.fetch(req);
-      const entry: Entry = {};
+      const entry: Entry = {
+        startedDateTime,
+        request: {
+          bodySize: 0,
+          cookies: [],
+          headers: Array.from(req.headers, ([name, value]) => ({ name, value })),
+          headersSize: 0,
+          httpVersion: '',
+          method: req.method,
+          queryString: Array.from(url.searchParams, ([name, value]) => ({ name, value })),
+          url: url.href,
+        },
+        response: {
+          bodySize: 0,
+          content: {
+            mimeType: res.headers.get('content-type') || '',
+            size: Number(res.headers.get('content-length')) || 0,
+          },
+          cookies: [],
+          headers: Array.from(res.headers, ([name, value]) => ({ name, value })),
+          headersSize: 0,
+          httpVersion: '',
+          redirectURL: '',
+          status: res.status,
+          statusText: res.statusText,
+        },
+        cache: {},
+        time: Date.now(),
+        timings: {
+          wait: 0,
+          receive: 0,
+        },
+      };
+      har.log.entries.push(entry);
       return res;
     };
 
-    return execfetch(fetchQuery, params, {
+    const result = await execfetch(fetchQuery, params, {
       connection: null,
       fetchImpl: instrumentedFetch,
       evalExpression,
     });
+
+    return { ...result, har };
   }
 
   return serverFetch(fetchQuery, params);

7e062a17c871f47ada8e81f2dccdd6882afd851d
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2022-11-08 13:59:32 +0000

    Improve types

--- packages/toolpad-app/src/apiErrors.ts
+++ packages/toolpad-app/src/apiErrors.ts
@@ -1,15 +1,8 @@
+import * as apiErrorCodes from './apiErrorCodes';
+
 import { ValueOf } from './utils/types';
 
-// Do not export this object directly for easier tree-shaking
-const API_ERROR_CODES = {
-  APP_NAME_EXISTS: 'TOOLPAD_APP_NAME_EXISTS',
-  VALIDATE_CAPTCHA_FAILED: 'TOOLPAD_VALIDATE_CAPTCHA_FAILED',
-};
-
-export const APP_NAME_EXISTS_ERROR_CODE = API_ERROR_CODES.APP_NAME_EXISTS;
-export const VALIDATE_CAPTCHA_FAILED_ERROR_CODE = API_ERROR_CODES.VALIDATE_CAPTCHA_FAILED;
-
-export type ApiErrorCode = ValueOf<typeof API_ERROR_CODES>;
+export type ApiErrorCode = ValueOf<typeof apiErrorCodes>;
 
 export class ApiError extends Error {
   code: ApiErrorCode;

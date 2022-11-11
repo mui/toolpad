bdafe286e53fe0a8b2483d6b321034178e7106e4
Bharat Kashyap <bharatkashyap@outlook.com>
2022-11-12 01:16:31 +0530

    Backward compatibility with Typography/Link

--- packages/toolpad-app/src/appDom/migrations/v3.ts
+++ packages/toolpad-app/src/appDom/migrations/v3.ts
@@ -2,14 +2,30 @@ import invariant from 'invariant';
 import * as appDom from '..';
 import { mapValues } from '../../utils/collections';
 
-function replaceQueryParams(node: any): appDom.AppDomNode {
-  if (node.type === 'query') {
+// function replaceQueryParams(node: any): appDom.AppDomNode {
+//   if (node.type === 'query') {
+//     return {
+//       ...node,
+//       params: node.params ? Object.entries(node.params) : undefined,
+//     };
+//   }
+
+//   return node;
+// }
+
+function replaceTypographyAndLinkWithText(node: any): appDom.AppDomNode {
+  if (node.type === 'element') {
     return {
+      name: node.name.replace(/Typography|Link/gi, 'text'),
+      attributes: {
+        component: {
+          ...node.attributes.component,
+          value: 'Text',
+        },
+      },
       ...node,
-      params: node.params ? Object.entries(node.params) : undefined,
     };
   }
-
   return node;
 }
 
@@ -18,7 +34,7 @@ export default {
     invariant(dom.version === 2, 'Can only migrate dom of version 2');
     return {
       ...dom,
-      nodes: mapValues(dom.nodes, (node) => replaceQueryParams(node)),
+      nodes: mapValues(dom.nodes, (node) => replaceTypographyAndLinkWithText(node)),
       version: 3,
     };
   },

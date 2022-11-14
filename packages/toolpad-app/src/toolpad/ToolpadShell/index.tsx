054519892da394dd4ee5bbe60b880ff0141159b6
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2022-11-14 21:33:51 +0000

    Show self-host bar in demo (#1309)

--- packages/toolpad-app/src/toolpad/ToolpadShell/index.tsx
+++ packages/toolpad-app/src/toolpad/ToolpadShell/index.tsx
@@ -2,6 +2,10 @@ import * as React from 'react';
 import { styled } from '@mui/material';
 import Header from './Header';
 
+import DemoBar from './DemoBar';
+
+import config from '../../config';
+
 export interface ToolpadShellProps {
   actions?: React.ReactNode;
   status?: React.ReactNode;
@@ -27,6 +31,7 @@ export default function ToolpadShell({ children, ...props }: ToolpadShellProps) 
     <ToolpadShellRoot>
       <Header {...props} />
       <ViewPort>{children}</ViewPort>
+      {config.isDemo ? <DemoBar /> : null}
     </ToolpadShellRoot>
   );
 }

35812f2afff87fbf9a403c3891674516d31caa80
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2022-11-15 20:16:35 +0000

    Nevermind

--- packages/toolpad-app/src/utils/crypto.ts
+++ packages/toolpad-app/src/utils/crypto.ts
@@ -3,7 +3,7 @@ import invariant from 'invariant';
 const crypto =
   typeof window === 'undefined'
     ? // eslint-disable-next-line global-require
-      (invariant(!global.crypto, 'Remove the crypto polyfill'), require('crypto'))
+      (invariant(!!global.crypto, 'Remove the crypto polyfill'), require('crypto'))
     : global.crypto;
 
 export default crypto;

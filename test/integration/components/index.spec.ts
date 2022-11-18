3cfe2f616d48dc5e08f924625a0579fa528f3e9c
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-11-18 13:04:45 +0100

    tests

--- test/integration/components/index.spec.ts
+++ test/integration/components/index.spec.ts
@@ -1,10 +1,21 @@
 import * as path from 'path';
+import { ToolpadEditor } from '../../models/ToolpadEditor';
 import { ToolpadHome } from '../../models/ToolpadHome';
 import { ToolpadRuntime } from '../../models/ToolpadRuntime';
-import { test } from '../../playwright/test';
+import { FrameLocator, Page, test } from '../../playwright/test';
 import { readJsonFile } from '../../utils/fs';
 
-test('components', async ({ page }) => {
+async function waitForComponents(page: Page | FrameLocator) {
+  await page.locator('text="foo button"').waitFor({ state: 'visible' });
+  await page.locator('img[alt="foo image"]').waitFor({ state: 'attached' });
+  await page.locator('text="foo datagrid column"').waitFor({ state: 'visible' });
+  await page.locator('text="custom component 1"').waitFor({ state: 'visible' });
+  await page.locator('label:has-text("foo textfield")').waitFor({ state: 'visible' });
+  await page.locator('text="foo typography"').waitFor({ state: 'visible' });
+  await page.locator('label:has-text("foo select")').waitFor({ state: 'visible' });
+}
+
+test.only('components', async ({ page, browserName }) => {
   const dom = await readJsonFile(path.resolve(__dirname, './componentsDom.json'));
 
   const homeModel = new ToolpadHome(page);
@@ -14,10 +25,10 @@ test('components', async ({ page }) => {
   const runtimeModel = new ToolpadRuntime(page);
   await runtimeModel.gotoPage(app.id, 'components');
 
-  await page.locator('text="foo button"').waitFor({ state: 'visible' });
-  await page.locator('img[alt="foo image"]').waitFor({ state: 'attached' });
-  await page.locator('text="foo datagrid column"').waitFor({ state: 'visible' });
-  await page.locator('label:has-text("foo textfield")').waitFor({ state: 'visible' });
-  await page.locator('text="foo typography"').waitFor({ state: 'visible' });
-  await page.locator('label:has-text("foo select")').waitFor({ state: 'visible' });
+  await waitForComponents(page);
+
+  const editorModel = new ToolpadEditor(page, browserName);
+  editorModel.goto(app.id);
+
+  await waitForComponents(editorModel.appCanvas);
 });

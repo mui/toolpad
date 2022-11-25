8dac510adbf50d8a7d6dc72a5049f2eef7804907
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-11-25 11:28:07 +0100

    Add full test

--- test/integration/rest-basic/index.spec.ts
+++ test/integration/rest-basic/index.spec.ts
@@ -3,6 +3,7 @@ import { test, expect } from '@playwright/test';
 import { ToolpadHome } from '../../models/ToolpadHome';
 import { ToolpadRuntime } from '../../models/ToolpadRuntime';
 import { readJsonFile } from '../../utils/fs';
+import { ToolpadEditor } from '../../models/ToolpadEditor';
 
 // We can run our own httpbin instance if necessary:
 //    $ docker run -p 80:80 kennethreitz/httpbin
@@ -15,7 +16,7 @@ if (customHttbinBaseUrl) {
 
 const HTTPBIN_BASEURL = customHttbinBaseUrl || 'https://httpbin.org/';
 
-test('rest basics', async ({ page }) => {
+test('rest basics', async ({ page, browserName }) => {
   const dom = await readJsonFile(path.resolve(__dirname, './restDom.json'));
 
   const httpbinConnection: any = Object.values(dom.nodes).find(
@@ -36,4 +37,13 @@ test('rest basics', async ({ page }) => {
   await expect(page.locator('text="query2: query2_value"')).toBeVisible();
   await expect(page.locator('text="browserQuery: browserQuery_value"')).toBeVisible();
   await expect(page.locator('text="browserQuery: browserQuery_value"')).toBeVisible();
+
+  const editorModel = new ToolpadEditor(page, browserName);
+  await editorModel.goto(app.id);
+
+  await editorModel.componentEditor.getByRole('button', { name: 'browserQuery' }).click();
+  const queryEditor = page.getByRole('dialog', { name: 'browserQuery' });
+  await queryEditor.getByRole('button', { name: 'Preview' }).click();
+  const networkTab = queryEditor.getByRole('tabpanel', { name: 'Network' });
+  await expect(networkTab.getByText('/get?browserQuery_param1=browserQuery_value')).not.toBeEmpty();
 });

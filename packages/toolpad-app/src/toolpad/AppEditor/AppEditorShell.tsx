945699b953bcd0ab9fe904a8c37878ea2e154a5e
bytasv <vytautas.butkus@gmail.com>
2022-11-29 15:08:23 +0200

    Remove undo/redo buttons

--- packages/toolpad-app/src/toolpad/AppEditor/AppEditorShell.tsx
+++ packages/toolpad-app/src/toolpad/AppEditor/AppEditorShell.tsx
@@ -3,7 +3,6 @@ import {
   Alert,
   Box,
   Button,
-  IconButton,
   Dialog,
   DialogActions,
   DialogContent,
@@ -18,15 +17,13 @@ import SyncIcon from '@mui/icons-material/Sync';
 import SyncProblemIcon from '@mui/icons-material/SyncProblem';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
-import Undo from '@mui/icons-material/Undo';
-import Redo from '@mui/icons-material/Redo';
 
 import * as React from 'react';
 import { useForm } from 'react-hook-form';
 import { Outlet } from 'react-router-dom';
 import invariant from 'invariant';
 import DialogForm from '../../components/DialogForm';
-import { DomLoader, useDomLoader, useDomApi } from '../DomLoader';
+import { DomLoader, useDomLoader } from '../DomLoader';
 import ToolpadShell from '../ToolpadShell';
 import PagePanel from './PagePanel';
 import client from '../../api';
@@ -141,7 +138,6 @@ export interface ToolpadShellProps {
 
 export default function AppEditorShell({ appId, ...props }: ToolpadShellProps) {
   const domLoader = useDomLoader();
-  const domApi = useDomApi();
 
   const {
     value: createReleaseDialogOpen,
@@ -153,16 +149,6 @@ export default function AppEditorShell({ appId, ...props }: ToolpadShellProps) {
     <ToolpadShell
       actions={
         <Stack direction="row" gap={1} alignItems="center">
-          <IconButton onClick={domApi.undo} data-testid="undo-button">
-            <Tooltip title="Undo">
-              <Undo />
-            </Tooltip>
-          </IconButton>
-          <IconButton onClick={domApi.redo} data-testid="redo-button">
-            <Tooltip title="Redo">
-              <Redo />
-            </Tooltip>
-          </IconButton>
           <Button
             variant="outlined"
             endIcon={<OpenInNewIcon />}

baabb445233342b9129332d58f1a78577cb7a9ac
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2022-12-14 22:07:01 +0000

    Remove batching test

--- test/integration/undo-redo/index.spec.ts
+++ test/integration/undo-redo/index.spec.ts
@@ -2,7 +2,6 @@ import * as path from 'path';
 import { ToolpadEditor } from '../../models/ToolpadEditor';
 import { test, expect } from '../../playwright/test';
 import { readJsonFile } from '../../utils/fs';
-import clickCenter from '../../utils/clickCenter';
 import generateId from '../../utils/generateId';
 
 test('test basic undo and redo', async ({ page, browserName, api }) => {
@@ -41,43 +40,4 @@ test('test basic undo and redo', async ({ page, browserName, api }) => {
 
   // Redo should bring back text field
   await expect(canvasInputLocator).toHaveCount(3);
-});
-
-test('test batching quick actions into single undo entry', async ({ page, browserName, api }) => {
-  const dom = await readJsonFile(path.resolve(__dirname, './dom.json'));
-
-  const app = await api.mutation.createApp(`App ${generateId()}`, {
-    from: { kind: 'dom', dom },
-  });
-
-  const editorModel = new ToolpadEditor(page, browserName);
-  await editorModel.goto(app.id);
-
-  await editorModel.waitForOverlay();
-
-  const input = editorModel.appCanvas.locator('input').first();
-
-  clickCenter(page, input);
-
-  await editorModel.componentEditor.getByLabel('defaultValue').click();
-
-  await page.keyboard.type('some value');
-
-  // Wait for undo stack to be updated
-  await page.waitForTimeout(600);
-
-  await page.keyboard.type(' hello');
-
-  await editorModel.componentEditor.getByLabel('defaultValue').blur();
-
-  await expect(input).toHaveValue('some value hello');
-
-  // Wait for undo stack to be updated
-  await page.waitForTimeout(600);
-
-  // Undo changes
-  await page.keyboard.press('Control+Z');
-
-  // Asssert that batched changes were reverted
-  await expect(input).toHaveValue('some value');
 });

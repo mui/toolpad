ce44d4b07f09a14872df793dc04f5271cb68e3ff
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2022-12-15 18:04:00 +0000

    Prevent native undo/redo behavior

--- packages/toolpad-app/src/toolpad/hooks/useUndoRedo.ts
+++ packages/toolpad-app/src/toolpad/hooks/useUndoRedo.ts
@@ -1,18 +1,13 @@
 import { useDomApi } from '../DomLoader';
-import { hasFieldFocus } from '../../utils/fields';
 import useShortcut from '../../utils/useShortcut';
 
 export default function useUndoRedo() {
   const domApi = useDomApi();
 
-  useShortcut({ key: 'z', metaKey: true, preventDefault: false }, () => {
-    if (!hasFieldFocus()) {
-      domApi.undo();
-    }
+  useShortcut({ key: 'z', metaKey: true, preventDefault: true }, () => {
+    domApi.undo();
   });
-  useShortcut({ key: 'z', metaKey: true, shiftKey: true, preventDefault: false }, () => {
-    if (!hasFieldFocus()) {
-      domApi.redo();
-    }
+  useShortcut({ key: 'z', metaKey: true, shiftKey: true, preventDefault: true }, () => {
+    domApi.redo();
   });
 }

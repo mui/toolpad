4422494aecb2950b058553c1e9b2f70acd405af6
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2022-12-15 18:36:26 +0000

    Enable native undo/redo in all views except page

--- packages/toolpad-app/src/toolpad/hooks/useUndoRedo.ts
+++ packages/toolpad-app/src/toolpad/hooks/useUndoRedo.ts
@@ -1,13 +1,36 @@
-import { useDomApi } from '../DomLoader';
+import * as React from 'react';
+import { useDom, useDomApi } from '../DomLoader';
 import useShortcut from '../../utils/useShortcut';
+import { hasFieldFocus } from '../../utils/fields';
 
 export default function useUndoRedo() {
   const domApi = useDomApi();
+  const { currentView } = useDom();
 
-  useShortcut({ key: 'z', metaKey: true, preventDefault: true }, () => {
-    domApi.undo();
-  });
-  useShortcut({ key: 'z', metaKey: true, shiftKey: true, preventDefault: true }, () => {
-    domApi.redo();
-  });
+  const handleUndo = React.useCallback(
+    (event: KeyboardEvent) => {
+      if (currentView.kind === 'page') {
+        event.preventDefault();
+        domApi.undo();
+      } else if (!hasFieldFocus()) {
+        domApi.undo();
+      }
+    },
+    [currentView.kind, domApi],
+  );
+
+  const handleRedo = React.useCallback(
+    (event: KeyboardEvent) => {
+      if (currentView.kind === 'page') {
+        event.preventDefault();
+        domApi.redo();
+      } else if (!hasFieldFocus()) {
+        domApi.redo();
+      }
+    },
+    [currentView.kind, domApi],
+  );
+
+  useShortcut({ key: 'z', metaKey: true, preventDefault: false }, handleUndo);
+  useShortcut({ key: 'z', metaKey: true, shiftKey: true, preventDefault: false }, handleRedo);
 }

e01badd94937579b46b26a77ca32dee9fef66a89
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-12-21 15:44:36 +0100

    Reviver type

--- test/utils/fs.ts
+++ test/utils/fs.ts
@@ -1,9 +1,8 @@
 import * as fs from 'fs/promises';
 
-export async function readJsonFile(
-  path: string,
-  reviver?: (this: any, key: string, value: any) => any,
-): Promise<any> {
+export type Reviver = NonNullable<Parameters<typeof JSON.parse>[1]>;
+
+export async function readJsonFile(path: string, reviver?: Reviver): Promise<any> {
   const content = await fs.readFile(path, { encoding: 'utf-8' });
   return JSON.parse(content, reviver);
 }

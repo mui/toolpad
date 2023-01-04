459a0ffbc226470bcb795c48c42384fc6678807d
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2023-01-04 13:26:43 +0100

    Fix function editor save (#1507)

--- packages/toolpad-app/src/utils/react.tsx
+++ packages/toolpad-app/src/utils/react.tsx
@@ -1,3 +1,4 @@
+import mitt from 'mitt';
 import * as React from 'react';
 
 /**
@@ -33,4 +34,35 @@ export function interleave(items: React.ReactNode[], separator: React.ReactNode)
   }
 
   return result;
+}
+
+/**
+ * Create a shared state to be used across the application. Returns a useState hook that
+ * is synchronised on the same state between all instances where it is called.
+ */
+export function createGlobalState<T = undefined>(initialValue: T) {
+  const emitter = mitt();
+  let result: [T, React.Dispatch<React.SetStateAction<T>>];
+  const setState: React.Dispatch<React.SetStateAction<T>> = (newValue) => {
+    const updateValue =
+      typeof newValue === 'function' ? (newValue as (newValue: T) => T)(result[0]) : newValue;
+
+    if (updateValue !== result[0]) {
+      result = [updateValue, setState];
+      emitter.emit('change');
+    }
+  };
+  result = [initialValue, setState];
+  return function useGlobalState() {
+    return React.useSyncExternalStore<[T, React.Dispatch<React.SetStateAction<T>>]>(
+      (cb) => {
+        emitter.on('change', cb);
+        return () => {
+          emitter.off('change', cb);
+        };
+      },
+      () => result,
+      () => result,
+    );
+  };
 }

eae7102febb25ec1f889f774406766ebef6d622c
bytasv <vytautas.butkus@gmail.com>
2023-01-13 17:13:23 +0200

    Add sx prop to container

--- packages/toolpad-components/src/Container.tsx
+++ packages/toolpad-components/src/Container.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
-import { Container as MUIContainer } from '@mui/material';
+import { Container as MUIContainer, ContainerProps } from '@mui/material';
 import { createComponent } from '@mui/toolpad-core';
+import { SX_PROP_HELPER_TEXT } from './constants';
 
-interface Props {
-  children: React.ReactNode;
+interface Props extends ContainerProps {
   visible: boolean;
 }
 
-function Container({ children, visible, ...props }: Props) {
+function Container({ children, visible, sx, ...props }: Props) {
   return visible ? (
-    <MUIContainer disableGutters sx={{ padding: 1 }} {...props}>
+    <MUIContainer disableGutters sx={sx} {...props}>
       {children}
     </MUIContainer>
   ) : null;
@@ -25,6 +25,11 @@ export default createComponent(Container, {
       typeDef: { type: 'boolean' },
       defaultValue: true,
       helperText: 'Control whether container element is visible.',
+    },
+    sx: {
+      helperText: SX_PROP_HELPER_TEXT,
+      typeDef: { type: 'object' },
+      defaultValue: { padding: 1, border: 'solid 1px' },
     },
   },
 });

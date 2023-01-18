bc5dee26bb65007787f8d56a44016891a082ab19
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2023-01-18 18:28:03 +0000

    Attempt to fix CI tests

--- test/integration/pages/index.spec.ts
+++ test/integration/pages/index.spec.ts
@@ -12,8 +12,10 @@ test('must load page in initial URL without altering URL', async ({ page, browse
   });
 
   const editorModel = new ToolpadEditor(page, browserName);
+
   await page.goto(`/_toolpad/app/${app.id}/pages/g433ywb?abcd=123`);
-  await page.waitForNavigation();
+
+  await editorModel.pageRoot.waitFor();
 
   const pageButton2 = editorModel.appCanvas.getByRole('button', {
     name: 'page2Button',

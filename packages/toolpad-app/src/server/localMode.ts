b264058cdb40334afb924db1c05d78137b915041
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2023-01-24 19:11:36 +0100

    Load components from disk

--- packages/toolpad-app/src/server/localMode.ts
+++ packages/toolpad-app/src/server/localMode.ts
@@ -13,17 +13,96 @@ export function getConfigFilePath() {
   return filePath;
 }
 
-export async function saveLocalDom(app: appDom.AppDom): Promise<void> {
+type ComponentsContent = Record<string, string>;
+
+function getComponentFolder(configFilePath: string): string {
+  return path.resolve(path.dirname(configFilePath), './toolpad-components');
+}
+
+async function loadCodeComponentsFromFiles(componentsFolder: string): Promise<ComponentsContent> {
+  const entries = await fs.readdir(componentsFolder, { withFileTypes: true });
+  const resultEntries = await Promise.all(
+    entries.map(async (entry): Promise<[string, string] | null> => {
+      if (entry.isFile()) {
+        const fileName = entry.name;
+        const componentName = entry.name.replace(/\.tsx$/, '');
+        const filePath = path.resolve(componentsFolder, fileName);
+        const content = await fs.readFile(filePath, { encoding: 'utf-8' });
+        return [componentName, content];
+      }
+
+      return null;
+    }),
+  );
+
+  return Object.fromEntries(resultEntries.filter(Boolean));
+}
+
+async function writeCodeComponentsToFiles(
+  componentsFolder: string,
+  components: ComponentsContent,
+): Promise<void> {
+  await fs.mkdir(componentsFolder, { recursive: true });
+  await Promise.all(
+    Object.entries(components).map(async ([componentName, content]) => {
+      const filePath = path.resolve(componentsFolder, `${componentName}.tsx`);
+      await fs.writeFile(filePath, content, { encoding: 'utf-8' });
+    }),
+  );
+}
+
+function mergeComponentsContentIntoDom(
+  dom: appDom.AppDom,
+  componentsContent: ComponentsContent,
+): appDom.AppDom {
+  const rootNode = appDom.getApp(dom);
+  const { codeComponents: codeComponentNodes = [] } = appDom.getChildNodes(dom, rootNode);
+  for (const node of codeComponentNodes) {
+    const content = componentsContent[node.name];
+    if (content) {
+      dom = appDom.setNodeNamespacedProp(
+        dom,
+        node,
+        'attributes',
+        'code',
+        appDom.createConst(content),
+      );
+    }
+  }
+  return dom;
+}
+
+function extractComponentsContentFromDom(dom: appDom.AppDom): ComponentsContent {
+  const rootNode = appDom.getApp(dom);
+  const { codeComponents: codeComponentNodes = [] } = appDom.getChildNodes(dom, rootNode);
+  return Object.fromEntries(
+    codeComponentNodes.map((node) => [node.name, node.attributes.code.value]),
+  );
+}
+
+export async function saveLocalDom(dom: appDom.AppDom): Promise<void> {
   const filePath = getConfigFilePath();
-  await fs.writeFile(filePath, yaml.stringify(app), { encoding: 'utf-8' });
+  const componentsFolder = getComponentFolder(filePath);
+  const componentsContent = extractComponentsContentFromDom(dom);
+  await Promise.all([
+    fs.writeFile(filePath, yaml.stringify(dom), { encoding: 'utf-8' }),
+    writeCodeComponentsToFiles(componentsFolder, componentsContent),
+  ]);
 }
 
 export async function loadLocalDomFromFile(filePath: string): Promise<appDom.AppDom> {
+  const componentsFolder = getComponentFolder(filePath);
   try {
-    const content = await fs.readFile(filePath, { encoding: 'utf-8' });
-    return yaml.parse(content);
+    const [configContent, componentsContent] = await Promise.all([
+      fs.readFile(filePath, { encoding: 'utf-8' }),
+      loadCodeComponentsFromFiles(componentsFolder),
+    ]);
+    const parsedConfig = yaml.parse(configContent);
+    const dom = mergeComponentsContentIntoDom(parsedConfig, componentsContent);
+    return dom;
   } catch (rawError) {
     const error = errorFrom(rawError);
+    console.log(error);
     if (error.code === 'ENOENT') {
       const dom = appDom.createDefaultDom();
       await saveLocalDom(dom);

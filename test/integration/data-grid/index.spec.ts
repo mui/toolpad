1be2175e6990d7d7ea59b737da134ec7ae365ed9
Bharat Kashyap <bharatkashyap@outlook.com>
2023-01-27 16:24:40 +0530

    Add: Column prop update test

--- test/integration/data-grid/index.spec.ts
+++ test/integration/data-grid/index.spec.ts
@@ -2,6 +2,7 @@ import * as path from 'path';
 import { ToolpadEditor } from '../../models/ToolpadEditor';
 import { test, expect } from '../../playwright/test';
 import { readJsonFile } from '../../utils/fs';
+import clickCenter from '../../utils/clickCenter';
 import generateId from '../../utils/generateId';
 
 test('Code component cell', async ({ page, browserName, api }) => {
@@ -23,4 +24,51 @@ test('Code component cell', async ({ page, browserName, api }) => {
     ),
   ).toBeVisible();
   await expect(editorModel.pageRoot.getByText('field: "customField"')).toBeVisible();
+});
+
+test('Column prop updates are not lost on drag interactions', async ({
+  page,
+  browserName,
+  api,
+}) => {
+  const app = await api.mutation.createApp(`App ${generateId()}`, {
+    from: { kind: 'template', id: 'default' },
+  });
+
+  const editorModel = new ToolpadEditor(page, browserName);
+  editorModel.goto(app.id);
+
+  await editorModel.pageRoot.waitFor({ state: 'visible' });
+
+  const canvasGridLocator = editorModel.appCanvas.getByRole('grid');
+
+  // Change the "Avatar" column type from "image" to "string"
+
+  const firstGridLocator = canvasGridLocator.first();
+  await clickCenter(page, firstGridLocator);
+
+  await editorModel.componentEditor.locator('button:has-text("columns")').click();
+
+  await editorModel.page.getByRole('button', { name: 'Avatar' }).click();
+
+  await editorModel.page.getByRole('button', { name: 'image' }).click();
+
+  await editorModel.page.getByRole('option', { name: 'string' }).click();
+
+  await clickCenter(page, firstGridLocator);
+
+  // Drag the "Avatar" column to the end of the grid
+
+  const avatarColumn = editorModel.pageRoot.getByText('Avatar', { exact: true });
+  const profileColumn = editorModel.pageRoot.getByText('Profile', { exact: true });
+
+  await avatarColumn.dragTo(profileColumn);
+
+  // Expect the "Avatar" column to continue to be of type "string" instead of "image"
+
+  await expect(
+    editorModel.pageRoot.getByRole('cell', {
+      name: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/793.jpg',
+    }),
+  ).toBeVisible();
 });

6bd8f686d3f92afb20ff948b184b5307ea6ded96
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2023-02-01 11:24:22 +0100

    fixz

--- test/integration/components/index.spec.ts
+++ test/integration/components/index.spec.ts
@@ -7,28 +7,29 @@ import { readJsonFile } from '../../utils/fs';
 import generateId from '../../utils/generateId';
 
 async function waitForComponents(page: Page, frame: Page | FrameLocator = page, isEditor = false) {
-  await frame.locator('text="foo button"').waitFor({ state: 'visible' });
-  await frame.locator('img[alt="foo image"]').waitFor({ state: 'attached' });
-  await frame.locator('text="foo datagrid column"').waitFor({ state: 'visible' });
-  await frame.locator('text="custom component 1"').waitFor({ state: 'visible' });
-  await frame.locator('label:has-text("foo textfield")').waitFor({ state: 'visible' });
-  await frame.locator('text="foo typography"').waitFor({ state: 'visible' });
-  await frame.locator('label:has-text("foo select")').waitFor({ state: 'visible' });
+  const button = frame.locator('text="foo button"');
+  await button.waitFor({ state: 'visible' });
 
-  const optionsSelect = frame.getByRole('button', { name: /select with options/ });
-  await optionsSelect.scrollIntoViewIfNeeded();
-  if (isEditor) {
-    await clickCenter(page, optionsSelect);
-  }
-  await clickCenter(page, optionsSelect);
-  await expect(frame.getByRole('option', { name: 'one' })).toBeVisible();
-  await expect(frame.getByRole('option', { name: '2' })).toBeVisible();
-  await expect(frame.getByRole('option', { name: 'three' })).toBeVisible();
-  await expect(frame.getByRole('option', { name: '4' })).toBeVisible();
-  await expect(frame.getByRole('option', { name: 'undefined' })).toBeVisible();
+  const image = frame.locator('img[alt="foo image"]');
+  await image.waitFor({ state: 'attached' });
+
+  const datagrid = frame.locator('text="foo datagrid column"');
+  await datagrid.waitFor({ state: 'visible' });
+
+  const customComponent = frame.locator('text="custom component 1"');
+  await customComponent.waitFor({ state: 'visible' });
+
+  const textField = frame.locator('label:has-text("foo textfield")');
+  await textField.waitFor({ state: 'visible' });
+
+  const text = frame.locator('text="foo typography"');
+  await text.waitFor({ state: 'visible' });
+
+  const select = frame.locator('label:has-text("foo select")');
+  await select.waitFor({ state: 'visible' });
 }
 
-test('components runtime', async ({ page, api }) => {
+test('rendering components in the app runtime', async ({ page, api }) => {
   const dom = await readJsonFile(path.resolve(__dirname, './componentsDom.json'));
 
   const app = await api.mutation.createApp(`App ${generateId()}`, {
@@ -41,7 +42,7 @@ test('components runtime', async ({ page, api }) => {
   await waitForComponents(page);
 });
 
-test('components editor', async ({ page, browserName, api }) => {
+test('rendering components in the app editor', async ({ page, browserName, api }) => {
   const dom = await readJsonFile(path.resolve(__dirname, './componentsDom.json'));
 
   const app = await api.mutation.createApp(`App ${generateId()}`, {
@@ -52,4 +53,24 @@ test('components editor', async ({ page, browserName, api }) => {
   editorModel.goto(app.id);
 
   await waitForComponents(page, editorModel.appCanvas, true);
+});
+
+test('select component behavior', async ({ page, api }) => {
+  const dom = await readJsonFile(path.resolve(__dirname, './componentsDom.json'));
+
+  const app = await api.mutation.createApp(`App ${generateId()}`, {
+    from: { kind: 'dom', dom },
+  });
+
+  const runtimeModel = new ToolpadRuntime(page);
+  await runtimeModel.gotoPage(app.id, 'select');
+
+  const optionsSelect = page.getByRole('button', { name: /select with options/ });
+  await optionsSelect.scrollIntoViewIfNeeded();
+  await clickCenter(page, optionsSelect);
+  await expect(page.getByRole('option', { name: 'one' })).toBeVisible();
+  await expect(page.getByRole('option', { name: '2' })).toBeVisible();
+  await expect(page.getByRole('option', { name: 'three' })).toBeVisible();
+  await expect(page.getByRole('option', { name: '4' })).toBeVisible();
+  await expect(page.getByRole('option', { name: 'undefined' })).toBeVisible();
 });

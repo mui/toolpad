57a6847f6e1c5185e16ca462a1afe6a726b81b5a
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2023-02-01 11:49:13 +0100

    Update ToolpadEditor.ts

--- test/models/ToolpadEditor.ts
+++ test/models/ToolpadEditor.ts
@@ -148,43 +148,11 @@ export class ToolpadEditor {
     const appCanvasFrame = this.page.frame('data-toolpad-canvas');
     expect(appCanvasFrame).toBeDefined();
 
-    // Source drag event needs to be dispatched manually in Firefox for tests to work (Playwright bug)
-    // https://github.com/microsoft/playwright/issues/17441
-    const isFirefox = this.browserName === 'firefox';
-    if (isFirefox) {
-      if (isSourceInCanvas) {
-        const dataTransfer = await appCanvasFrame!.evaluateHandle(() => new DataTransfer());
-        await appCanvasFrame!.dispatchEvent(sourceSelector, 'dragstart', { dataTransfer });
-      } else {
-        const dataTransfer = await this.page.evaluateHandle(() => new DataTransfer());
-        await this.page.dispatchEvent(sourceSelector, 'dragstart', { dataTransfer });
-      }
-    } else {
-      await this.page.mouse.down();
-    }
+    await this.page.mouse.down();
 
     await this.page.mouse.move(moveTargetX, moveTargetY, { steps: 10 });
 
-    // Overlay drag events need to be dispatched manually in Firefox for tests to work (Playwright bug)
-    // https://github.com/microsoft/playwright/issues/17441
-    if (isFirefox) {
-      const pageOverlayBoundingBox = await this.pageOverlay.boundingBox();
-
-      expect(pageOverlayBoundingBox).toBeDefined();
-
-      const eventMousePosition = {
-        clientX: moveTargetX - pageOverlayBoundingBox!.x,
-        clientY: moveTargetY - pageOverlayBoundingBox!.y,
-      };
-
-      const pageOverlaySelector = 'data-testid=page-overlay';
-
-      await appCanvasFrame!.dispatchEvent(pageOverlaySelector, 'dragover', eventMousePosition);
-      await appCanvasFrame!.dispatchEvent(pageOverlaySelector, 'drop', eventMousePosition);
-      await appCanvasFrame!.dispatchEvent(pageOverlaySelector, 'dragend');
-    } else {
-      await this.page.mouse.up();
-    }
+    await this.page.mouse.up();
   }
 
   async dragNewComponentToAppCanvas(componentName: string) {

7486cba0d3ba173ac0347e59534ea4db1eaeba49
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2023-02-20 19:07:46 +0000

    Refactor, improve readability

--- packages/toolpad-app/src/toolpad/AppState.tsx
+++ packages/toolpad-app/src/toolpad/AppState.tsx
@@ -249,26 +249,24 @@ export function appStateReducer(state: AppState, action: AppStateAction): AppSta
       }
 
       let newView = action.view;
-      if (action.view.kind === 'page' && typeof action.view.selectedNodeId === 'undefined') {
-        const isSamePage =
-          action.view.kind === 'page' && action.view.nodeId === state.currentView.nodeId;
+      if (action.view.kind === 'page') {
+        if (typeof action.view.selectedNodeId === 'undefined') {
+          const isSameNode = action.view.nodeId === state.currentView.nodeId;
 
-        newView = {
-          ...action.view,
-          selectedNodeId:
-            state.currentView.kind === 'page' && isSamePage
-              ? state.currentView.selectedNodeId
-              : null,
-        };
-      } else if (
-        action.view.kind === 'page' &&
-        action.view.selectedNodeId &&
-        typeof action.view.tab === 'undefined'
-      ) {
-        newView = {
-          ...action.view,
-          tab: 'component',
-        };
+          newView = {
+            ...action.view,
+            selectedNodeId:
+              state.currentView.kind === 'page' && isSameNode
+                ? state.currentView.selectedNodeId
+                : null,
+          };
+        }
+        if (action.view.selectedNodeId && typeof action.view.tab === 'undefined') {
+          newView = {
+            ...action.view,
+            tab: 'component',
+          };
+        }
       }
 
       return update(state, {

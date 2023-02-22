db76d7f21d94cce8fe613b3d49c3fce995f124e8
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2023-02-22 09:48:19 +0100

    this

--- docs/babel.config.js
+++ docs/babel.config.js
@@ -26,12 +26,6 @@ module.exports = {
         },
       },
     ],
-    [
-      'babel-plugin-transform-rename-import',
-      {
-        replacements: [{ original: '@mui/utils/macros/MuiError.macro', replacement: 'react' }],
-      },
-    ],
     'babel-plugin-optimize-clsx',
     // for IE 11 support
     '@babel/plugin-transform-object-assign',

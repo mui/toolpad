c1c66f186b0599fa036aeffc1c775a56475e7638
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2023-02-27 19:22:20 +0000

    Infer createQuery parameter types from config

--- packages/toolpad-core/src/createQuery.tsx
+++ packages/toolpad-core/src/createQuery.tsx
@@ -1,30 +1,58 @@
 import { TOOLPAD_QUERY } from './constants.js';
-import { PropValueType } from './types.js';
+import {
+  PrimitiveValueType,
+  NumberValueType,
+  BooleanValueType,
+  ArrayValueType,
+  ObjectValueType,
+  StringValueType,
+} from './types.js';
 
-export interface QueryResolverParams<P> {
-  parameters: P;
-}
+export type QueryParameterValue<V extends PrimitiveValueType['default']> = V extends NumberValueType
+  ? number
+  : V extends StringValueType
+  ? string
+  : V extends BooleanValueType
+  ? boolean
+  : V extends ArrayValueType
+  ? unknown[]
+  : V extends ObjectValueType
+  ? Record<string, unknown>
+  : never;
 
-export interface QueryResolver<P> {
-  (params: QueryResolverParams<P>): Promise<any>;
-}
-
-export interface QueryParameterConfig<P, K extends keyof P> {
-  typeDef: PropValueType;
+export interface QueryParameterConfig<P extends Record<string, unknown>, K extends keyof P> {
+  typeDef: PrimitiveValueType;
   defaultValue?: P[K];
 }
 
-export interface CreateQueryConfig<P> {
+export interface CreateQueryConfig<P extends Record<string, unknown>> {
   parameters: {
     [K in keyof P]: QueryParameterConfig<P, K>;
   };
 }
 
-export interface ToolpadQuery<P> extends QueryResolver<P> {
-  [TOOLPAD_QUERY]: CreateQueryConfig<P>;
+type CreateQueryConfigParameters<C extends CreateQueryConfig<CreateQueryConfigParameters<C>>> =
+  QueryResolverParams<C>['parameters'];
+
+export interface QueryResolverParams<C extends CreateQueryConfig<CreateQueryConfigParameters<C>>> {
+  parameters: {
+    [K in keyof C['parameters']]: QueryParameterValue<C['parameters'][K]['typeDef']>;
+  };
 }
 
-export default function createQuery<P>(resolver: QueryResolver<P>, config?: CreateQueryConfig<P>) {
+export interface QueryResolver<C extends CreateQueryConfig<CreateQueryConfigParameters<C>>> {
+  (params: QueryResolverParams<C>): Promise<unknown>;
+}
+
+export interface ToolpadQuery<C extends CreateQueryConfig<CreateQueryConfigParameters<C>>>
+  extends QueryResolver<C> {
+  [TOOLPAD_QUERY]: C;
+}
+
+export default function createQuery<C extends CreateQueryConfig<CreateQueryConfigParameters<C>>>(
+  resolver: QueryResolver<C>,
+  config?: C,
+) {
   return Object.assign(resolver, {
     [TOOLPAD_QUERY]: config || { parameters: {} },
   });

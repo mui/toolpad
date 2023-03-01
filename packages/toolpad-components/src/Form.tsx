86271e7f958bf3b80a2942b45251763fc40d7741
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2023-03-01 19:13:21 +0000

    Store form values in scope

--- packages/toolpad-components/src/Form.tsx
+++ packages/toolpad-components/src/Form.tsx
@@ -9,21 +9,27 @@ import { SX_PROP_HELPER_TEXT } from './constants';
 export const FormContext = React.createContext<ReturnType<typeof useForm> | null>(null);
 
 interface FormProps extends ContainerProps {
+  value: FieldValues;
+  onChange: (newValue: FieldValues) => void;
   validationSchema: Yup.AnyObjectSchema;
-  onSubmit: (data: FieldValues) => unknown | Promise<unknown>;
+  onSubmit: (data?: FieldValues) => unknown | Promise<unknown>;
 }
 
-function Form({ children, onSubmit, validationSchema, sx, ...rest }: FormProps) {
+function Form({ children, onChange, onSubmit, validationSchema, sx, ...rest }: FormProps) {
   const form = useForm({
     resolver: yupResolver(validationSchema),
   });
 
-  const handleSubmit = React.useCallback(
-    (data: Record<string, unknown>) => {
-      onSubmit(data);
-    },
-    [onSubmit],
-  );
+  const handleSubmit = React.useCallback(() => {
+    onSubmit();
+  }, [onSubmit]);
+
+  React.useEffect(() => {
+    const formSubscription = form.watch((value) => {
+      onChange(value);
+    });
+    return () => formSubscription.unsubscribe();
+  }, [form, onChange]);
 
   return (
     <Container disableGutters sx={sx} {...rest}>
@@ -47,13 +53,20 @@ export default createComponent(Form, {
       typeDef: { type: 'element' },
       control: { type: 'layoutSlot' },
     },
+    value: {
+      helperText: 'The value that is controlled by this text input.',
+      typeDef: { type: 'object', default: {} },
+      onChangeProp: 'onChange',
+    },
     validationSchema: {
       helperText: 'Form [Yup](https://www.npmjs.com/package/yup) validation schema.',
-      typeDef: { type: 'object', default: Yup.object({}) },
+      typeDef: { type: 'object', default: Yup.object() },
     },
     onSubmit: {
       helperText: 'Add logic to be executed when the user submits the form.',
-      typeDef: { type: 'event' },
+      typeDef: {
+        type: 'event',
+      },
     },
     sx: {
       helperText: SX_PROP_HELPER_TEXT,

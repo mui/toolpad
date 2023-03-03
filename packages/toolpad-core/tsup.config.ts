bc95f1d6fcbbceb7ef69fb2e8211a02b19087f99
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2023-03-03 13:26:44 +0100

    more

--- packages/toolpad-core/tsup.config.ts
+++ packages/toolpad-core/tsup.config.ts
@@ -1,7 +1,11 @@
+import { spawnSync } from 'child_process';
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/*.ts', 'src/*.tsx', 'src/utils/*.ts', 'src/utils/*.tsx'],
   format: ['esm', 'cjs'],
-  dts: true,
+  dts: false,
+  async onSuccess() {
+    spawnSync('tsc', ['--emitDeclarationOnly', '--declaration']);
+  },
 });
--- packages/toolpad/tsup.config.ts
+++ packages/toolpad/tsup.config.ts
@@ -1,7 +1,11 @@
+import { spawnSync } from 'child_process';
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/*.ts', 'src/*.tsx'],
   format: ['esm', 'cjs'],
-  dts: true,
+  dts: false,
+  async onSuccess() {
+    spawnSync('tsc', ['--emitDeclarationOnly', '--declaration']);
+  },
 });

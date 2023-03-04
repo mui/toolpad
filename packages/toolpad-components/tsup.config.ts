cfef66a69eee20e8e101b7c93f0691a51be7f5f3
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2023-03-04 07:52:01 +0100

    fix

--- packages/toolpad-components/tsup.config.ts
+++ packages/toolpad-components/tsup.config.ts
@@ -5,6 +5,14 @@ export default defineConfig({
   entry: ['src/*.ts', 'src/*.tsx'],
   format: ['esm', 'cjs'],
   dts: false,
+  esbuildOptions: (options) => {
+    // jest can't handle untranspiled import()
+    // https://github.com/evanw/esbuild/issues/2966
+    options.supported ??= {};
+    options.supported['dynamic-import'] = false;
+
+    return options;
+  },
   async onSuccess() {
     spawnSync('tsc', ['--emitDeclarationOnly', '--declaration']);
   },

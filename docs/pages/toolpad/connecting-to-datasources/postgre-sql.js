d5fe24c4ce266dd3c697c46fc3116501345478d9
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2023-03-15 18:09:25 +0100

    name

--- docs/pages/toolpad/connecting-to-datasources/postgre-sql.js
+++ docs/pages/toolpad/connecting-to-datasources/postgre-sql.js
@@ -4,7 +4,7 @@ import {
   demos,
   docs,
   demoComponents,
-} from '../../../data/toolpad/connecting-to-datasources/postgre-sql.md?@mui/markdown';
+} from '../../../data/toolpad/connecting-to-datasources/postgresql.md?@mui/markdown';
 
 export default function Page() {
   return <MarkdownDocs demos={demos} docs={docs} demoComponents={demoComponents} disableAd />;

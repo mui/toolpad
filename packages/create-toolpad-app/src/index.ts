d88f45b93dfc907ccf7e1bd51d5729e710271762
Bharat Kashyap <bharatkashyap@outlook.com>
2023-03-17 09:11:48 -0400

    Fix: Review

--- packages/create-toolpad-app/src/index.ts
+++ packages/create-toolpad-app/src/index.ts
@@ -5,43 +5,72 @@ import { execaCommand } from 'execa';
 import inquirer from 'inquirer';
 import * as fs from 'fs/promises';
 import path from 'path';
-import { getPackageManager } from './utils/getPackageManager';
-import { isFolderEmpty } from './utils/isFolderEmpty';
+
+type PackageManager = 'npm' | 'pnpm' | 'yarn';
+
+function getPackageManager(): PackageManager {
+  const userAgent = process.env.npm_config_user_agent;
+
+  if (userAgent) {
+    if (userAgent.startsWith('yarn')) {
+      return 'yarn';
+    }
+    if (userAgent.startsWith('pnpm')) {
+      return 'pnpm';
+    }
+    if (userAgent.startsWith('npm')) {
+      return 'npm';
+    }
+  }
+  return 'yarn';
+}
+
+// From https://github.com/vercel/next.js/blob/canary/packages/create-next-app/helpers/is-folder-empty.ts
+
+async function isFolderEmpty(root: string, name: string): Promise<boolean> {
+  const files = await fs.readdir(root);
+
+  if (files.length > 0) {
+    // eslint-disable-next-line no-console
+    console.log(`The directory ${chalk.green(name)} is non-empty.`);
+    // eslint-disable-next-line no-console
+    console.log();
+    // eslint-disable-next-line no-console
+    console.log('Either try using a new directory name, or remove the files listed above.');
+    // eslint-disable-next-line no-console
+    console.log();
+    return false;
+  }
+
+  return true;
+}
+
 // Detect the package manager
 const packageManager = getPackageManager();
 
 // Install the dependencies
-const installDeps = async () => {
+const installDeps = async (projectName: string, cwd: string) => {
   // eslint-disable-next-line no-console
   console.log(`${chalk.blue('info')} - Installing dependencies`);
 
   const installVerb = packageManager === 'yarn' ? 'add' : 'install';
   const command = `${packageManager} ${installVerb} @mui/toolpad @mui/toolpad-core`;
-  await execaCommand(command, { stdio: 'inherit' });
+  await execaCommand(command, { stdio: 'inherit', cwd: path.join(cwd, projectName) });
 
   // eslint-disable-next-line no-console
   console.log(`${chalk.green('success')} - Dependencies installed successfully!`);
 };
 
 // Create a new directory and initialize a new project
-const scaffoldProject = async () => {
-  const name = await inquirer.prompt([
-    {
-      type: 'input',
-      name: 'projectName',
-      message: 'Enter the name of your project:',
-      default: 'my-toolpad-app',
-    },
-  ]);
-
+const scaffoldProject = async (projectName: string, cwd: string): Promise<string | null> => {
   // eslint-disable-next-line no-console
-  console.log(`Creating a new MUI Toolpad project in ${chalk.blue(name.projectName)}`);
+  console.log(`Creating a new MUI Toolpad project in ${chalk.blue(projectName)}`);
   try {
-    await fs.mkdir(name.projectName);
-    process.chdir(name.projectName);
+    await fs.mkdir(projectName);
+    // process.chdir(projectName);
 
     const packageJson = {
-      name: name.projectName,
+      name: projectName,
       version: '0.1.0',
       private: true,
       scripts: {
@@ -51,15 +80,15 @@ const scaffoldProject = async () => {
       },
     };
 
-    await fs.writeFile('package.json', JSON.stringify(packageJson, null, 2));
-    return name.projectName;
+    await fs.writeFile(`./${projectName}/package.json`, JSON.stringify(packageJson, null, 2));
+    return projectName;
   } catch (error) {
     // Directory exists, verify if it is empty to continue
-    if (!isFolderEmpty(path.join(process.cwd(), name.projectName), name.projectName)) {
+    if (!isFolderEmpty(path.join(cwd, projectName), projectName)) {
       return null;
     }
     try {
-      await fs.access(name.projectName);
+      await fs.access(projectName);
       const installDependenciesConsent = await inquirer.prompt([
         {
           type: 'confirm',
@@ -72,15 +101,13 @@ const scaffoldProject = async () => {
       ]);
 
       if (installDependenciesConsent.installInExisting) {
-        return name.projectName;
+        return projectName;
       }
       console.error(`${chalk.red('error')} - Dependencies are required to be installed.`);
       process.exit(1);
     } catch (err) {
       console.error(
-        `Unable to create directory ${chalk.red(
-          name.projectName,
-        )}. Please provide a different name.`,
+        `Unable to create directory ${chalk.red(projectName)}. Please provide a different name.`,
       );
     }
     return null;
@@ -90,12 +117,23 @@ const scaffoldProject = async () => {
 // Run the CLI interaction with Inquirer.js
 const run = async () => {
   let projectName;
+  const cwd = process.cwd();
   do {
     // eslint-disable-next-line no-await-in-loop
-    projectName = await scaffoldProject();
+    const name = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'projectName',
+        message: 'Enter the name of your project:',
+        default: 'my-toolpad-app',
+      },
+    ]);
+
+    // eslint-disable-next-line no-await-in-loop
+    projectName = await scaffoldProject(name.projectName, cwd);
   } while (!projectName);
 
-  await installDeps();
+  await installDeps(projectName, cwd);
 
   const message = `\nRun the following to get started: \n\n ${chalk.magentaBright(
     `cd ${projectName} && ${packageManager}${packageManager === 'yarn' ? '' : ' run'} dev`,

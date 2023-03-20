816f0693c201b8d78662f377f21f74dfceeef9fd
prakhargupta1 <prakhar@mui.com>
2023-03-20 11:58:49 +0100

    Small typo fix

--- docs/src/content/marquee.js
+++ docs/src/content/marquee.js
@@ -1,7 +1,7 @@
 import ROUTES from '../route';
 
 const marquee = {
-  title: 'Import in your exisitng codebase',
+  title: 'Import in your existing codebase',
   subtitle: 'Using a simple command, build your first app in a few minutes.',
   action: {
     href: ROUTES.toolpadQuickstart,

619b25608fef26b90537e9545583c320385d9698
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2023-03-21 19:00:19 +0100

    propControls

--- test/integration/propControls/index.spec.ts
+++ test/integration/propControls/index.spec.ts
@@ -1,91 +1,98 @@
 import * as path from 'path';
-import { test, expect } from '../../playwright/test';
+import { test, expect } from '../../playwright/localTest';
 import { ToolpadEditor } from '../../models/ToolpadEditor';
 import clickCenter from '../../utils/clickCenter';
-import { readJsonFile } from '../../utils/fs';
-import generateId from '../../utils/generateId';
+import { APP_ID_LOCAL_MARKER } from '../../../packages/toolpad-app/src/constants';
 
-test.skip(!!process.env.LOCAL_MODE_TESTS, 'These are hosted mode tests');
+test.skip(!process.env.LOCAL_MODE_TESTS, 'These are local mode tests');
 
-test('can control component prop values in properties control panel', async ({ page, api }) => {
-  const dom = await readJsonFile(path.resolve(__dirname, './domInput.json'));
-
-  const app = await api.mutation.createApp(`App ${generateId()}`, {
-    from: { kind: 'dom', dom },
+test.describe.only('basic', () => {
+  test.use({
+    localAppConfig: {
+      template: path.resolve(__dirname, './fixture-basic'),
+      cmd: 'dev',
+    },
   });
 
-  const editorModel = new ToolpadEditor(page);
+  test('can control component prop values in properties control panel', async ({ page }) => {
+    const editorModel = new ToolpadEditor(page);
 
-  await editorModel.goto(app.id);
+    await editorModel.goto(APP_ID_LOCAL_MARKER);
 
-  await editorModel.pageRoot.waitFor();
+    await editorModel.pageRoot.waitFor();
 
-  const canvasInputLocator = editorModel.appCanvas.locator('input');
+    const canvasInputLocator = editorModel.appCanvas.locator('input');
 
-  // Verify that initial prop control values are correct
+    await expect(editorModel.appCanvas.getByTestId('page-overlay')).toBeVisible();
+    // Verify that initial prop control values are correct
 
-  const firstInputLocator = canvasInputLocator.first();
-  await clickCenter(page, firstInputLocator);
+    const firstInputLocator = canvasInputLocator.first();
+    await clickCenter(page, firstInputLocator);
 
-  await editorModel.componentEditor
-    .locator('h6:has-text("Text field")')
-    .waitFor({ state: 'visible' });
+    await page.screenshot({ path: 'screenshot.png' });
+    await editorModel.componentEditor
+      .locator('h6:has-text("Text field")')
+      .waitFor({ state: 'visible' });
 
-  const labelControlInput = editorModel.componentEditor.getByLabel('label', { exact: true });
+    const labelControlInput = editorModel.componentEditor.getByLabel('label', { exact: true });
 
-  const labelControlInputValue = await labelControlInput.inputValue();
+    const labelControlInputValue = await labelControlInput.inputValue();
 
-  expect(labelControlInputValue).toBe('textField1');
+    expect(labelControlInputValue).toBe('textField1');
 
-  // Change component prop values directly
-  const TEST_VALUE_1 = 'value1';
-  const valueControl = editorModel.componentEditor.getByLabel('value', { exact: true });
-  expect(await valueControl.inputValue()).not.toBe(TEST_VALUE_1);
-  await firstInputLocator.fill(TEST_VALUE_1);
-  expect(await valueControl.inputValue()).toBe(TEST_VALUE_1);
+    // Change component prop values directly
+    const TEST_VALUE_1 = 'value1';
+    const valueControl = editorModel.componentEditor.getByLabel('value', { exact: true });
+    expect(await valueControl.inputValue()).not.toBe(TEST_VALUE_1);
+    await firstInputLocator.fill(TEST_VALUE_1);
+    expect(await valueControl.inputValue()).toBe(TEST_VALUE_1);
 
-  // Change component prop values through controls
-  const TEST_VALUE_2 = 'value2';
-  const inputByLabel = editorModel.appCanvas.getByLabel(TEST_VALUE_2, { exact: true });
-  await expect(inputByLabel).toHaveCount(0);
-  await labelControlInput.click();
-  await labelControlInput.fill('');
-  await labelControlInput.fill(TEST_VALUE_2);
+    // Change component prop values through controls
+    const TEST_VALUE_2 = 'value2';
+    const inputByLabel = editorModel.appCanvas.getByLabel(TEST_VALUE_2, { exact: true });
+    await expect(inputByLabel).toHaveCount(0);
+    await labelControlInput.click();
+    await labelControlInput.fill('');
+    await labelControlInput.fill(TEST_VALUE_2);
 
-  await inputByLabel.waitFor({ state: 'visible' });
+    await inputByLabel.waitFor({ state: 'visible' });
+  });
 });
 
-test('changing defaultValue resets controlled value', async ({ page, api }) => {
-  const dom = await readJsonFile(path.resolve(__dirname, './defaultValueDom.json'));
-
-  const app = await api.mutation.createApp(`App ${generateId()}`, {
-    from: { kind: 'dom', dom },
+test.describe('default values', () => {
+  test.use({
+    localAppConfig: {
+      template: path.resolve(__dirname, './fixture-defaults'),
+      cmd: 'dev',
+    },
   });
 
-  const editorModel = new ToolpadEditor(page);
-  await editorModel.goto(app.id);
+  test('changing defaultValue resets controlled value', async ({ page }) => {
+    const editorModel = new ToolpadEditor(page);
+    await editorModel.goto(APP_ID_LOCAL_MARKER);
 
-  await editorModel.waitForOverlay();
+    await editorModel.waitForOverlay();
 
-  const firstInput = editorModel.appCanvas.locator('input').nth(0);
-  const secondInput = editorModel.appCanvas.locator('input').nth(1);
+    const firstInput = editorModel.appCanvas.locator('input').nth(0);
+    const secondInput = editorModel.appCanvas.locator('input').nth(1);
 
-  await secondInput.focus();
+    await secondInput.focus();
 
-  await page.keyboard.type('Extra');
+    await page.keyboard.type('Extra');
 
-  await expect(firstInput).toHaveValue('defaultTwoExtra');
+    await expect(firstInput).toHaveValue('defaultTwoExtra');
 
-  await firstInput.focus();
+    await firstInput.focus();
 
-  await page.keyboard.type('Value');
+    await page.keyboard.type('Value');
 
-  await expect(firstInput).toHaveValue('defaultTwoExtraValue');
+    await expect(firstInput).toHaveValue('defaultTwoExtraValue');
 
-  clickCenter(page, secondInput);
+    clickCenter(page, secondInput);
 
-  await editorModel.componentEditor.getByLabel('defaultValue', { exact: true }).fill('New');
+    await editorModel.componentEditor.getByLabel('defaultValue', { exact: true }).fill('New');
 
-  await expect(firstInput).toHaveValue('New');
-  await expect(secondInput).toHaveValue('New');
+    await expect(firstInput).toHaveValue('New');
+    await expect(secondInput).toHaveValue('New');
+  });
 });

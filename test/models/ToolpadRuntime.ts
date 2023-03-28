0d5cc96379739ab33b9aa6ba03153da3fd37237f
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2023-03-28 18:47:29 +0100

    Improve go to page test method

--- test/models/ToolpadRuntime.ts
+++ test/models/ToolpadRuntime.ts
@@ -14,7 +14,9 @@ export class ToolpadRuntime {
 
   async gotoPage(pageName: string) {
     await this.goto();
-    await this.page.getByRole('button', { name: pageName }).click();
+
+    const navLocator = this.page.locator('nav');
+    await navLocator.getByRole('button', { name: pageName }).click();
   }
 
   async gotoPageById(appId: string, pageId: string) {

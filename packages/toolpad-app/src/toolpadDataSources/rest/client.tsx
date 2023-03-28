72636bc9970bb69edcb93fc9f24029251c52bb15
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2023-03-28 19:30:02 +0100

    Remove query from query scope

--- packages/toolpad-app/src/toolpadDataSources/rest/client.tsx
+++ packages/toolpad-app/src/toolpadDataSources/rest/client.tsx
@@ -62,10 +62,6 @@ import QueryPreview from '../QueryPreview';
 const HTTP_METHODS = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD'];
 
 const QUERY_SCOPE_META: ScopeMeta = {
-  query: {
-    deprecated: 'Use parameters variable instead',
-    description: 'Parameters that can be bound to app scope variables',
-  },
   parameters: {
     description: 'Parameters that can be bound to app scope variables',
   },
@@ -353,12 +349,12 @@ function QueryEditor({
 
   const queryScope = React.useMemo(
     () => ({
-      // TODO mark query as @deprecated remove after v1
-      query: previewParams,
       parameters: previewParams,
     }),
     [previewParams],
   );
+
+  console.log(queryScope);
 
   const liveUrl: LiveBinding = useEvaluateLiveBinding({
     jsRuntime: jsServerRuntime,

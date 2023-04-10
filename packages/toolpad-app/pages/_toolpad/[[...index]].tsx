b016d0dd2c3d6ace380f1ad134d75d0c97e1c494
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2023-04-10 20:53:36 +0100

    Fix build

--- packages/toolpad-app/pages/_toolpad/[[...index]].tsx
+++ packages/toolpad-app/pages/_toolpad/[[...index]].tsx
@@ -2,13 +2,13 @@ import type { GetServerSideProps, NextPage } from 'next';
 import * as React from 'react';
 import config from '../../src/config';
 import Toolpad from '../../src/toolpad';
-import { loadEnvFile } from '../../src/toolpadDataSources/local/server';
 
 export const getServerSideProps: GetServerSideProps<{}> = async () => {
   if (config.cmd !== 'dev') {
     return { notFound: true };
   }
 
+  const { loadEnvFile } = await import('../../src/toolpadDataSources/local/server');
   const envFile = await loadEnvFile();
   const envVarNames = Object.keys(envFile);
 

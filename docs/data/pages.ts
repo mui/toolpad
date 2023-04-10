61ddb78f194f81a2ffc2fa7af02526bc8a00a1d6
Prakhar Gupta <92228082+prakhargupta1@users.noreply.github.com>
2023-04-10 19:50:17 +0530

    Docs-update (#1829)
    Co-authored-by: Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>

--- docs/data/pages.ts
+++ docs/data/pages.ts
@@ -55,6 +55,10 @@ const pages: MuiPage[] = [
     pathname: '/toolpad/data-binding',
     icon: 'CodeIcon',
   },
+  {
+    pathname: '/toolpad/deployment',
+    icon: 'BuildIcon',
+  },
   // {
   //   pathname: '/toolpad/versioning-and-deploying',
   //   title: 'Versioning & deploying [TODO]',

69feee6bc054285ba0bd128abb287bc748649be4
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2023-04-14 13:51:54 -0300

    [website] Landing page design tweaks (#1786)
    Co-authored-by: Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
    Co-authored-by: Bharat Kashyap <bharatkashyap@outlook.com>

--- docs/src/components/landing/DemoVideo.js
+++ docs/src/components/landing/DemoVideo.js
@@ -1,55 +1,68 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import { styled } from '@mui/material/styles';
+import { styled, alpha } from '@mui/material/styles';
 
 const Video = styled('video')(({ theme }) => ({
-  borderStyle: 'solid',
-  borderColor:
-    theme.palette.mode === 'dark' ? theme.palette.primaryDark[900] : theme.palette.grey[100],
-  borderRadius: theme.shape.borderRadius,
   overflow: 'hidden',
   objectFit: 'cover',
   width: '100%',
   height: '100%',
+  borderRadius: '5px',
+  border: '1px solid',
+  borderColor:
+    theme.palette.mode === 'dark'
+      ? `${theme.palette.primaryDark[700]}`
+      : `${theme.palette.primary[100]}`,
 }));
 
 const VIDEO_BREAKPOINT_GAP = 100;
 
 const VideoContainer = styled(Box)(({ theme }) => ({
-  borderStyle: 'solid',
   maxWidth: 1100,
-  height: { xs: 200, sm: 390, md: 445, lg: 615 },
+  height: '100%',
   width: {
     xs: 360,
     sm: theme.breakpoints.values.sm - VIDEO_BREAKPOINT_GAP,
     md: theme.breakpoints.values.md + VIDEO_BREAKPOINT_GAP,
     lg: theme.breakpoints.values.lg + VIDEO_BREAKPOINT_GAP,
   },
-  borderColor:
-    theme.palette.mode === 'dark' ? theme.palette.primaryDark[400] : theme.palette.grey[300],
   borderRadius: theme.shape.borderRadius,
-  borderWidth: '1px',
+  padding: 16,
+  background:
+    theme.palette.mode === 'dark'
+      ? `linear-gradient(230deg, ${theme.palette.primaryDark[600]} 0%, ${alpha(
+          theme.palette.primaryDark[700],
+          0.4,
+        )} 150%)`
+      : `linear-gradient(230deg, ${theme.palette.primary[50]} 0%, ${alpha(
+          theme.palette.primary[100],
+          0.4,
+        )} 150%)`,
+  border: '1px solid',
+  borderColor:
+    theme.palette.mode === 'dark'
+      ? `${alpha(theme.palette.primaryDark[300], 0.5)}`
+      : `${alpha(theme.palette.primary[200], 0.5)}`,
+  boxShadow:
+    theme.palette.mode === 'dark'
+      ? `4px 0 60px ${alpha(theme.palette.primary[300], 0.5)}`
+      : `4px 0 60px ${alpha(theme.palette.primary[100], 0.8)}`,
 }));
 
 export default function DemoVideo() {
   return (
     <Box
-      sx={(theme) => ({
+      sx={{
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'center',
-        mt: { xs: theme.spacing(2.5), sm: theme.spacing(7.5) },
-        py: { xs: 4, sm: 6 },
-        bgcolor: theme.palette.mode === 'dark' ? 'primaryDark.900' : 'grey.50',
-      })}
+        py: { xs: 4, sm: 8 },
+      }}
     >
       <Container>
         <VideoContainer>
           <Video
-            sx={{
-              borderWidth: { xs: '10px', sm: '20px' },
-            }}
             muted
             autoPlay
             playsInline

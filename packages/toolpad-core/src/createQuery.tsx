8c9d12e56670ca8277138fcb2f3f572f875b5fce
Bharat Kashyap <bharatkashyap@outlook.com>
2023-04-17 17:34:15 -0700

    Deprecate `createQuery` => Replace with `createFunction`

--- packages/toolpad-core/src/createQuery.tsx
+++ packages/toolpad-core/src/createQuery.tsx
@@ -9,11 +9,19 @@ interface QueryParameterTypeLookup {
   object: Record<string, unknown>;
 }
 
+/**
+ * @deprecated
+ * QueryParameterConfig is deprecated. Use FunctionParameterConfig instead.
+ */
 export interface QueryParameterConfig<P, K extends keyof P> {
   typeDef: PrimitiveValueType;
   defaultValue?: P[K];
 }
 
+/**
+ * @deprecated
+ * CreateQueryConfig is deprecated. Use CreateFunctionConfig instead.
+ */
 export interface CreateQueryConfig<P> {
   parameters: {
     [K in keyof P]: QueryParameterConfig<P, K>;
@@ -23,21 +31,37 @@ export interface CreateQueryConfig<P> {
 type CreateQueryConfigParameters<C extends CreateQueryConfig<CreateQueryConfigParameters<C>>> =
   QueryResolverParams<C>['parameters'];
 
+/**
+ * @deprecated
+ * QueryResolverParams is deprecated. Use FunctionResolverParams instead.
+ */
 export interface QueryResolverParams<C extends CreateQueryConfig<CreateQueryConfigParameters<C>>> {
   parameters: {
     [K in keyof C['parameters']]: QueryParameterTypeLookup[C['parameters'][K]['typeDef']['type']];
   };
 }
 
+/**
+ * @deprecated
+ * QueryResolver is deprecated. Use FunctionResolver instead.
+ */
 export interface QueryResolver<C extends CreateQueryConfig<CreateQueryConfigParameters<C>>> {
   (params: QueryResolverParams<C>): Promise<unknown>;
 }
 
+/**
+ * @deprecated
+ * ToolpadQuery is deprecated. Use ToolpaFunction instead.
+ */
 export interface ToolpadQuery<C extends CreateQueryConfig<CreateQueryConfigParameters<C>>>
   extends QueryResolver<C> {
   [TOOLPAD_QUERY]: C;
 }
 
+/**
+ * @deprecated
+ * createQuery is deprecated. Use createFunction instead.
+ */
 export default function createQuery<C extends CreateQueryConfig<CreateQueryConfigParameters<C>>>(
   resolver: QueryResolver<C>,
   config?: C,
--- packages/toolpad-core/src/index.tsx
+++ packages/toolpad-core/src/index.tsx
@@ -22,3 +22,6 @@ export * from './componentsContext.js';
 
 export { default as createQuery } from './createQuery.js';
 export * from './createQuery.js';
+
+export { default as createFunction } from './createFunction.js';
+export * from './createFunction.js';

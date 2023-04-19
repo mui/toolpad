7e001b69901dc34cfe0eeabce27bedf4cbde56f2
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2023-04-19 20:43:27 +0100

    Fix Windows issues (WIP) (#1910)

--- packages/toolpad-app/src/toolpadDataSources/local/server.ts
+++ packages/toolpad-app/src/toolpadDataSources/local/server.ts
@@ -77,6 +77,11 @@ function formatCodeFrame(location: esbuild.Location): string {
   ].join('\n');
 }
 
+function pathToNodeImportSpecifier(importPath: string): string {
+  const normalized = path.normalize(importPath).split(path.sep).join('/');
+  return normalized.startsWith('/') ? normalized : `./${normalized}`;
+}
+
 async function createMain(): Promise<string> {
   const relativeFunctionsFilePath = [`.`, getFunctionsFile('.')].join(path.sep);
   return `
@@ -102,7 +107,7 @@ async function createMain(): Promise<string> {
       if (!resolversPromise) {
         resolversPromise = (async () => {
           const functions = await import(${JSON.stringify(
-            relativeFunctionsFilePath,
+            pathToNodeImportSpecifier(relativeFunctionsFilePath),
           )}).catch((err) => {
             console.error(err);
             return {};

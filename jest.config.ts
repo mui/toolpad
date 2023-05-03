8f10ea3aa9d167f63d889fd006bfa3875f683f8e
Bharat Kashyap <bharatkashyap@outlook.com>
2023-05-03 22:27:25 +0530

    test create-toolpad-app using health-check

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@ const config: Config.InitialOptions = {
   projects: [
     '<rootDir>/packages/toolpad-app/jest.config.ts',
     '<rootDir>/packages/toolpad-utils/jest.config.ts',
+    '<rootDir>/packages/create-toolpad-app/jest.config.ts',
   ],
   setupFilesAfterEnv: ['<rootDir>/test/utils/jest-setup.ts'],
 };

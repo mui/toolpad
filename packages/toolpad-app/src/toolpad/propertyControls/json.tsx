267973f9035ba9e7463c1118429c84302cf00d15
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2023-05-12 09:53:43 +0200

    fixes

--- packages/toolpad-app/src/toolpad/propertyControls/json.tsx
+++ packages/toolpad-app/src/toolpad/propertyControls/json.tsx
@@ -8,7 +8,7 @@ import {
   Skeleton,
 } from '@mui/material';
 import * as React from 'react';
-import * as JSON5 from 'json5';
+import JSON5 from 'json5';
 import type { EditorProps } from '../../types';
 import useShortcut from '../../utils/useShortcut';
 import lazyComponent from '../../utils/lazyComponent';

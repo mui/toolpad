93d75d85a4da5fa3d2a583a954d5c051bb3676da
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2023-05-15 19:07:57 +0200

    ignore tests in dist

--- packages/toolpad-utils/jest.config.ts
+++ packages/toolpad-utils/jest.config.ts
@@ -2,5 +2,6 @@ import { Config } from 'jest';
 
 export default {
   preset: 'ts-jest/presets/default-esm',
+  testPathIgnorePatterns: ['dist'],
   setupFilesAfterEnv: ['<rootDir>/../../test/setupJest.ts'],
 } satisfies Config;

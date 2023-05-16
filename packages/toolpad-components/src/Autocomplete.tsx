a5b564e939b23791ef47d75cfe1182ff8e79c0b8
Bharat Kashyap <bharatkashyap@outlook.com>
2023-05-16 14:19:30 +0530

    Fix: Add HelperText and Select control

--- packages/toolpad-components/src/Autocomplete.tsx
+++ packages/toolpad-components/src/Autocomplete.tsx
@@ -5,6 +5,7 @@ import {
   TextField,
 } from '@mui/material';
 import { createComponent } from '@mui/toolpad-core';
+import { SX_PROP_HELPER_TEXT } from './constants.js';
 
 type AutocompleteOption = string | { label?: string };
 
@@ -28,35 +29,46 @@ export default createComponent(Autocomplete, {
   layoutDirection: 'both',
   argTypes: {
     options: {
+      helperText: 'The options available to search from.',
       type: 'array',
       default: [],
+      control: {
+        type: 'SelectOptions',
+      },
     },
     label: {
+      helperText: 'The label to display for the autocomplete.',
       type: 'string',
       default: 'Search…',
     },
     value: {
+      helperText: 'The value of the autocomplete.',
       type: 'string',
       onChangeProp: 'onChange',
       onChangeHandler: (event: React.ChangeEvent<HTMLInputElement>) => event.target.textContent,
       default: '',
     },
     fullWidth: {
+      helperText: 'If true, the autocomplete will take up the full width of its container.',
       type: 'boolean',
       default: true,
     },
     size: {
+      helperText: 'The size of the autocomplete. One of `small`, `medium`, or `large`.',
       type: 'string',
       enum: ['small', 'medium', 'large'],
       default: 'small',
     },
     loading: {
+      helperText: 'If true, the autocomplete will display a loading indicator.',
       type: 'boolean',
     },
     disabled: {
+      helperText: 'If true, the autocomplete will be disabled.',
       type: 'boolean',
     },
     sx: {
+      helperText: SX_PROP_HELPER_TEXT,
       type: 'object',
     },
   },

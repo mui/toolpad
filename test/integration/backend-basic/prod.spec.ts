77fe49f53dd0a2a7ba680a967ec6b24cd41dd7a0
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2023-05-16 10:56:44 +0200

    test

--- test/integration/backend-basic/prod.spec.ts
+++ test/integration/backend-basic/prod.spec.ts
@@ -1,8 +1,6 @@
 import * as path from 'path';
 import { test, expect } from '../../playwright/localTest';
 import { ToolpadRuntime } from '../../models/ToolpadRuntime';
-import { ToolpadEditor } from '../../models/ToolpadEditor';
-import { fileReplace } from '../../utils/fs';
 
 test.use({
   ignoreConsoleErrors: [
@@ -26,9 +24,9 @@ test('functions basics', async ({ page }) => {
   const runtimeModel = new ToolpadRuntime(page, { prod: true });
   await runtimeModel.gotoPage('page1');
 
-  await page.locator('text="hello, message: hello world"').waitFor({ state: 'visible' });
-  await page.locator('text="throws, error.message: BOOM!"').waitFor({ state: 'visible' });
-  await page.locator('text="throws, data undefined"').waitFor({ state: 'visible' });
-  await page.locator('text="echo, parameter: bound foo parameter"').waitFor({ state: 'visible' });
-  await page.locator('text="echo, secret: Some bar secret"').waitFor({ state: 'visible' });
+  await expect(page.locator('text="hello, message: hello world"')).toBeVisible();
+  await expect(page.locator('text="throws, error.message: BOOM!"')).toBeVisible();
+  await expect(page.locator('text="throws, data undefined"')).toBeVisible();
+  await expect(page.locator('text="echo, parameter: bound foo parameter"')).toBeVisible();
+  await expect(page.locator('text="echo, secret: Some bar secret"')).toBeVisible();
 });

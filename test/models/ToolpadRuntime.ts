a66782f8f71217093a2b41664a1bd238341d8e86
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2023-05-17 10:36:04 +0200

    Improve and test basic auth implementation (#2022)

--- test/models/ToolpadRuntime.ts
+++ test/models/ToolpadRuntime.ts
@@ -1,26 +1,40 @@
 import { Page } from '@playwright/test';
 import { gotoIfNotCurrent } from './shared';
 
+export interface ToolpadRuntimeOptions {
+  prod: boolean;
+}
+
 export class ToolpadRuntime {
   readonly page: Page;
 
-  constructor(page: Page) {
+  readonly options: ToolpadRuntimeOptions;
+
+  constructor(page: Page, options: Partial<ToolpadRuntimeOptions> = {}) {
     this.page = page;
+    this.options = {
+      prod: false,
+      ...options,
+    };
+  }
+
+  getPrefix() {
+    return this.options.prod ? '/prod' : '/preview';
   }
 
   async goto() {
-    await gotoIfNotCurrent(this.page, `/preview`);
+    await gotoIfNotCurrent(this.page, this.getPrefix());
   }
 
   async gotoPage(pageName: string) {
-    await gotoIfNotCurrent(this.page, `/preview/pages/${pageName}`);
+    await gotoIfNotCurrent(this.page, `${this.getPrefix()}/pages/${pageName}`);
   }
 
   async gotoPageById(appId: string, pageId: string) {
-    await this.page.goto(`/preview/pages/${pageId}`);
+    await this.page.goto(`${this.getPrefix()}/pages/${pageId}`);
   }
 
   async waitForNavigation() {
-    await this.page.waitForURL(/\/preview\/pages\/[^/]+$/);
+    await this.page.waitForURL((url) => url.pathname.startsWith(`${this.getPrefix()}/pages`));
   }
 }

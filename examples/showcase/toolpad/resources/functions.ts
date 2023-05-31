fc6b48a409ada02a0a0972ac1b0eefbe854eac1e
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2023-05-31 14:32:43 +0200

    First set of updates

--- examples/showcase/toolpad/resources/functions.ts
+++ examples/showcase/toolpad/resources/functions.ts
@@ -1,8 +1,16 @@
 import { createFunction } from '@mui/toolpad/server';
 
-let customers = [
+let nextId = 1;
+
+function generateId() {
+  const id = nextId;
+  nextId += 1;
+  return id;
+}
+
+const customers = [
   {
-    id: 1,
+    id: generateId(),
     name: 'Emily Lee',
     account_creation_date: '2022-03-15',
     country_of_residence: 'Brazil',
@@ -10,11 +18,9 @@ let customers = [
     email: 'emilylee@example.com',
     address: '123 Main St, Sao Paulo, Brazil',
     gender: 'Female',
-    id_1: 'ABC123',
-    image: 'https://i.pravatar.cc/300',
   },
   {
-    id: 23,
+    id: generateId(),
     name: 'Liam Patel',
     account_creation_date: '2022-02-02',
     country_of_residence: 'India',
@@ -22,11 +28,9 @@ let customers = [
     email: 'liampatel@example.com',
     address: '456 Park Rd, Mumbai, India',
     gender: 'Male',
-    id_1: 'XYZ789',
-    image: 'https://i.pravatar.cc/300',
   },
   {
-    id: 3,
+    id: generateId(),
     name: 'Emma Garcia',
     account_creation_date: '2022-01-23',
     country_of_residence: 'Spain',
@@ -34,11 +38,9 @@ let customers = [
     email: 'emmagarcia@example.com',
     address: '789 Oak Ave, Madrid, Spain',
     gender: 'Female',
-    id_1: 'PQR456',
-    image: 'https://i.pravatar.cc/300',
   },
   {
-    id: 9,
+    id: generateId(),
     name: 'William Wong',
     account_creation_date: '2022-04-08',
     country_of_residence: 'United States',
@@ -46,11 +48,9 @@ let customers = [
     email: 'williamwong@example.com',
     address: '456 Elm St, San Francisco, CA, USA',
     gender: 'Male',
-    id_1: 'DEF789',
-    image: 'https://i.pravatar.cc/300',
   },
   {
-    id: 5,
+    id: generateId(),
     name: 'Ava Kim',
     account_creation_date: '2022-02-14',
     country_of_residence: 'South Korea',
@@ -58,11 +58,9 @@ let customers = [
     email: 'avakim@example.com',
     address: '321 Maple St, Seoul, South Korea',
     gender: 'Female',
-    id_1: 'LMN012',
-    image: 'https://i.pravatar.cc/300',
   },
   {
-    id: 12,
+    id: generateId(),
     name: 'Ethan Chen',
     account_creation_date: '2022-03-01',
     country_of_residence: 'China',
@@ -70,8 +68,6 @@ let customers = [
     email: 'ethanchen@example.com',
     address: '789 Pine Rd, Beijing, China',
     gender: 'Male',
-    id_1: 'UVW234',
-    image: 'https://i.pravatar.cc/300',
   },
 ];
 
@@ -81,50 +77,37 @@ export async function getCustomers() {
 
 const customerParams = {
   name: {
-    typeDef: { type: 'string' },
+    type: 'string' as const,
   },
   account_creation_date: {
-    typeDef: { type: 'string', format: 'date' },
+    type: 'string' as const,
   },
   country_of_residence: {
-    typeDef: { type: 'string' },
+    type: 'string' as const,
   },
   phone_number: {
-    typeDef: { type: 'number' },
+    type: 'number' as const,
   },
   email: {
-    typeDef: { type: 'string', format: 'email' },
+    type: 'string' as const,
   },
   address: {
-    typeDef: { type: 'string' },
+    type: 'string' as const,
   },
   gender: {
-    typeDef: { type: 'string', enum: ['Male', 'Female', 'Other'] },
-  },
-  id_1: {
-    typeDef: { type: 'string' },
-  },
-  image: {
-    typeDef: { type: 'string', format: 'uri' },
+    type: 'string' as const,
+    enum: ['Male', 'Female', 'Other'],
   },
 };
 
 export const addCustomer = createFunction(
   async ({ parameters }) => {
-    const maxId = customers.reduce((max, customer) => Math.max(max, customer.id), -1);
-    customers.push({
-      name: parameters.name,
-      id: maxId + 1,
-      account_creation_date: parameters.account_creation_date,
-      country_of_residence: parameters.country_of_residence,
-      phone_number: parameters.phone_number,
-      email: parameters.email,
-      address: parameters.address,
-      gender: parameters.gender,
-      id_1: parameters.id_1,
-      image: 'https://i.pravatar.cc/300',
-    });
-    return customers[maxId + 1];
+    const newCustomer = {
+      id: generateId(),
+      ...parameters,
+    };
+    customers.push(newCustomer);
+    return newCustomer;
   },
   {
     parameters: customerParams,
@@ -135,39 +118,31 @@ export const updateCustomer = createFunction(
   async ({ parameters }) => {
     const index = customers.findIndex((item) => item.id === parameters.id);
 
-    customers[index] = {
-      id: parameters.id,
-      name: parameters.name,
-      account_creation_date: parameters.account_creation_date,
-      country_of_residence: parameters.country_of_residence,
-      phone_number: parameters.phone_number,
-      email: parameters.email,
-      address: parameters.address,
-      gender: parameters.gender,
-      id_1: parameters.id_1,
-      image: 'https://i.pravatar.cc/300',
-    };
+    if (customers[index]) {
+      Object.assign(customers[index], parameters);
+    }
+
     return customers[index];
   },
   {
     parameters: {
       id: {
-        typeDef: { type: 'number' },
+        type: 'number',
       },
       ...customerParams,
     },
   },
 );
 
-export const del = createFunction(
+export const deleteCustomer = createFunction(
   async ({ parameters }) => {
-    customers = customers.filter(({ id }) => id !== parameters.id);
-    return customers;
+    const index = customers.findIndex((item) => item.id === parameters.id);
+    customers.splice(index, 1);
   },
   {
     parameters: {
       id: {
-        typeDef: { type: 'number' },
+        type: 'number',
       },
     },
   },

76b25c164733e98c37d57410646813fb98d0f30b
Prakhar Gupta <92228082+prakhargupta1@users.noreply.github.com>
2023-06-15 18:06:09 +0530

    fix custom-datagrid-column app

---
+++ examples/custom-datagrid-column/toolpad/components/FullNameColumn.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { Typography } from '@mui/material';
+import { createComponent } from '@mui/toolpad/browser';
+
+export interface FullNameColumnProps {
+  row: {
+    age: number;
+    firstname: string;
+    lastname: string;
+  };
+}
+
+function FullNameColumn({ row }: FullNameColumnProps) {
+  return <Typography noWrap>{`${row.firstname} ${row.lastname}`}</Typography>;
+}
+
+export default createComponent(FullNameColumn, {
+  argTypes: {
+    row: {
+      type: 'object',
+    },
+  },
+});
--- examples/dog-app/toolpad/resources/functions.ts
+++
@@ -1,7 +0,0 @@
-import { createFunction } from '@mui/toolpad/server';
-
-export const getDogs = createFunction(async function getDogs() {
-  // fetch from https://dog.ceo/api/breeds/image/random
-  const dogs = await fetch('https://dog.ceo/api/breeds/image/random');
-  return [await dogs.json()];
-});

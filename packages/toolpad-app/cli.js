ded051921b72bea5f3243e67c3af93f9eae43b0b
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2023-06-15 16:12:08 +0200

    ok

--- packages/toolpad-app/cli.js
+++ packages/toolpad-app/cli.js
@@ -1,6 +1,31 @@
 #!/usr/bin/env node
 /* eslint-disable */
 
-const { default: cli } = require('./dist/cli');
+const { fork } = require('child_process');
 
-cli(process.argv.slice(2));
+/**
+ * We want to ensure that the node process that runs the cli has the --enable-source-maps flag
+ * enabled. Since we can't add it to the shebang line we fork a new process with the flag enabled.
+ */
+
+const requiredFlags = ['--enable-source-maps'];
+const actualFlags = new Set(process.execArgv);
+const missingFlags = requiredFlags.filter((requiredFlag) => !actualFlags.has(requiredFlag));
+
+if (missingFlags.length > 0) {
+  // Get the node binary, file, and non-node arguments that we ran with
+  const [, module, ...args] = process.argv;
+
+  // Re-running with --enable-source-maps flag
+  fork(module, args, {
+    execArgv: [
+      // Get the arguments passed to the node binary
+      ...process.execArgv,
+      // Pass more arguments to node binary as desired
+      ...missingFlags,
+    ],
+  }).once('exit', process.exit);
+} else {
+  const { default: cli } = require('./dist/cli');
+  cli(process.argv.slice(2));
+}

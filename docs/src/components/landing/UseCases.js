23bc150b70a49385c5009ad8bf7e4004b912cad1
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2023-07-04 17:50:24 -0300

    continue work on the accordion section

--- docs/src/components/landing/UseCases.js
+++ docs/src/components/landing/UseCases.js
@@ -1,19 +1,66 @@
 import * as React from 'react';
 import Link from '@mui/material/Link';
 import PropTypes from 'prop-types';
+import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import KeyboardArrowRightRounded from '@mui/icons-material/KeyboardArrowRightRounded';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-// import Grid from '@mui/material/Unstable_Grid2';
-import SectionHeadline from 'docs/src/components/typography/SectionHeadline';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import SectionHeadline from 'docs/src/components/typography/SectionHeadline';
+import GradientText from 'docs/src/components/typography/GradientText';
+import ROUTES from '../../route';
 
-export default function CardGrid(props) {
-  const { content } = props;
+const USE_CASE_IMAGES = new Map([
+  ['panel1', '/static/toolpad/marketing/qr-generator.png'],
+  ['panel2', '/static/toolpad/marketing/admin-app.png'],
+  ['panel3', '/static/toolpad/marketing/npm-stats.png'],
+]);
+
+const StyledAccordion = styled(Accordion)(({ theme }) => [
+  {
+    backgroundColor: 'transparent',
+  },
+  theme.applyDarkStyles({
+    backgroundColor: 'transparent',
+  }),
+]);
+
+const StyledAccordionSummary = styled(AccordionSummary)(({ theme }) => [
+  {
+    margin: '0 -12px',
+    padding: '0 12px',
+    borderRadius: 12,
+    '& h3': {
+      marginBottom: 0,
+    },
+    '& svg': {
+      opacity: 0.5,
+    },
+    '&:hover': {
+      backgroundColor: (theme.vars || theme).palette.grey[100],
+      '& svg': {
+        opacity: 1,
+      },
+    },
+  },
+  theme.applyDarkStyles({
+    '&:hover': {
+      backgroundColor: (theme.vars || theme).palette.primaryDark[800],
+    },
+  }),
+]);
+
+export default function CardGrid() {
+  const [expanded, setExpanded] = React.useState('panel1');
+
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
+  };
+
   return (
     <Box
       sx={[
@@ -26,103 +73,101 @@ export default function CardGrid(props) {
       ]}
     >
       <Container sx={{ py: { xs: 8, sm: 12 } }}>
-        <SectionHeadline overline={content.overline} title={content.Headline} />
-        {/* <Grid container columns={{ xs: 1, sm: 3 }} spacing={4}>
-          {content.cards.map(({ title, imageUrl, description, action }) => (
-            <Grid key={title} xs={3} sm={1}>
-              <Box key={title}>
-                <Box
-                  sx={[
-                    (theme) => ({
-                      display: 'block',
-                      backgroundRepeat: 'no-repeat',
-                      backgroundPosition: 'center',
-                      objectFit: 'contain',
-                      background: `${
-                        (theme.vars || theme).palette.primaryDark[800]
-                      } url(${imageUrl})`,
-                      backgroundSize: 'cover',
-                      borderRadius: '8px',
-                      paddingTop: '55%',
-                      overflow: 'auto',
-                      boxShadow: `0 0 8px 2px ${
-                        (theme.vars || theme).palette.primary[100]
-                      }, 0 4px 40px ${(theme.vars || theme).palette.primary[100]}`,
-                      ...theme.applyDarkStyles({
-                        boxShadow: `0 0 8px 2px ${
-                          (theme.vars || theme).palette.primaryDark[600]
-                        }, 0 4px 40px ${(theme.vars || theme).palette.primaryDark[500]}`,
-                      }),
-                    }),
-                  ]}
-                />
-                <Box
-                  mt={2}
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'space-between',
-                  }}
-                >
-                  <Typography
-                    component="h3"
-                    fontWeight="semiBold"
-                    color="text.primary"
-                    gutterBottom
-                  >
-                    {title}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {description}
-                  </Typography>
-                  {action ? (
-                    <Link href={action.href} variant="body2" sx={{ mt: 1 }}>
-                      {action.label}
-                      <KeyboardArrowRightRounded font />
-                    </Link>
-                  ) : null}
-                </Box>
-              </Box>
-            </Grid>
-          ))}
-        </Grid> */}
+        <SectionHeadline
+          overline="Use cases"
+          title={
+            <Typography variant="h2" maxWidth={300} marginBottom={3}>
+              Toolpad is <br />
+              <GradientText>ideal for building</GradientText>
+            </Typography>
+          }
+        />
         <Box sx={{ display: 'flex', alignItems: 'center', gap: 8 }}>
           <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-            {content.cards.map(({ title, description, action }) => (
-              <Accordion
-                disableGutters
-                elevation={0}
-                key={title}
-                sx={{ background: 'transparent' }}
+            <StyledAccordion
+              expanded={expanded === 'panel1'}
+              onChange={handleChange('panel1')}
+              disableGutters
+              elevation={0}
+            >
+              <StyledAccordionSummary
+                expandIcon={<ExpandMoreIcon color="primary" />}
+                aria-controls="use-cases-content"
+                id="use-case-header"
               >
-                <AccordionSummary
-                  expandIcon={<ExpandMoreIcon color="primary" />}
-                  aria-controls="use-cases-content"
-                  id="use-case-header"
-                  sx={{ p: 0 }}
-                >
-                  <Typography
-                    component="h3"
-                    fontWeight="semiBold"
-                    color="text.primary"
-                    gutterBottom
-                  >
-                    {title}
-                  </Typography>
-                </AccordionSummary>
-                <AccordionDetails sx={{ p: 0, mb: 2 }}>
-                  <Typography variant="body2" color="text.secondary">
-                    {description}
-                  </Typography>
-                  {action ? (
-                    <Link href={action.href} variant="body2" sx={{ mt: 1 }}>
-                      {action.label}
-                      <KeyboardArrowRightRounded fontSize="small" />
-                    </Link>
-                  ) : null}
-                </AccordionDetails>
-              </Accordion>
-            ))}
+                <Typography component="h3" fontWeight="semiBold" color="text.primary" gutterBottom>
+                  Utility apps
+                </Typography>
+              </StyledAccordionSummary>
+              <AccordionDetails sx={{ p: 0, mb: 2 }}>
+                <Typography variant="body2" color="text.secondary">
+                  Provide the stakeholders with simple apps to manage their daily operations. You
+                  can quickly build an app on Toolpad by calling APIs or writing custom functions.
+                  Your app remains secure as the code never leaves your network, and you can
+                  securely deploy it to any service you choose.
+                </Typography>
+                <Link href={ROUTES.toolpadUtilityAppExample} variant="body2" sx={{ mt: 1 }}>
+                  View more
+                  <KeyboardArrowRightRounded fontSize="small" />
+                </Link>
+              </AccordionDetails>
+            </StyledAccordion>
+            <StyledAccordion
+              expanded={expanded === 'panel2'}
+              onChange={handleChange('panel2')}
+              disableGutters
+              elevation={0}
+            >
+              <StyledAccordionSummary
+                expandIcon={<ExpandMoreIcon color="primary" />}
+                aria-controls="use-cases-content"
+                id="use-case-header"
+              >
+                <Typography component="h3" fontWeight="semiBold" color="text.primary" gutterBottom>
+                  Admin panel
+                </Typography>
+              </StyledAccordionSummary>
+              <AccordionDetails sx={{ p: 0, mb: 2 }}>
+                <Typography variant="body2" color="text.secondary">
+                  Enable your teams to quickly view and manage customer orders, queries, and refunds
+                  by creating admin apps that gather data from third-party APIs providers like
+                  Stripe, Twilio, Zendesk, etc. Toolpad allows end users to create, read, update, or
+                  delete records.
+                </Typography>
+                <Link href={ROUTES.toolpadAdminExample} variant="body2" sx={{ mt: 1 }}>
+                  View more
+                  <KeyboardArrowRightRounded fontSize="small" />
+                </Link>
+              </AccordionDetails>
+            </StyledAccordion>
+            <StyledAccordion
+              expanded={expanded === 'panel3'}
+              onChange={handleChange('panel3')}
+              disableGutters
+              elevation={0}
+            >
+              <StyledAccordionSummary
+                expandIcon={<ExpandMoreIcon color="primary" />}
+                aria-controls="use-cases-content"
+                id="use-case-header"
+              >
+                <Typography component="h3" fontWeight="semiBold" color="text.primary" gutterBottom>
+                  BI dashboard
+                </Typography>
+              </StyledAccordionSummary>
+              <AccordionDetails sx={{ p: 0, mb: 2 }}>
+                <Typography variant="body2" color="text.secondary">
+                  Build BI dashboards to slice and dice any metric across various dimensions.
+                  Further, use them to monitor KPIs, track business goals, and identify trends and
+                  opportunities. Toolpad allows you to combine data from multiple sources and bind
+                  it by writing JavaScript anywhere.
+                </Typography>
+                <Link href={ROUTES.toolpadBIExample} variant="body2" sx={{ mt: 1 }}>
+                  View more
+                  <KeyboardArrowRightRounded fontSize="small" />
+                </Link>
+              </AccordionDetails>
+            </StyledAccordion>
           </Box>
           <Box
             sx={[
@@ -130,12 +175,13 @@ export default function CardGrid(props) {
                 display: 'block',
                 minWidth: 510,
                 minHeight: 320,
+                height: 'auto',
                 backgroundRepeat: 'no-repeat',
                 backgroundPosition: 'center',
                 objectFit: 'contain',
                 background: `${
                   (theme.vars || theme).palette.primaryDark[800]
-                } url(/static/toolpad/marketing/qr-generator.png)`,
+                } url(${USE_CASE_IMAGES.get(expanded)})`,
                 backgroundSize: 'cover',
                 borderRadius: 1,
                 border: '1px solid',

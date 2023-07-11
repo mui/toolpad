11998c67f67191338560f515504d04a7e0501699
Prakhar Gupta <92228082+prakhargupta1@users.noreply.github.com>
2023-07-11 15:35:09 +0530

    update-image

--- docs/src/components/landing/Hero.js
+++ docs/src/components/landing/Hero.js
@@ -100,7 +100,7 @@ export default function Hero() {
         <CodeBlock />
         <Box
           component="img"
-          src="/static/toolpad/marketing/with-prisma.png" // image will be replaced for a better one
+          src="/static/toolpad/marketing/with-prisma-hero.png" // image will be replaced for a better one
           sx={[
             (theme) => ({
               position: 'absolute',

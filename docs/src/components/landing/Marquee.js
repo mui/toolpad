8309fbfbe7ed571271d7394f8e6359d0741e5002
Bharat Kashyap <bharatkashyap@outlook.com>
2023-07-12 12:13:41 +0530

    use `theme.applyDarkStyles`, Marquee differ bg

--- docs/src/components/landing/Marquee.js
+++ docs/src/components/landing/Marquee.js
@@ -7,26 +7,34 @@ import SignUp from './SignUp';
 function Marquee({ content }) {
   return (
     <Container
-      sx={(theme) => ({
-        mt: 8,
-        mx: 0,
-        minWidth: '100%',
-        py: { xs: 4, sm: 6, md: 12 },
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        background: `linear-gradient(180deg, ${theme.palette.primaryDark[900]} 0%, ${theme.palette.primaryDark[800]})`,
-        borderBottom: '1px solid',
-        borderColor: theme.palette.mode === 'dark' ? 'primaryDark.600' : 'grey.200',
-      })}
+      sx={[
+        (theme) => ({
+          mt: 4,
+          mx: 0,
+          minWidth: '100%',
+          py: { xs: 4, sm: 6, md: 12 },
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          // background: `linear-gradient(180deg, ${theme.palette.primaryDark[900]} 0%, ${theme.palette.primaryDark[800]})`,
+          background: `radial-gradient(140% 150% at 50% 10%, transparent 20%,  ${theme.palette.primary[100]} 90%,  transparent 100%)`,
+          borderBottom: '1px solid',
+          borderColor: 'grey.200',
+        }),
+        (theme) =>
+          theme.applyDarkStyles({
+            borderColor: 'primaryDark.600',
+            background: `linear-gradient(180deg, ${theme.palette.primaryDark[900]} 0%, ${theme.palette.primaryDark[800]})`,
+          }),
+      ]}
     >
       <Typography
-        color="white"
         textAlign="center"
         variant="h2"
         sx={{
           mt: 4,
           mx: 'auto',
+          color: (theme) => theme.palette.primary[800],
         }}
       >
         {content.title}
@@ -45,8 +53,10 @@ function Marquee({ content }) {
       <Typography
         component="label"
         variant="body2"
-        color={(theme) => (theme.palette.mode === 'dark' ? '#fff' : `text.secondary`)}
-        sx={{ fontWeight: 'medium', display: 'block', mb: 1, mx: 'auto' }}
+        sx={[
+          { fontWeight: 'medium', display: 'block', mb: 1, mx: 'auto', color: `text.secondary` },
+          (theme) => theme.applyDarkStyles({ color: '#fff' }),
+        ]}
         htmlFor="email-landing"
       >
         {content.action.label}

014e31cb38094c72ae53f5cf9e180c72c8b1f1a7
Prakhar Gupta <92228082+prakhargupta1@users.noreply.github.com>
2023-07-20 22:23:25 +0530

    stay in the loop

--- docs/data/toolpad/landing/marquee.js
+++ docs/data/toolpad/landing/marquee.js
@@ -1,9 +1,9 @@
 import ROUTES from '../../../src/route';
 
 const marquee = {
-  title: 'Stay on the loop',
+  title: 'Stay in the loop',
   subtitle:
-    'Subscribe to the Toolpad newsletter and receive product updates & success stories from  Beta users.',
+    'Subscribe to the Toolpad newsletter and receive product updates & success stories from Beta users.',
   action: {
     href: ROUTES.toolpadQuickstart,
   },

b93b7d9d2adbb9c512385a39f220f12d33178878
Bharat Kashyap <bharatkashyap@outlook.com>
2023-07-30 02:58:16 +0530

    Create a draggable Debugger

--- packages/toolpad-app/src/components/Devtools.tsx
+++ packages/toolpad-app/src/components/Devtools.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
 import { TabPanel, TabContext, TabList } from '@mui/lab';
-import { Box, IconButton, styled, SxProps, Tab } from '@mui/material';
+import { Box, Button, IconButton, styled, SxProps, Paper, Tab } from '@mui/material';
+import { Theme } from '@mui/material/styles';
 import { Har } from 'har-format';
+import Draggable from 'react-draggable';
 import DoDisturbIcon from '@mui/icons-material/DoDisturb';
+import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
+import ClearOutlinedIcon from '@mui/icons-material/ClearOutlined';
+import BugReportIcon from '@mui/icons-material/BugReport';
 import Console, { LogEntry } from './Console';
 import lazyComponent from '../utils/lazyComponent';
 
@@ -18,7 +23,21 @@ export interface DevtoolsProps {
   sx?: SxProps;
 }
 
+function PaperComponent({ children }: { children: React.ReactNode }) {
+  return (
+    <Draggable handle="#draggable-dialog-title" cancel={'[class*="MuiDialogContent-root"]'}>
+      <Paper sx={{ position: 'fixed', bottom: '10vh', right: '1vw', maxHeight: 400, width: 500 }}>
+        {children}
+      </Paper>
+    </Draggable>
+  );
+}
+
 export default function Devtools({ sx, log, onLogClear, har, onHarClear }: DevtoolsProps) {
+  const [open, setOpen] = React.useState(false);
+  const handleClose = React.useCallback(() => {
+    setOpen(false);
+  }, []);
   const [activeTab, setActiveTab] = React.useState(() => {
     if (log) {
       return 'console';
@@ -28,6 +47,21 @@ export default function Devtools({ sx, log, onLogClear, har, onHarClear }: Devto
     }
     return '';
   });
+
+  const buttonStyles = React.useCallback(
+    (theme: Theme) => ({
+      color: theme.palette.grey[400],
+      fontSize: 12,
+      transition: theme.transitions.create('color', {
+        duration: theme.transitions.duration.shorter,
+      }),
+      '&:hover': {
+        color: theme.palette.grey[500],
+        backgroundColor: theme.palette.grey[100],
+      },
+    }),
+    [],
+  );
 
   const handleDebuggerTabChange = (event: React.SyntheticEvent, newValue: string) => {
     setActiveTab(newValue);
@@ -59,39 +93,82 @@ export default function Devtools({ sx, log, onLogClear, har, onHarClear }: Devto
   }, [activeTab, onHarClear, onLogClear]);
 
   return (
-    <Box sx={{ ...sx, display: 'flex', flexDirection: 'column' }}>
-      <TabContext value={activeTab}>
-        <Box
-          sx={{
-            borderBottom: 1,
-            borderColor: 'divider',
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            pr: 1,
-          }}
-        >
-          <TabList onChange={handleDebuggerTabChange} aria-label="Debugger">
-            {log ? <Tab label="Console" value="console" /> : null}
-            {har ? <Tab label="Network" value="network" /> : null}
-          </TabList>
-          {handleClearClick ? (
-            <IconButton disabled={!clearEnabled} onClick={handleClearClick}>
-              <DoDisturbIcon />
-            </IconButton>
-          ) : null}
-        </Box>
-        {log ? (
-          <DebuggerTabPanel value="console">
-            <Console sx={{ flex: 1 }} value={log} />
-          </DebuggerTabPanel>
-        ) : null}
-        {har ? (
-          <DebuggerTabPanel value="network">
-            <HarViewer sx={{ flex: 1 }} value={har} />
-          </DebuggerTabPanel>
-        ) : null}
-      </TabContext>
-    </Box>
+    <React.Fragment>
+      <Box
+        sx={{
+          ...sx,
+          display: open ? 'flex' : 'none',
+          flexDirection: 'column',
+        }}
+      >
+        <PaperComponent>
+          <TabContext value={activeTab}>
+            <Box
+              sx={{
+                borderBottom: 1,
+                borderColor: 'divider',
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'start',
+                pr: 1,
+              }}
+            >
+              <IconButton id={'draggable-dialog-title'} sx={{ cursor: 'move' }}>
+                <DragIndicatorIcon />
+              </IconButton>
+              {handleClearClick ? (
+                <IconButton disabled={!clearEnabled} onClick={handleClearClick}>
+                  <DoDisturbIcon />
+                </IconButton>
+              ) : null}
+
+              <TabList
+                onChange={handleDebuggerTabChange}
+                aria-label="Debugger"
+                sx={{ '& button': { fontSize: 12, fontWeight: 'normal' } }}
+              >
+                {log ? (
+                  <Tab
+                    label="Console"
+                    value="console"
+                    sx={{ borderBottom: (theme) => `1px solid ${theme.palette.grey[300]}` }}
+                  />
+                ) : null}
+                {har ? (
+                  <Tab
+                    label="Network"
+                    value="network"
+                    sx={{ borderBottom: (theme) => `1px solid ${theme.palette.grey[300]}` }}
+                  />
+                ) : null}
+              </TabList>
+              <IconButton onClick={handleClose} disableRipple>
+                <ClearOutlinedIcon fontSize="inherit" sx={buttonStyles} />
+              </IconButton>
+            </Box>
+
+            {log ? (
+              <DebuggerTabPanel value="console">
+                <Console sx={{ flex: 1 }} value={log} />
+              </DebuggerTabPanel>
+            ) : null}
+            {har ? (
+              <DebuggerTabPanel value="network">
+                <HarViewer sx={{ flex: 1 }} value={har} />
+              </DebuggerTabPanel>
+            ) : null}
+          </TabContext>
+        </PaperComponent>
+      </Box>
+
+      <Button
+        onClick={() => setOpen(true)}
+        sx={(theme) => buttonStyles(theme)}
+        startIcon={<BugReportIcon />}
+        disabled={!logLength && !harLength}
+      >
+        Debug
+      </Button>
+    </React.Fragment>
   );
 }

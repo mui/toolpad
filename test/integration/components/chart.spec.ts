09778146054eb5ce7fe38d7004fe3338ec0cc8f0
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2023-08-08 20:35:44 +0100

    Remove spacing

--- test/integration/components/chart.spec.ts
+++ test/integration/components/chart.spec.ts
@@ -34,6 +34,5 @@ test('shows chart loading and errors', async ({ page }) => {
   await runtimeModel.gotoPage('loadingAndError');
 
   await expect(page.getByText('BOOM!', { exact: true })).toBeVisible();
-
   await expect(page.getByRole('progressbar')).toBeVisible();
 });

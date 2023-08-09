98d397a316a7d1647bcf8e38916cffbe577fcb02
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2023-08-09 15:29:18 +0100

    Chart improvements from npm stats example update (#2400)

--- packages/toolpad-app/src/runtime/evalJsBindings.ts
+++ packages/toolpad-app/src/runtime/evalJsBindings.ts
@@ -242,6 +242,9 @@ export default function evalJsBindings(
 
     let bindingResult = results[bindingId];
 
+    let nestedBindingsLoading: boolean | undefined;
+    let nestedBindingsError: Error | undefined;
+
     const mergeNestedBindings = (value: unknown, parentBindingId: string) => {
       if (value && typeof value === 'object') {
         for (const nestedPropName of Object.keys(value)) {
@@ -252,6 +255,13 @@ export default function evalJsBindings(
           const nestedBindingResult = results[nestedBindingId];
 
           if (nestedBindingResult) {
+            if (!nestedBindingsError) {
+              nestedBindingsError = bubbleError(flatDependencies, results, nestedBindingId);
+            }
+            if (!nestedBindingsLoading) {
+              nestedBindingsLoading = bubbleLoading(flatDependencies, results, nestedBindingId);
+            }
+
             bindingResult = updatePath(
               bindingResult,
               `value.${nestedBindingId.replace(bindingId, '')}`,
@@ -276,8 +286,8 @@ export default function evalJsBindings(
       dependencies: Array.from(flatDependencies.get(bindingId) ?? []),
       result: {
         ...bindingResult,
-        error: bubbleError(flatDependencies, results, bindingId),
-        loading: bubbleLoading(flatDependencies, results, bindingId),
+        error: nestedBindingsError || bubbleError(flatDependencies, results, bindingId),
+        loading: nestedBindingsLoading || bubbleLoading(flatDependencies, results, bindingId),
       },
     };
   });

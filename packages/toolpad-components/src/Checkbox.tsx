70f6fffbbdaf6531a89340e7a2e24bc880957978
wuls <linsheng.wu@beantechs.com>
2023-08-15 09:44:16 +0800

    add checkbox

---
+++ packages/toolpad-components/src/Checkbox.tsx
@@ -0,0 +1,7 @@
+import { createComponent } from '@mui/toolpad-core';
+import * as React from 'react';
+
+function Container() {
+  return <div>2</div>;
+}
+export default createComponent(Container);

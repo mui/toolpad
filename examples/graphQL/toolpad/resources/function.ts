42fbc39af15c36acb386c40186443c4ddf808d4e
Prakhar Gupta <92228082+prakhargupta1@users.noreply.github.com>
2023-08-17 15:18:57 +0530

    pedro-review

--- examples/graphQL/toolpad/resources/function.ts
+++ examples/graphQL/toolpad/resources/function.ts
@@ -1,4 +1,5 @@
 import { request } from 'graphql-request';
+import gql from 'graphql-tag';
 
 export async function queryStargazers(owner: string, repository: string) {
   if (!process.env.GITHUB_TOKEN) {
@@ -8,34 +9,34 @@ export async function queryStargazers(owner: string, repository: string) {
   const endpoint = 'https://api.github.com/graphql';
   const token = process.env.GITHUB_TOKEN;
 
-  const query = `
-query stargazersList($repository: String!, $owner: String! ) {
-  repository(name: $repository, owner: $owner) {
-    stargazerCount
-    stargazers(first: 100, after: null orderBy: {field: STARRED_AT, direction: DESC}) {
-      nodes {
-        company
-        email
-        location
-        name
-        updatedAt
-        createdAt
-        login
-        bio
-        followers {
-          totalCount
+  const query = gql`
+    query stargazersList($repository: String!, $owner: String!) {
+      repository(name: $repository, owner: $owner) {
+        stargazerCount
+        stargazers(first: 100, after: null, orderBy: { field: STARRED_AT, direction: DESC }) {
+          nodes {
+            company
+            email
+            location
+            name
+            createdAt
+            url
+            bio
+            followersCount: followers {
+              totalCount
+            }
+          }
         }
       }
     }
-  }
-}`;
+  `;
 
   const response = request(
     endpoint,
     query,
     {
-      repository: repository,
-      owner: owner,
+      repository,
+      owner,
     },
     {
       Authorization: `Bearer ${token}`,

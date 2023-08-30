a50adde61000ff2d5b7d722d21403a481ffa40e6
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2023-08-30 22:43:50 +0100

    Use transformIgnorePatterns

--- packages/toolpad-app/jest.config.js
+++ packages/toolpad-app/jest.config.js
@@ -1,8 +1,24 @@
+const esModules = [
+  'get-port',
+  'lodash-es',
+  'nanoid',
+  'fractional-indexing',
+  'd3-scale',
+  'd3-array',
+  'internmap',
+  'd3-interpolate',
+  'd3-color',
+  'd3-format',
+  'd3-time',
+  'd3-time-format',
+  'd3-shape',
+  'd3-path',
+].join('|');
+
 module.exports = /** @type {import('jest').Config} */ ({
-  preset: 'ts-jest/presets/default-esm',
   transform: {
     '^.+\\.(t|j)sx?$': '@swc/jest',
   },
   setupFilesAfterEnv: ['<rootDir>/../../test/setupJest.ts'],
-  transformIgnorePatterns: ['node_modules/(?!(get-port|lodash-es|nanoid|fractional-indexing)/)'],
+  transformIgnorePatterns: [`node_modules/(?!(${esModules})/)`],
 });

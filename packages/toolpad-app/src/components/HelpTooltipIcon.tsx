13ed9f95d156b5f8d19babfec55fa099679105d9
Rohan Prasad <rohanpd.work@gmail.com>
2023-09-05 00:10:00 +0530

    fix: omit props and accept string to helpttooltip

--- packages/toolpad-app/src/components/HelpTooltipIcon.tsx
+++ packages/toolpad-app/src/components/HelpTooltipIcon.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { Tooltip } from '@mui/material';
+import { Tooltip, TooltipProps } from '@mui/material';
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 
-export interface HelpTooltipIconProps {
-  helpText: React.ReactNode;
+export interface HelpTooltipIconProps extends Omit<TooltipProps, 'title' | 'children'> {
+  helpText: string | React.ReactNode;
 }
 
 export default function HelpTooltipIcon({ helpText, ...props }: HelpTooltipIconProps) {

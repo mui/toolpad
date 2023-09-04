1eb90b1e5ca6739ec89b0e064291df7ad8811ed8
Rohan Prasad <rohanpd.work@gmail.com>
2023-09-04 20:54:45 +0530

    [Toolpad Editor] Fix : text component alignment rendering (#2587)

--- packages/toolpad-components/src/Text.tsx
+++ packages/toolpad-components/src/Text.tsx
@@ -242,7 +242,6 @@ function TextContent({ value, loading, sx, variant }: TextContentProps) {
     <MuiTypography
       sx={{
         ...sx,
-        width: '100%',
         // This will give it height, even when empty.
         // REMARK: Does it make sense to put it in MUI core?
         [`&:empty::before`]: { content: '""', display: 'inline-block' },

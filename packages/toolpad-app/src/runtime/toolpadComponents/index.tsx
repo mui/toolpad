c862eb82e70c024379faf9d60f2d48c556b375f4
Wainaina R Kirera <52700465+Kirera-Wainaina@users.noreply.github.com>
2023-10-16 23:56:19 +0300

    Add password option to TextField Component (#2797)
    Co-authored-by: Jan Potoms <2109932+Janpot@users.noreply.github.com>
    Co-authored-by: Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>

--- packages/toolpad-app/src/runtime/toolpadComponents/index.tsx
+++ packages/toolpad-app/src/runtime/toolpadComponents/index.tsx
@@ -54,7 +54,10 @@ export const INTERNAL_COMPONENTS = new Map<string, ToolpadComponentDefinition>([
       synonyms: ['graph', 'bar chart', 'pie chart', 'line chart', 'plot'],
     },
   ],
-  ['TextField', { displayName: 'Text Field', builtIn: 'TextField', synonyms: ['input', 'field'] }],
+  [
+    'TextField',
+    { displayName: 'Text Field', builtIn: 'TextField', synonyms: ['input', 'field', 'password'] },
+  ],
   ['DatePicker', { displayName: 'Date Picker', builtIn: 'DatePicker', synonyms: ['time'] }],
   ['FilePicker', { displayName: 'File Picker', builtIn: 'FilePicker', synonyms: [] }],
   ['Text', { displayName: 'Text', builtIn: 'Text', synonyms: ['markdown', 'link', 'output'] }],
@@ -88,6 +91,15 @@ export const INTERNAL_COMPONENTS = new Map<string, ToolpadComponentDefinition>([
   ['Metric', { displayName: 'Metric', builtIn: 'Metric', synonyms: ['value', 'number', 'output'] }],
   ['Checkbox', { displayName: 'Checkbox', builtIn: 'Checkbox', synonyms: ['switch'] }],
   [FORM_COMPONENT_ID, { displayName: 'Form', builtIn: 'Form', synonyms: [] }],
+  [
+    'Password',
+    {
+      displayName: 'Password',
+      builtIn: 'TextField',
+      synonyms: [],
+      initialProps: { password: true },
+    },
+  ],
 ]);
 
 function createCodeComponent(domNode: appDom.CodeComponentNode): ToolpadComponentDefinition {

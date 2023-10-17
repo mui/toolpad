3a95cfdce737c81b3e93c8020bb409f194698a9c
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2023-10-17 19:42:05 +0100

    Remove unneeded assertion

--- test/utils/clickCenter.ts
+++ test/utils/clickCenter.ts
@@ -1,8 +1,6 @@
 import { Page, Locator, expect } from '@playwright/test';
 
 export default async function clickCenter(page: Page, targetLocator: Locator) {
-  await expect(targetLocator).toBeVisible();
-
   const targetBoundingBox = await targetLocator.boundingBox();
   expect(targetBoundingBox).toBeTruthy();
 

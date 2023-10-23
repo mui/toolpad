77afb5f8241a85ec76ae4fc55d4d03180b7a6aae
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2023-10-23 18:19:43 +0100

    Fix some more types

--- packages/toolpad-app/src/rpcClient.ts
+++ packages/toolpad-app/src/rpcClient.ts
@@ -8,6 +8,9 @@ import {
   useQuery,
   UseQueryOptions,
   UseQueryResult,
+  useSuspenseQuery,
+  UseSuspenseQueryOptions,
+  UseSuspenseQueryResult,
 } from '@tanstack/react-query';
 import type { MethodsOf, RpcRequest, RpcResponse, MethodResolvers, Methods } from './server/rpc';
 
@@ -55,12 +58,26 @@ export interface UseQueryFnOptions<F extends (...args: any[]) => any>
     'queryKey' | 'queryFn'
   > {}
 
+export interface UseSuspenseQueryFnOptions<F extends (...args: any[]) => any>
+  extends Omit<
+    UseSuspenseQueryOptions<Awaited<ReturnType<F>>, Error, Awaited<ReturnType<F>>, any[]>,
+    'queryKey' | 'queryFn'
+  > {}
+
 export interface UseQueryFn<M extends Methods> {
   <K extends keyof M & string>(
     name: K,
     params: Parameters<M[K]> | null,
     options?: UseQueryFnOptions<M[K]>,
   ): UseQueryResult<Awaited<ReturnType<M[K]>>>;
+}
+
+export interface UseSuspenseQueryFn<M extends Methods> {
+  <K extends keyof M & string>(
+    name: K,
+    params: Parameters<M[K]> | null,
+    options?: UseSuspenseQueryFnOptions<M[K]>,
+  ): UseSuspenseQueryResult<Awaited<ReturnType<M[K]>>>;
 }
 
 export interface UseMutationFn<M extends Methods> {
@@ -73,6 +90,7 @@ export interface UseMutationFn<M extends Methods> {
 export interface ApiClient<D extends MethodResolvers, M extends Methods = MethodsOf<D>> {
   methods: M;
   useQuery: UseQueryFn<M>;
+  useSuspenseQuery: UseSuspenseQueryFn<M>;
   useMutation: UseMutationFn<M>;
   refetchQueries: <K extends keyof M>(key: K, params: Parameters<M[K]>) => Promise<void>;
   invalidateQueries: <K extends keyof M>(key: K, params: Parameters<M[K]>) => Promise<void>;
@@ -93,6 +111,16 @@ export function createRpcApi<D extends MethodResolvers>(
         queryKey: [key, params],
         queryFn: () => {
           invariant(params, `"enabled" prop of useQuery should prevent this call'`);
+          return methods[key](...params);
+        },
+      });
+    },
+    useSuspenseQuery: (key, params, options) => {
+      return useSuspenseQuery({
+        ...options,
+        queryKey: [key, params],
+        queryFn: () => {
+          invariant(params, 'Query has no parameters');
           return methods[key](...params);
         },
       });

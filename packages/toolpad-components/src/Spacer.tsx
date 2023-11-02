007c3117d2af4cf95d97c1436df81e1c84143618
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2023-11-02 15:26:57 +0000

    Improve Spacer component height

--- packages/toolpad-components/src/Spacer.tsx
+++ packages/toolpad-components/src/Spacer.tsx
@@ -3,14 +3,15 @@ import { Box, BoxProps } from '@mui/material';
 import createBuiltin from './createBuiltin';
 import { SX_PROP_HELPER_TEXT } from './constants';
 
+const SPACER_MINIMUM_HEIGHT = 20; // pixels
+
 function Spacer(props: BoxProps) {
-  return <Box {...props} height="100%" />;
+  return <Box {...props} height="100%" minHeight={SPACER_MINIMUM_HEIGHT} />;
 }
 
 export default createBuiltin(Spacer, {
   helperText: 'Spacer component.\nIt allows for creating space between elements.',
-  defaultLayoutHeight: 40,
-  minimumLayoutHeight: 4,
+  minimumLayoutHeight: SPACER_MINIMUM_HEIGHT,
   argTypes: {
     sx: {
       helperText: SX_PROP_HELPER_TEXT,

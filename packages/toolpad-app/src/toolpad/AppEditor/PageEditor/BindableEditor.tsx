719bbab9c3f448d25663e8e6f2d410a818e65875
Jerry_wu <409187100@qq.com>
2023-11-08 00:29:41 +0800

    Allow binding query parameters to environment variables (#2777)
    Signed-off-by: Jerry_wu <409187100@qq.com>
    Co-authored-by: wuls <linsheng.wu@beantechs.com>

--- packages/toolpad-app/src/toolpad/AppEditor/PageEditor/BindableEditor.tsx
+++ packages/toolpad-app/src/toolpad/AppEditor/PageEditor/BindableEditor.tsx
@@ -31,7 +31,7 @@ export interface BindableEditorProps<V> extends WithControlledProp<BindableAttrV
   liveBinding?: LiveBinding;
   globalScope?: Record<string, unknown>;
   globalScopeMeta: ScopeMeta;
-  envVarNames?: string[];
+  env?: Record<string, string>;
   sx?: SxProps;
 }
 
@@ -47,7 +47,7 @@ export default function BindableEditor<V>({
   liveBinding,
   globalScope = {},
   globalScopeMeta = {},
-  envVarNames,
+  env,
   sx,
 }: BindableEditorProps<V>) {
   const propTypeControls = usePropControlsContext();
@@ -103,7 +103,7 @@ export default function BindableEditor<V>({
           disabled={disabled || !bindable}
           hidden={!bindable}
           liveBinding={liveBinding}
-          envVarNames={envVarNames}
+          env={env}
         />
       </React.Fragment>
     </Stack>

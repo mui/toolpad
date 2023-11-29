e5fd825e9e722fbe88f4a76e19c8eaea5de0fed7
wuls <linsheng.wu@beantechs.com>
2023-11-29 18:13:58 +0800

    fix

--- packages/toolpad-app/src/toolpad/propertyControls/SelectOptions.tsx
+++ packages/toolpad-app/src/toolpad/propertyControls/SelectOptions.tsx
@@ -51,7 +51,6 @@ function SelectOptionsPropEditor({
     (event: React.KeyboardEvent<HTMLInputElement>) => {
       if (event.key === 'Enter') {
         const inputText = (event.target as HTMLInputElement).value;
-        debugger;
         onChange([...value, inputText]);
         if (optionInputRef.current) {
           optionInputRef.current.value = '';

e1acc0875a7da1eed8a25c2a8e7dd5ec20d3f3e4
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2024-01-08 17:56:11 +0000

    Best possible test without creating test users with public credentials

--- test/integration/auth/prod.spec.ts
+++ test/integration/auth/prod.spec.ts
@@ -26,44 +26,24 @@ test.use({
 });
 
 test('Must be authenticated to view pages', async ({ page, context, baseURL }) => {
-  // @TODO: Try mocking https://api.github.com/user and https://api.github.com/user/emails instead
-  await page.route('*/**/api/auth/signin/github', async (route) => {
-    const token = await encode({
-      token: {
-        name: 'Adelbert Steiner',
-        email: 'steiner@plutoknights.com',
-        picture: 'https://placehold.co/600x400',
-      },
-      secret: TOOLPAD_AUTH_SECRET,
-      salt: 'authjs.session-token',
-    });
-
-    context.addCookies([{ name: 'authjs.session-token', value: token, url: baseURL }]);
-
-    const json = { url: '/prod/pages/mypage' };
-    await route.fulfill({ json });
-  });
-
-  await page.goto('/prod/pages/mypage');
-
   // Is redirected when unauthenticated
+  await page.goto('/prod/pages/mypage');
   await expect(page).toHaveURL('/prod/signin');
 
-  const githubLoginButton = page.getByText('Sign in with GitHub');
+  // Authenticate mock user
+  const token = await encode({
+    token: {
+      name: 'Adelbert Steiner',
+      email: 'steiner@plutoknights.com',
+      picture: 'https://placehold.co/600x400',
+    },
+    secret: TOOLPAD_AUTH_SECRET,
+    salt: 'authjs.session-token',
+  });
+  await context.addCookies([{ name: 'authjs.session-token', value: token, url: baseURL }]);
 
-  await githubLoginButton.click();
-
-  // Goes to correct redirect URL, and is not redirected when authenticated
+  // Sees page content when authenticated
+  await page.goto('/prod/pages/mypage');
   await expect(page).toHaveURL('/prod/pages/mypage');
-
-  const profileButtonLocator = page.getByText('Adelbert Steiner');
-
-  await expect(profileButtonLocator).toBeVisible();
-
-  // Sign out
-
-  await profileButtonLocator.click();
-  await page.getByText('Sign out').click();
-
-  await expect(page).toHaveURL(/\/prod\/signin/);
+  await expect(page.getByText('Adelbert Steiner')).toBeVisible();
 });

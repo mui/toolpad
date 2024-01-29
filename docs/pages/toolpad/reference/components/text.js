769ecd73d82d07b32d49bf5f7a5699510da355da
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2024-01-29 19:19:37 +0100

    Add stronger warning to the auto generated files to avoid hand-editing (#3146)

--- docs/pages/toolpad/reference/components/text.js
+++ docs/pages/toolpad/reference/components/text.js
@@ -1,4 +1,4 @@
-/* This file has been auto-generated using `pnpm docs:build:api`. */
+/* ATTENTION: DO NOT EDIT! This file has been auto-generated using `pnpm docs:build:api`. */
 
 import * as React from 'react';
 import MarkdownDocs from '@mui/monorepo/docs/src/modules/components/MarkdownDocs';

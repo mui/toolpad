8f6370c2903711bdc14da721069a4b2c304bd33d
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2024-03-13 01:55:26 +0000

    adjust comment

--- test/integration/auth/domain.spec.ts
+++ test/integration/auth/domain.spec.ts
@@ -58,7 +58,7 @@ test('Must be authenticated with valid domain to access app', async ({ page, req
 
   await page.waitForURL(/\/prod\/signin/);
 
-  // Must wait for network to be idle or next CSRF request fails with "Failed to fetch" on the next sign out, probably interrupts itself somehow
+  // Must wait for network to be idle or next CSRF request fails with "Failed to fetch" on the next sign out, somehow due to the ongoing sign out requests
   await page.waitForLoadState('networkidle');
 
   // Is redirected when unauthenticated

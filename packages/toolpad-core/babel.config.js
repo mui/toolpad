c939d988f52bfd864e4a0d0d4fe19b7b7f324d03
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2024-05-07 17:05:14 +0100

    Try Jan's changes and some other but keep playground

--- packages/toolpad-core/babel.config.js
+++ packages/toolpad-core/babel.config.js
@@ -49,12 +49,6 @@ module.exports = function getBabelConfig(api) {
       },
     ],
     'babel-plugin-optimize-clsx',
-    // Need the following 3 proposals for all targets in .browserslistrc.
-    // With our usage the transpiled loose mode is equivalent to spec mode.
-    ['@babel/plugin-transform-class-properties', { loose: true }],
-    ['@babel/plugin-transform-private-methods', { loose: true }],
-    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
-    ['@babel/plugin-transform-object-rest-spread', { loose: true }],
     [
       '@babel/plugin-transform-runtime',
       {

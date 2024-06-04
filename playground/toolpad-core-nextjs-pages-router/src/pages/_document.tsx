fd7c9cc8a6da27e637a1144fd04864c5fe1ed19f
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2024-06-04 18:28:04 +0100

    Improve pages playground

--- playground/toolpad-core-nextjs-pages-router/src/pages/_document.tsx
+++ playground/toolpad-core-nextjs-pages-router/src/pages/_document.tsx
@@ -1,9 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import * as React from 'react';
+import { Html, Head, Main, NextScript, DocumentProps, DocumentContext } from 'next/document';
+import {
+  DocumentHeadTags,
+  DocumentHeadTagsProps,
+  documentGetInitialProps,
+} from '@mui/material-nextjs/v14-pagesRouter';
 
-export default function Document() {
+export default function Document(props: DocumentProps & DocumentHeadTagsProps) {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="emotion-insertion-point" content="" />
+        <DocumentHeadTags {...props} />
+      </Head>
       <body>
         <Main />
         <NextScript />
@@ -11,3 +20,8 @@ export default function Document() {
     </Html>
   );
 }
+
+Document.getInitialProps = async (ctx: DocumentContext) => {
+  const finalProps = await documentGetInitialProps(ctx);
+  return finalProps;
+};

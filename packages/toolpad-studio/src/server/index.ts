6aa6ce22bcf4ecd370d29b767656690c6efbe5e9
Olivier Tassinari <olivier.tassinari@gmail.com>
2024-07-06 01:23:08 +0200

    try thsi

--- packages/toolpad-studio/src/server/index.ts
+++ packages/toolpad-studio/src/server/index.ts
@@ -241,14 +241,14 @@ async function createToolpadHandler({
       [
         `default-src * data: mediastream: blob: filesystem: about: ws: wss: 'unsafe-eval' 'wasm-unsafe-eval' 'unsafe-inline';`,
         `script-src * data: blob: 'unsafe-inline' 'unsafe-eval';`,
-        `script-src-elem * data: blob: 'unsafe-inline' 'unsafe-eval';`,
+        `script-src-elem * data: blob: 'unsafe-inline';`,
         `connect-src * data: blob: 'unsafe-inline';`,
         `img-src * data: blob: 'unsafe-inline';`,
         `media-src * data: blob: 'unsafe-inline';`,
         `frame-src * data: blob: ;`,
         `style-src * data: blob: 'unsafe-inline';`,
         `font-src * data: blob: 'unsafe-inline';`,
-        `frame-ancestors * data: blob:;`,
+        `frame-ancestors *:* data: blob:;`,
       ].join(' '),
     );
     expressNext();

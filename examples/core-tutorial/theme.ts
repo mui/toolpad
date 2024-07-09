ad9bddb719a81cd113b73e82d304ae5ead1a0e8c
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2024-07-09 19:16:10 +0100

    Update some examples/tutorials

--- examples/core-tutorial/theme.ts
+++ examples/core-tutorial/theme.ts
@@ -1,12 +1,28 @@
 'use client';
-import { createTheme } from '@mui/material/styles';
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles';
+import type {} from '@mui/material/themeCssVarsAugmentation';
 
-const defaultTheme = createTheme();
-
-const theme = createTheme(defaultTheme, {
-  palette: {
-    background: {
-      default: defaultTheme.palette.grey['50'],
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        background: {
+          default: 'var(--mui-palette-grey-50)',
+          defaultChannel: 'var(--mui-palette-grey-50)',
+        },
+      },
+    },
+    dark: {
+      palette: {
+        background: {
+          default: 'var(--mui-palette-grey-900)',
+          defaultChannel: 'var(--mui-palette-grey-900)',
+        },
+        text: {
+          primary: 'var(--mui-palette-grey-200)',
+          primaryChannel: 'var(--mui-palette-grey-200)',
+        },
+      },
     },
   },
 });

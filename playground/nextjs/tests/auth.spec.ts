88ccdd8da3725dc21206c1edb7b735021f339ea8
Bharat Kashyap <bharatkashyap@outlook.com>
2024-07-10 23:57:02 -0700

    feat: Add playwright tests to playground app

--- playground/nextjs/tests/auth.spec.ts
+++ playground/nextjs/tests/auth.spec.ts
@@ -2,32 +2,14 @@ import { test, expect, Page } from '@playwright/test';
 
 const BASE_URL = process.env.BASE_URL || 'http://localhost:3000';
 
-test.describe('Sign In', () => {
+test.describe('Credentials Sign In', () => {
   let page: Page;
 
   test.beforeEach(async ({ browser }) => {
     page = await browser.newPage();
   });
 
-  test('credentials successful sign in', async () => {
-    await page.goto(`${BASE_URL}/auth/signin`);
-
-    await page.fill('input[name="email"]', 'test@example.com');
-    await page.fill('input[name="password"]', 'password');
-
-    const credentialsSignIn = await page.getByRole('button', {
-      name: 'Sign in',
-      exact: true,
-    });
-
-    await credentialsSignIn.click();
-
-    await page.waitForNavigation();
-
-    expect(page.url()).toBe(`${BASE_URL}/`);
-  });
-
-  test('failed sign in with incorrect credentials', async () => {
+  test('sign in with incorrect credentials displays error notification', async () => {
     await page.goto(`${BASE_URL}/auth/signin`);
 
     await page.fill('input[name="email"]', 'wrong@example.com');
@@ -43,6 +25,51 @@ test.describe('Sign In', () => {
     await page.waitForSelector('text=Invalid credentials.');
 
     expect(page.url()).toBe(`${BASE_URL}/auth/signin`);
+  });
+
+  test('sign in with correct credentials redirects to home page', async () => {
+    await page.goto(`${BASE_URL}/auth/signin`);
+
+    await page.fill('input[name="email"]', 'test@example.com');
+    await page.fill('input[name="password"]', 'password');
+
+    const credentialsSignIn = await page.getByRole('button', {
+      name: 'Sign in',
+      exact: true,
+    });
+
+    await credentialsSignIn.click();
+
+    await page.waitForSelector('text=Welcome to Toolpad, Test User!');
+
+    expect(page.url()).toBe(`${BASE_URL}/`);
+  });
+
+  test.afterEach(async () => {
+    await page.close();
+  });
+});
+
+test.describe('GitHub Sign In', () => {
+  let page: Page;
+
+  test.beforeEach(async ({ browser }) => {
+    page = await browser.newPage();
+  });
+
+  test('OAuth sign in flow is initiated successfully', async () => {
+    await page.goto(`${BASE_URL}/auth/signin`);
+
+    const githubSignIn = await page.getByRole('button', {
+      name: 'Sign in with GitHub',
+      exact: true,
+    });
+
+    await githubSignIn.click();
+
+    await page.waitForSelector(
+      'text=Sign in to GitHub to continue to Toolpad Core Next.js Playground',
+    );
   });
 
   test.afterEach(async () => {

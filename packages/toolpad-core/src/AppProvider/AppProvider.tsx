34b0d72e7c2c30deec07c857a2c0ec9f4354fae8
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2024-07-12 20:28:11 +0100

    Adjust import

--- packages/toolpad-core/src/AppProvider/AppProvider.tsx
+++ packages/toolpad-core/src/AppProvider/AppProvider.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import { ThemeProvider, Theme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { BadgeProps } from '@mui/material';
+import type { BadgeProps } from '@mui/material/Badge';
 import { baseTheme } from '../themes';
 import { NotificationsProvider } from '../useNotifications';
 import { DialogsProvider } from '../useDialogs';

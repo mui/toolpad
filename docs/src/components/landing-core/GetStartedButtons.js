e8b6d87fe63b1d5f45a1add05cbeb806075af931
Prakhar Gupta <92228082+prakhargupta1@users.noreply.github.com>
2024-07-20 00:37:56 +0530

    [website] Add Toolpad core landing page (#3690)
    Signed-off-by: Prakhar Gupta <92228082+prakhargupta1@users.noreply.github.com>
    Co-authored-by: Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
    Co-authored-by: MUI bot <2109932+Janpot@users.noreply.github.com>

---
+++ docs/src/components/landing-core/GetStartedButtons.js
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import KeyboardArrowRightRounded from '@mui/icons-material/KeyboardArrowRightRounded';
+import { Link } from '@mui/docs/Link';
+import NpmCopyButton from 'docs/src/components/action/NpmCopyButton';
+import GithubStars from '../landing/GithubStars';
+
+export default function GetStartedButtons(props) {
+  const { installation, primaryLabel, primaryUrl, ...other } = props;
+  return (
+    <React.Fragment>
+      <Box
+        {...other}
+        sx={{
+          display: 'flex',
+          flexWrap: { xs: 'wrap', md: 'nowrap' },
+          '&& > *': {
+            minWidth: { xs: '100%', md: '0%' },
+          },
+          ...other.sx,
+        }}
+      >
+        <Button
+          href={primaryUrl}
+          component={Link}
+          noLinkStyle
+          variant="contained"
+          endIcon={<KeyboardArrowRightRounded />}
+          sx={{
+            flexShrink: 0,
+            mr: { xs: 0, md: 1.5 },
+            mb: { xs: 2, md: 0 },
+          }}
+        >
+          {primaryLabel}
+        </Button>
+        <GithubStars />
+      </Box>
+      <NpmCopyButton installation={installation} sx={{ mt: 2 }} />
+    </React.Fragment>
+  );
+}
+
+GetStartedButtons.propTypes = {
+  installation: PropTypes.string.isRequired,
+  primaryLabel: PropTypes.string.isRequired,
+  primaryUrl: PropTypes.string.isRequired,
+  secondaryLabel: PropTypes.string,
+  secondaryUrl: PropTypes.string,
+};

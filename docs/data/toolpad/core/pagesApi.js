885b06bec281d1bce073126539e61356bf925449
Bharat Kashyap <bharatkashyap@outlook.com>
2024-07-25 13:00:06 +0530

    fix: Should also be a pre-commit hook

--- docs/data/toolpad/core/pagesApi.js
+++ docs/data/toolpad/core/pagesApi.js
@@ -4,7 +4,7 @@ module.exports = [
   { pathname: '/toolpad/core/api/dashboard-layout' },
   { pathname: '/toolpad/core/api/dialogs-provider' },
   { pathname: '/toolpad/core/api/notifications-provider' },
-  { pathname: '/toolpad/core/api/sign-in-page' },
   { pathname: '/toolpad/core/api/page-container' },
   { pathname: '/toolpad/core/api/page-container-toolbar' },
+  { pathname: '/toolpad/core/api/sign-in-page' },
 ];

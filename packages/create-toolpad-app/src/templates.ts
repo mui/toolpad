92b9e2db50b9e3503dd9630e36091025e143fb7a
Bharat Kashyap <bharatkashyap@outlook.com>
2024-07-28 19:30:29 +0530

    fix: Resolve auth deps

--- packages/create-toolpad-app/src/templates.ts
+++ packages/create-toolpad-app/src/templates.ts
@@ -445,14 +445,33 @@ export const packageJson: PackageJson = {
   },
 };
 export const packageJsonAuthApp: PackageJson = {
-  ...packageJson,
+  version: '0.1.0',
+  scripts: {
+    dev: 'next dev',
+    build: 'next build',
+    start: 'next start',
+    lint: 'next lint',
+  },
   dependencies: {
-    ...packageJson.dependencies,
+    '@emotion/react': '^11.11.4',
+    '@emotion/styled': '^11.11.5',
+    '@mui/icons-material': '^5.16.0',
+    '@mui/lab': '^5.0.0-alpha.170',
+    '@mui/material': '^5.16.0',
+    '@mui/material-nextjs': '^5.15.11',
+    '@toolpad/core': 'latest',
+    next: '14.2.4',
     'next-auth': '5.0.0-beta.18',
+    react: '18.3.1',
+    'react-dom': '18.3.1',
+  },
+  devDependencies: {
+    '@types/node': '^20.14.10',
+    '@types/react': '^18.3.3',
+    '@types/react-dom': '^18.3.0',
+    'eslint-config-next': '14.2.4',
   },
 };
-
-// export const packageJsonAuthApp: PackageJson = {
 //   version: '0.1.0',
 //   scripts: {
 //     dev: 'next dev',

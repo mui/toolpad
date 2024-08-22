5de42fab5adcbb831e8854669132a2a52fb6507d
Bharat Kashyap <bharatkashyap@outlook.com>
2024-08-22 13:10:06 +0530

    fix: Didn't build demos, add script to automate this

--- docs/data/toolpad/core/components/account/AccountDemo.js
+++ docs/data/toolpad/core/components/account/AccountDemo.js
@@ -1,18 +1,20 @@
 import * as React from 'react';
 import { Account, AuthenticationContext, SessionContext } from '@toolpad/core';
 
+const demoSession = {
+  user: {
+    name: 'Bharat Kashyap',
+    email: 'bharatkashyap@outlook.com',
+    image: 'https://avatars.githubusercontent.com/u/19550456',
+  },
+};
+
 export default function AccountDemo() {
-  const [session, setSession] = React.useState(null);
+  const [session, setSession] = React.useState(demoSession);
   const authentication = React.useMemo(() => {
     return {
       signIn: () => {
-        setSession({
-          user: {
-            name: 'Bharat Kashyap',
-            email: 'bharatkashyap@outlook.com',
-            image: 'https://avatars.githubusercontent.com/u/19550456',
-          },
-        });
+        setSession(demoSession);
       },
       signOut: () => {
         setSession(null);

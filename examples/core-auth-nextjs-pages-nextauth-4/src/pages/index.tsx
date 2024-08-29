f640f9fc5397c92c8bf454680dd357be42376e6b
Bharat Kashyap <bharatkashyap@outlook.com>
2024-08-29 18:14:30 +0530

    fix: Correct deps, upgrade to `material-ui` v6

--- examples/core-auth-nextjs-pages-nextauth-4/src/pages/index.tsx
+++ examples/core-auth-nextjs-pages-nextauth-4/src/pages/index.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { useSession } from 'next-auth/react';
 
 export default function HomePage() {
+  const { data: session } = useSession();
   return (
     <Box
       sx={{
@@ -15,7 +17,7 @@ export default function HomePage() {
       }}
     >
       <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-        Welcome to Toolpad!
+        Welcome to Toolpad{session ? `, ${session.user?.name}!` : ''}
       </Typography>
     </Box>
   );

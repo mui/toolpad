4788ed9322868420e34f42feda63b3c86cd64cb0
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2024-10-16 14:14:11 +0100

    Refactor DashboardLayout because of proptypes

---
+++ packages/toolpad-core/src/DashboardLayout/DashboardSidebarSubNavigation.tsx
@@ -0,0 +1,277 @@
+'use client';
+import * as React from 'react';
+import { styled } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import Collapse from '@mui/material/Collapse';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
+import Tooltip from '@mui/material/Tooltip';
+import type {} from '@mui/material/themeCssVarsAugmentation';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Link } from '../shared/Link';
+import { RouterContext } from '../shared/context';
+import type { Navigation } from '../AppProvider';
+import {
+  getItemTitle,
+  getPageItemFullPath,
+  hasSelectedNavigationChildren,
+  isPageItemSelected,
+} from '../shared/navigation';
+import { getDrawerSxTransitionMixin } from './utils';
+
+const NavigationListItemButton = styled(ListItemButton)(({ theme }) => ({
+  borderRadius: 8,
+  '&.Mui-selected': {
+    '& .MuiListItemIcon-root': {
+      color: (theme.vars ?? theme).palette.primary.dark,
+    },
+    '& .MuiTypography-root': {
+      color: (theme.vars ?? theme).palette.primary.dark,
+    },
+    '& .MuiSvgIcon-root': {
+      color: (theme.vars ?? theme).palette.primary.dark,
+    },
+    '& .MuiAvatar-root': {
+      backgroundColor: (theme.vars ?? theme).palette.primary.dark,
+    },
+    '& .MuiTouchRipple-child': {
+      backgroundColor: (theme.vars ?? theme).palette.primary.dark,
+    },
+  },
+  '& .MuiSvgIcon-root': {
+    color: (theme.vars ?? theme).palette.action.active,
+  },
+  '& .MuiAvatar-root': {
+    backgroundColor: (theme.vars ?? theme).palette.action.active,
+  },
+}));
+
+interface DashboardSidebarSubNavigationProps {
+  subNavigation: Navigation;
+  basePath?: string;
+  depth?: number;
+  onLinkClick: () => void;
+  isMini?: boolean;
+  isFullyExpanded?: boolean;
+  hasDrawerTransitions?: boolean;
+  selectedItemId: string;
+}
+
+/**
+ * @ignore - internal component.
+ */
+function DashboardSidebarSubNavigation({
+  subNavigation,
+  basePath = '',
+  depth = 0,
+  onLinkClick,
+  isMini = false,
+  isFullyExpanded = true,
+  hasDrawerTransitions = false,
+  selectedItemId,
+}: DashboardSidebarSubNavigationProps) {
+  const routerContext = React.useContext(RouterContext);
+
+  const pathname = routerContext?.pathname ?? '/';
+
+  const initialExpandedSidebarItemIds = React.useMemo(
+    () =>
+      subNavigation
+        .map((navigationItem, navigationItemIndex) => ({
+          navigationItem,
+          originalIndex: navigationItemIndex,
+        }))
+        .filter(({ navigationItem }) =>
+          hasSelectedNavigationChildren(navigationItem, basePath, pathname),
+        )
+        .map(({ originalIndex }) => `${depth}-${originalIndex}`),
+    [basePath, depth, pathname, subNavigation],
+  );
+
+  const [expandedSidebarItemIds, setExpandedSidebarItemIds] = React.useState(
+    initialExpandedSidebarItemIds,
+  );
+
+  const handleOpenFolderClick = React.useCallback(
+    (itemId: string) => () => {
+      setExpandedSidebarItemIds((previousValue) =>
+        previousValue.includes(itemId)
+          ? previousValue.filter((previousValueItemId) => previousValueItemId !== itemId)
+          : [...previousValue, itemId],
+      );
+    },
+    [],
+  );
+
+  return (
+    <List sx={{ padding: 0, mb: depth === 0 ? 4 : 1, pl: 2 * depth }}>
+      {subNavigation.map((navigationItem, navigationItemIndex) => {
+        if (navigationItem.kind === 'header') {
+          return (
+            <ListSubheader
+              key={`subheader-${depth}-${navigationItemIndex}`}
+              component="div"
+              sx={{
+                fontSize: 12,
+                fontWeight: '700',
+                height: isMini ? 0 : 40,
+                ...(hasDrawerTransitions
+                  ? getDrawerSxTransitionMixin(isFullyExpanded, 'height')
+                  : {}),
+                px: 2,
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+                whiteSpace: 'nowrap',
+                zIndex: 2,
+              }}
+            >
+              {getItemTitle(navigationItem)}
+            </ListSubheader>
+          );
+        }
+
+        if (navigationItem.kind === 'divider') {
+          const nextItem = subNavigation[navigationItemIndex + 1];
+
+          return (
+            <Divider
+              key={`divider-${depth}-${navigationItemIndex}`}
+              sx={{
+                borderBottomWidth: 2,
+                mx: 1,
+                mt: 1,
+                mb: nextItem?.kind === 'header' && !isMini ? 0 : 1,
+                ...(hasDrawerTransitions
+                  ? getDrawerSxTransitionMixin(isFullyExpanded, 'margin')
+                  : {}),
+              }}
+            />
+          );
+        }
+
+        const navigationItemFullPath = getPageItemFullPath(basePath, navigationItem);
+        const navigationItemId = `${depth}-${navigationItemIndex}`;
+        const navigationItemTitle = getItemTitle(navigationItem);
+
+        const isNestedNavigationExpanded = expandedSidebarItemIds.includes(navigationItemId);
+
+        const nestedNavigationCollapseIcon = isNestedNavigationExpanded ? (
+          <ExpandLessIcon />
+        ) : (
+          <ExpandMoreIcon />
+        );
+
+        const listItemIconSize = 34;
+
+        const isSelected = isPageItemSelected(navigationItem, basePath, pathname);
+
+        if (process.env.NODE_ENV !== 'production' && isSelected && selectedItemId) {
+          console.warn(`Duplicate selected path in navigation: ${navigationItemFullPath}`);
+        }
+
+        if (isSelected && !selectedItemId) {
+          selectedItemId = navigationItemId;
+        }
+
+        const listItem = (
+          <ListItem
+            sx={{
+              py: 0,
+              px: 1,
+              overflowX: 'hidden',
+            }}
+          >
+            <NavigationListItemButton
+              selected={isSelected && (!navigationItem.children || isMini)}
+              sx={{
+                px: 1.4,
+                height: 48,
+              }}
+              {...(navigationItem.children && !isMini
+                ? {
+                    onClick: handleOpenFolderClick(navigationItemId),
+                  }
+                : {
+                    LinkComponent: Link,
+                    href: navigationItemFullPath,
+                    onClick: onLinkClick,
+                  })}
+            >
+              {navigationItem.icon || isMini ? (
+                <ListItemIcon
+                  sx={{
+                    minWidth: listItemIconSize,
+                    mr: 1.2,
+                  }}
+                >
+                  {navigationItem.icon ?? null}
+                  {!navigationItem.icon && isMini ? (
+                    <Avatar
+                      sx={{
+                        width: listItemIconSize - 7,
+                        height: listItemIconSize - 7,
+                        fontSize: 12,
+                        ml: '-2px',
+                      }}
+                    >
+                      {navigationItemTitle
+                        .split(' ')
+                        .slice(0, 2)
+                        .map((itemTitleWord) => itemTitleWord.charAt(0).toUpperCase())}
+                    </Avatar>
+                  ) : null}
+                </ListItemIcon>
+              ) : null}
+              <ListItemText
+                primary={navigationItemTitle}
+                sx={{
+                  whiteSpace: 'nowrap',
+                  zIndex: 1,
+                  '& .MuiTypography-root': {
+                    fontWeight: '500',
+                  },
+                }}
+              />
+              {navigationItem.action && !isMini && isFullyExpanded ? navigationItem.action : null}
+              {navigationItem.children && !isMini && isFullyExpanded
+                ? nestedNavigationCollapseIcon
+                : null}
+            </NavigationListItemButton>
+          </ListItem>
+        );
+
+        return (
+          <React.Fragment key={navigationItemId}>
+            {isMini ? (
+              <Tooltip title={navigationItemTitle} placement="right">
+                {listItem}
+              </Tooltip>
+            ) : (
+              listItem
+            )}
+
+            {navigationItem.children && !isMini ? (
+              <Collapse in={isNestedNavigationExpanded} timeout="auto" unmountOnExit>
+                <DashboardSidebarSubNavigation
+                  subNavigation={navigationItem.children}
+                  basePath={navigationItemFullPath}
+                  depth={depth + 1}
+                  onLinkClick={onLinkClick}
+                  selectedItemId={selectedItemId}
+                />
+              </Collapse>
+            ) : null}
+          </React.Fragment>
+        );
+      })}
+    </List>
+  );
+}
+
+export { DashboardSidebarSubNavigation };

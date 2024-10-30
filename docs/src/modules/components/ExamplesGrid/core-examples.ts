b51d16284570b21ccc811b91c4f0a6988beedaa6
Bharat Kashyap <bharatkashyap@outlook.com>
2024-10-30 14:26:20 +0530

    [docs] Add sandbox links for Toolpad Core examples (#4325)

--- docs/src/modules/components/ExamplesGrid/core-examples.ts
+++ docs/src/modules/components/ExamplesGrid/core-examples.ts
@@ -7,6 +7,8 @@ export default function examples() {
       src: '/static/toolpad/docs/core/tutorial-1.png',
       href: 'https://mui.com/toolpad/core/introduction/tutorial/',
       source: 'https://github.com/mui/toolpad/tree/master/examples/core-tutorial',
+      codesandbox: 'https://codesandbox.io/s/github/mui/toolpad/tree/master/examples/core-tutorial',
+      stackblitz: 'https://stackblitz.com/github/mui/toolpad/tree/master/examples/core-tutorial',
     },
     {
       title: 'Auth.js with Next.js App router',
@@ -14,6 +16,9 @@ export default function examples() {
         'This app shows you to how to get started using Toolpad Core with Auth.js and the Next.js App router',
       src: '/static/toolpad/docs/core/auth-next.png',
       source: 'https://github.com/mui/toolpad/tree/master/examples/core-auth-nextjs',
+      codesandbox:
+        'https://codesandbox.io/s/github/mui/toolpad/tree/master/examples/core-auth-nextjs',
+      stackblitz: 'https://stackblitz.com/github/mui/toolpad/tree/master/examples/core-auth-nextjs',
     },
     {
       title: 'Auth.js with Next.js Pages router',
@@ -21,6 +26,10 @@ export default function examples() {
         'This app shows you to how to get started using Toolpad Core with Auth.js and the Next.js Pages router',
       src: '/static/toolpad/docs/core/auth-next.png',
       source: 'https://github.com/mui/toolpad/tree/master/examples/core-auth-nextjs-pages',
+      codesandbox:
+        'https://codesandbox.io/s/github/mui/toolpad/tree/master/examples/core-auth-nextjs-pages',
+      stackblitz:
+        'https://stackblitz.com/github/mui/toolpad/tree/master/examples/core-auth-nextjs-pages',
     },
     {
       title: 'Vite with React Router',
@@ -28,6 +37,8 @@ export default function examples() {
         'This app shows you to how to get started using Toolpad Core with Vite and React Router',
       src: '/static/toolpad/docs/core/vite-react-router.png',
       source: 'https://github.com/mui/toolpad/tree/master/examples/core-vite',
+      codesandbox: 'https://codesandbox.io/s/github/mui/toolpad/tree/master/examples/core-vite',
+      stackblitz: 'https://stackblitz.com/github/mui/toolpad/tree/master/examples/core-vite',
     },
   ];
 }

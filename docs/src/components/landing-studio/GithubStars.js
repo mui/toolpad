1dd5660a627eac3cd208b6699c5add892ac379a4
Prakhar Gupta <92228082+prakhargupta1@users.noreply.github.com>
2024-11-06 11:14:59 +0300

    remove unnecessary code

--- docs/src/components/landing-studio/GithubStars.js
+++ docs/src/components/landing-studio/GithubStars.js
@@ -81,9 +81,6 @@ export default function GithubStars() {
   );
 
   function formatNumber(num) {
-    if (num >= 1000) {
-      return `${(num / 1000).toFixed(1)}k`;
-    }
-    return num;
+    return `${(num / 1000).toFixed(1)}k`;
   }
 }

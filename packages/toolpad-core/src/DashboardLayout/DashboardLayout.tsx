397ba3b86e17e34c0f390806e097ab7a3d77a3d3
Bharat Kashyap <bharatkashyap@outlook.com>
2024-11-19 20:06:19 +0530

    fix: CI

--- packages/toolpad-core/src/DashboardLayout/DashboardLayout.tsx
+++ packages/toolpad-core/src/DashboardLayout/DashboardLayout.tsx
@@ -333,7 +333,6 @@ function DashboardLayout(props: DashboardLayoutProps) {
             alignItems="center"
             sx={{
               flexWrap: 'wrap',
-
               width: '100%',
             }}
           >

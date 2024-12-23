e279c06a9af321c2946f9eb004759a7b3bec56c8
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2024-12-23 14:32:35 +0000

    Regen docs

--- packages/toolpad-core/src/nextjs/NextjsAppProvider.tsx
+++ packages/toolpad-core/src/nextjs/NextjsAppProvider.tsx
@@ -5,9 +5,6 @@ import { NextjsAppProviderApp } from './NextjsAppProviderApp';
 import { NextjsAppProviderPages } from './NextjsAppProviderPages';
 import type { AppProviderProps } from '../AppProvider';
 
-/**
- * @ignore - internal component.
- */
 function AppProvider(props: AppProviderProps) {
   const router = useRouter();
   const AppProviderComponent = router ? NextjsAppProviderPages : NextjsAppProviderApp;

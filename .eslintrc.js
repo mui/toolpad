11b95da60532fd4f3ee9bd48efeec2ea4630665b
Olivier Tassinari <olivier.tassinari@gmail.com>
2024-12-27 00:57:13 +0100

    no longer an issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -68,15 +68,6 @@ module.exports = {
     // Turning react/jsx-key back on.
     // https://github.com/airbnb/javascript/blob/5155aa5fc1ea9bb2c6493a06ddbd5c7a05414c86/packages/eslint-config-airbnb/rules/react.js#L94
     'react/jsx-key': ['error', { checkKeyMustBeforeSpread: true, warnOnDuplicates: true }],
-    // This got turned of in the mono-repo:
-    // See https://github.com/mui/toolpad/pull/866#discussion_r957222171
-    'react/no-unused-prop-types': [
-      'error',
-      {
-        customValidators: [],
-        skipShapeProps: true,
-      },
-    ],
     'import/no-unresolved': [
       'error',
       {

c41f97620f0d47525cbbe5d4bac66144a4b8412a
Bharat Kashyap <bharatkashyap@outlook.com>
2025-02-05 21:12:26 +0530

    fix: CI

--- docs/data/toolpad/core/components/page-container/CustomPageContainer.js
+++ docs/data/toolpad/core/components/page-container/CustomPageContainer.js
@@ -42,6 +42,7 @@ function Content({ router }) {
 
 Content.propTypes = {
   router: PropTypes.shape({
+    Link: PropTypes.elementType,
     navigate: PropTypes.func.isRequired,
     pathname: PropTypes.string.isRequired,
     searchParams: PropTypes.instanceOf(URLSearchParams).isRequired,

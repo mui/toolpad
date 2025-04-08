871375a8b4d9c43d4defac372aeb46dfeb61c10d
Bharat Kashyap <bharatkashyap@outlook.com>
2025-04-08 15:53:20 -0600

    fix: Bring changes from #4826

--- packages/toolpad-core/src/DashboardLayout/DashboardSidebarSubNavigation.tsx
+++ packages/toolpad-core/src/DashboardLayout/DashboardSidebarSubNavigation.tsx
@@ -157,19 +157,18 @@ function DashboardSidebarSubNavigation({
           const nextItem = subNavigation[navigationItemIndex + 1];
 
           return (
-            <li key={`divider-${depth}-${navigationItemIndex}`}>
-              <Divider
-                sx={{
-                  borderBottomWidth: 2,
-                  mx: 1,
-                  mt: 1,
-                  mb: nextItem?.kind === 'header' && !isMini ? 0 : 1,
-                  ...(hasDrawerTransitions
-                    ? getDrawerSxTransitionMixin(isFullyExpanded, 'margin')
-                    : {}),
-                }}
-              />
-            </li>
+            <Divider
+              key={`divider-${depth}-${navigationItemIndex}`}
+              sx={{
+                borderBottomWidth: 2,
+                mx: 1,
+                mt: 1,
+                mb: nextItem?.kind === 'header' && !isMini ? 0 : 1,
+                ...(hasDrawerTransitions
+                  ? getDrawerSxTransitionMixin(isFullyExpanded, 'margin')
+                  : {}),
+              }}
+            />
           );
         }
 

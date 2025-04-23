d22e39c3ed8a35d1e0e281387798df70cb97b2bd
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2025-04-24 00:59:48 +0100

    Last try today

--- packages/toolpad-core/src/nextjs/NextAppProvider.tsx
+++ packages/toolpad-core/src/nextjs/NextAppProvider.tsx
@@ -1,6 +1,6 @@
 'use client';
 import * as React from 'react';
-import { useRouter } from 'next/compat/router.js';
+import { useRouter } from 'next/compat/router';
 import { NextAppProviderApp } from './NextAppProviderApp';
 import { NextAppProviderPages } from './NextAppProviderPages';
 import type { AppProviderProps } from '../AppProvider';

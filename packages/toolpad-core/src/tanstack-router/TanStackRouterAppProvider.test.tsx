47bd4e72db69f1e9bde8b854140b5d3d52b2cf19
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2025-05-30 15:06:50 +0100

    [AppProvider] Add Tanstack React Router provider (#4971)

---
+++ packages/toolpad-core/src/tanstack-router/TanStackRouterAppProvider.test.tsx
@@ -0,0 +1,47 @@
+/**
+ * @vitest-environment jsdom
+ */
+
+import * as React from 'react';
+import { describe, test, expect } from 'vitest';
+import { act, render, screen } from '@testing-library/react';
+import {
+  createRootRoute,
+  createRoute,
+  createRouter,
+  Outlet,
+  RouterProvider,
+} from '@tanstack/react-router';
+import { TanStackRouterAppProvider } from './TanStackRouterAppProvider';
+
+describe('Tanstack React Router AppProvider', () => {
+  const rootRoute = createRootRoute({
+    component: () => (
+      <TanStackRouterAppProvider>
+        <Outlet />
+      </TanStackRouterAppProvider>
+    ),
+  });
+
+  const indexRoute = createRoute({
+    getParentRoute: () => rootRoute,
+    path: '/',
+    component: () => <span>Hello</span>,
+  });
+
+  const routeTree = rootRoute.addChildren([indexRoute]);
+  const router = createRouter({ routeTree, defaultPendingMinMs: 0 });
+
+  test('renders content correctly', async () => {
+    await act(() =>
+      router.navigate({
+        to: '/',
+      }),
+    );
+
+    // placeholder test
+    render(<RouterProvider router={router} />);
+
+    expect(screen.getByText('Hello')).toBeTruthy();
+  });
+});

7c44800fd0004a83c81a12372534c36c2f4fe416
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2025-06-09 17:31:26 +0100

    Remove Toolpad Studio (#4987)

--- packages/toolpad-studio/src/runtime/constants.ts
+++
@@ -1,2 +0,0 @@
-export const FONTS_URL =
-  'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap';
--- scripts/restTestServer.ts
+++
@@ -1,6 +0,0 @@
-import { startTestServer } from '@toolpad/studio-tests/integration/rest-basic/testServer';
-
-startTestServer(8080).then((server) => {
-  // eslint-disable-next-line no-console
-  console.log(`Server started at http://localhost:${server.port}`);
-});

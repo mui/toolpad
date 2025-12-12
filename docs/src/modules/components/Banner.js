e5311037918ac65d8cd582ed07dd1d40c29b5d64
Prakhar Gupta <92228082+prakhargupta1@users.noreply.github.com>
2025-12-12 14:13:34 +0530

    top-banner

---
+++ docs/src/modules/components/Banner.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import Alert from '@mui/material/Alert';
+
+export default function Banner() {
+  return (
+    <Alert
+      severity="warning"
+      sx={{
+        mb: 0,
+        borderRadius: 0,
+        '& .MuiAlert-message': {
+          width: '100%',
+        },
+      }}
+    >
+      At the moment, the maintainers are primarily focused on other projects and are not actively
+      working on Toolpad Core and Studio. You can use the CRUD dashboard template from the{' '}
+      <a
+        href="https://mui.com/material-ui/getting-started/templates/#free-templates"
+        style={{ color: 'inherit', textDecoration: 'underline' }}
+      >
+        Material UI React templates page
+      </a>{' '}
+      as an easy-to-customize, copy-pastable alternative to the main Toolpad Core components —
+      including dashboard layout and CRUD features. To share any feedback, please check{' '}
+      <a
+        href="https://github.com/mui/toolpad/discussions/5011"
+        style={{ color: 'inherit', textDecoration: 'underline' }}
+      >
+        this GitHub discussion.
+      </a>{' '}
+    </Alert>
+  );
+}
